d62db34dfdd3adb9264f3af6e34446c32d46bf50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:23 +0000

    Add package qctrl/11.0.1/qctrl-11.0.1-py3-none-any.whl

---
+++ packages/qctrl/qctrl-11.0.1-py3-none-any.whl/qctrl/queries/start_action.py
@@ -0,0 +1,25 @@
+# Copyright 2020 Q-CTRL Pty Ltd & Q-CTRL Inc. All rights reserved.
+#
+# Licensed under the Q-CTRL Terms of service (the "License"). Unauthorized
+# copying or use of this file, via any medium, is strictly prohibited.
+# Proprietary and confidential. You may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#     https://q-ctrl.com/terms
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS. See the
+# License for the specific language.
+# pylint:disable=missing-module-docstring
+from .base import PatternQuery
+
+
+class StartActionQuery(PatternQuery):  # pylint:disable=too-few-public-methods
+    """Query used to start an action."""
+
+    query_pattern = """
+        mutation startAction {
+            %(mutation_name)s(input: %(payload)s)
+            %(selection)s
+        }
+    """
