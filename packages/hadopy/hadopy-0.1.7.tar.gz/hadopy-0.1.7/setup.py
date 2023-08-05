3230ea21147ecd085c80d2e402d81b082470fd62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:05 +0000

    Add package hadopy/0.1.7/hadopy-0.1.7.tar.gz

---
+++ packages/hadopy/hadopy-0.1.7.tar.gz/hadopy-0.1.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'click>=7.0,<8.0'
+]
+
+setup(
+    name='hadopy',
+    description="Easy parallel map-reduce command line tool",
+    version='0.1.7',
+    packages=find_packages(),
+    install_requires=install_requires,
+    python_requires='>=3.6',
+    author='Lucas van der Horst',
+    author_email='Lucas.vanderhorst@student.hu.nl',
+    keyword="mapreduce, map, parallel, hadoop",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='GPL3',
+    url='https://github.com/MariaDukmak/Hadopy',
+    entry_points='''
+        [console_scripts]
+        hadopy=hadopy.main:cli
+    '''
+)
