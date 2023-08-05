b72aabdedf7fa1fbd6c0a1f62910135100794ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:55 +0000

    Add package erddap-python/0.0.9b0/erddap_python-0.0.9b0-py3-none-any.whl

---
+++ packages/erddap-python/erddap_python-0.0.9b0-py3-none-any.whl/erddapClient/__init__.py
@@ -0,0 +1,8 @@
+from erddapClient.erddap_server import ERDDAP_Server
+from erddapClient.erddap_dataset import ERDDAP_Dataset
+from erddapClient.erddap_tabledap import ERDDAP_Tabledap
+from erddapClient.erddap_griddap import ERDDAP_Griddap
+
+__all__ = ["ERDDAP_Server", "ERDDAP_Dataset", "ERDDAP_Tabledap", "ERDDAP_Griddap"]
+
+__version__ = "0.0.9b"
