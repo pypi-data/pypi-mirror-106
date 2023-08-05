67847605ce341b78586ed7f90adc9b758c066aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:57 +0000

    Add package wavelocator/0.0.0/wavelocator-0.0.0.tar.gz

---
+++ packages/wavelocator/wavelocator-0.0.0.tar.gz/wavelocator-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='wavelocator',
+    packages=["wavelocator"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
