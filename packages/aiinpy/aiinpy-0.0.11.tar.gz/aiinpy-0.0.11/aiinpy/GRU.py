30c742411e91bd494b5760062cc111fcf89e1dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:13 +0000

    Add package aiinpy/0.0.11/aiinpy-0.0.11.tar.gz

---
+++ packages/aiinpy/aiinpy-0.0.11.tar.gz/aiinpy-0.0.11/aiinpy/GRU.py
@@ -0,0 +1,9 @@
+import numpy as np
+from .ActivationFunctions import Sigmoid, DerivativeOfSigmoid, StableSoftMax, DerivativeOfStableSoftMax, ReLU, DerivativeOfReLU, Tanh, DerivativeOfTanh
+
+import numpy as np
+from .ActivationFunctions import Sigmoid, Tanh, ReLU, LeakyReLU, StableSoftMax
+
+class GRU:
+  def __init__(self):
+    print('Not Started')
---
+++ packages/aiinpy/aiinpy-0.0.11.tar.gz/aiinpy-0.0.11/aiinpy/LSTM.py
@@ -0,0 +1,6 @@
+import numpy as np
+from .ActivationFunctions import Sigmoid, Tanh, ReLU, LeakyReLU, StableSoftMax
+
+class LSTM:
+  def __init__(self):
+    print('Not Started')
