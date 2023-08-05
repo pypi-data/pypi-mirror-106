76416bab463a4cc86c9497547c3be144fd12b605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:39 +0000

    Add package testindatadev/0.0.15/testindatadev-0.0.15.tar.gz

---
+++ packages/testindatadev/testindatadev-0.0.15.tar.gz/testindatadev-0.0.15/testindatadev/s3/qiniu/qiniu.py
@@ -0,0 +1,14 @@
+from qiniu import Auth, put_file, etag
+import qiniu.config
+
+class Qiniu():
+    def __init__(self, upload_token):
+        self.upload_token = upload_token
+
+
+    #上传单个文件数据
+    def PutObject(self, bucketName, objectName, filePath, content_type='application/octet-stream', metadata=None):
+        try:
+            ret, info = put_file(self.upload_token, objectName, filePath)
+        except:
+            raise Exception("failed in putting file")
