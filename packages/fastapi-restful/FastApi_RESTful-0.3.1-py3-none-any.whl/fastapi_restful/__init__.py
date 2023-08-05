2f03f0916de0155fa778a5d58a090162d6c42219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:19 +0000

    Add package fastapi-restful/0.3.1/FastApi_RESTful-0.3.1-py3-none-any.whl

---
+++ packages/fastapi-restful/FastApi_RESTful-0.3.1-py3-none-any.whl/fastapi_restful/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.1"
+
+from .cbv_base import Api, Resource, set_responses, take_init_parameters
+
+__all__ = [
+    "Api",
+    "Resource",
+    "set_responses",
+    "take_init_parameters",
+]
