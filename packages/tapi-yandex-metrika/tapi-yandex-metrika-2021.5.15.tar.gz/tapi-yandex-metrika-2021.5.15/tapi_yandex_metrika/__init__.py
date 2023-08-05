976d6a2958a24f52a948dfdf39abc5d01cd795a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:15 +0000

    Add package tapi-yandex-metrika/2021.5.15/tapi-yandex-metrika-2021.5.15.tar.gz

---
+++ packages/tapi-yandex-metrika/tapi-yandex-metrika-2021.5.15.tar.gz/tapi-yandex-metrika-2021.5.15/tapi_yandex_metrika/__init__.py
@@ -0,0 +1,11 @@
+
+__author__ = 'Pavel Maksimov'
+__email__ = 'vur21@ya.com'
+__version__ = '2021.5.15'
+
+from .resource_mapping import *
+from .tapi_yandex_metrika import (
+    YandexMetrikaStats,
+    YandexMetrikaLogsapi,
+    YandexMetrikaManagement
+)
