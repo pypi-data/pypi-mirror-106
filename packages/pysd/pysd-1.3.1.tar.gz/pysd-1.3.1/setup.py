c88d61996acc15bd4670f1ec3d73b13a00ba80c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:51 +0000

    Add package pysd/1.3.1/pysd-1.3.1.tar.gz

---
+++ packages/pysd/pysd-1.3.1.tar.gz/pysd-1.3.1/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+exec(open('pysd/_version.py').read())
+print(__version__)
+
+long_description = """Project Documentation: http://pysd.readthedocs.org/"""
+
+setup(
+    name='pysd',
+    version=__version__,
+    python_requires='>=3.7',
+    author='James Houghton',
+    author_email='james.p.houghton@gmail.com',
+    packages=find_packages(exclude=['docs', 'tests', 'dist', 'build']),
+    url='https://github.com/JamesPHoughton/pysd',
+    license='LICENSE.txt',
+    description='System Dynamics Modeling in Python',
+    long_description=long_description,
+    keywords=['System Dynamics', 'Vensim', 'XMILE'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Intended Audience :: Science/Research',
+    ],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'parsimonious',
+        'black',
+        'xarray',
+        'lxml',
+        'xlwt==1.2.0',        
+        'funcsigs',
+        'pydoe',
+        'xlrd',
+        'regex',
+        'chardet',
+        'openpyxl'
+    ],
+    package_data={
+        'py_backend': [
+            'output_style.yapf',
+            'xmile/smile.grammar'
+        ]
+    },
+    include_package_data=True
+)
