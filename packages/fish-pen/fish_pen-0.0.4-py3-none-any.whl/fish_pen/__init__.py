e2b33584dcdffd5fa675e89ad21626b07b4bfa3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:53 +0000

    Add package fish-pen/0.0.4/fish_pen-0.0.4-py3-none-any.whl

---
+++ packages/fish-pen/fish_pen-0.0.4-py3-none-any.whl/fish_pen/__init__.py
@@ -0,0 +1,15 @@
+"""
+Test module meant primarily for showing off how to integrate documentation and
+CI into your projects.
+"""
+
+__version__ = "0.0.4"
+
+
+def hello() -> None:
+    """
+    Print ``Hello world!``. As simple as it gets.
+
+    :return: `None`.
+    """
+    print("Hello world!")
