987752009fd325baaac035f30be705d0d079ef25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:45 +0000

    Add package dsnd-distributions-MT/0.1/dsnd_distributions_MT-0.1.tar.gz

---
+++ packages/dsnd-distributions-MT/dsnd_distributions_MT-0.1.tar.gz/dsnd_distributions_MT-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_MT',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_distributions_MT'],
+      author = 'Meng Tan',
+      author_email = 'nanco104@gmail.com',
+      zip_safe=False)
