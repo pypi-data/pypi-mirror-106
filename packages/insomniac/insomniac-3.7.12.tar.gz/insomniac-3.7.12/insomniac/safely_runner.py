674a440ccf5d16d0dad2fbf8323ef76be5713f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:59 +0000

    Add package insomniac/3.7.12/insomniac-3.7.12.tar.gz

---
+++ packages/insomniac/insomniac-3.7.12.tar.gz/insomniac-3.7.12/insomniac/safely_runner.py
@@ -0,0 +1,39 @@
+from http.client import HTTPException
+from socket import timeout
+
+from insomniac.device_facade import DeviceFacade
+from insomniac.navigation import navigate, LanguageChangedException
+from insomniac.sleeper import sleeper
+from insomniac.utils import *
+from insomniac.views import TabBarTabs
+
+
+class RestartJobRequiredException(Exception):
+    pass
+
+
+def run_safely(device_wrapper):
+    def actual_decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                func(*args, **kwargs)
+            except (DeviceFacade.JsonRpcError, IndexError, HTTPException, timeout) as ex:
+                print(COLOR_FAIL + describe_exception(ex) + COLOR_ENDC)
+                save_crash(device_wrapper.get(), ex)
+                print("No idea what it was. Let's try again.")
+                # Hack for the case when IGTV was accidentally opened
+                close_instagram(device_wrapper.device_id, device_wrapper.app_id)
+                sleeper.random_sleep()
+                open_instagram(device_wrapper.device_id, device_wrapper.app_id)
+                sleeper.random_sleep()
+                navigate(device_wrapper.get(), TabBarTabs.PROFILE)
+            except LanguageChangedException:
+                print_timeless("")
+                print("Language was changed. We'll have to start from the beginning.")
+                navigate(device_wrapper.get(), TabBarTabs.PROFILE)
+            except RestartJobRequiredException:
+                print_timeless("")
+                print("Restarting job...")
+                navigate(device_wrapper.get(), TabBarTabs.PROFILE)
+        return wrapper
+    return actual_decorator
