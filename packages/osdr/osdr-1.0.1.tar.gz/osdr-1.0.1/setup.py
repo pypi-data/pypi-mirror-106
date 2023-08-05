aa0765d302010b3422ab9e915f8a2c76f17fbc73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:21 +0000

    Add package osdr/1.0.1/osdr-1.0.1.tar.gz

---
+++ packages/osdr/osdr-1.0.1.tar.gz/osdr-1.0.1/setup.py
@@ -0,0 +1,24 @@
+#from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="osdr",
+    py_modules=['osdr'],
+    version="1.0.1",
+    author="orion",
+    author_email="osdr@orion.com",
+    description="osdr package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
