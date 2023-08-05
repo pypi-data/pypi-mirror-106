53f95439a3f1d0cc976486d5c1582e3d3666e92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:52 +0000

    Add package cutfinder/0.1.1/cutfinder-0.1.1-py3-none-any.whl

---
+++ packages/cutfinder/cutfinder-0.1.1-py3-none-any.whl/cut_finder/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'ccluff'
+__version__ = "0.1.1"
+
+# from boards import FinalBoard, StockBoard
+# from board_sets import FinalBoardSet, StockBoardSet
+
+from cut_finder import CutFinder
