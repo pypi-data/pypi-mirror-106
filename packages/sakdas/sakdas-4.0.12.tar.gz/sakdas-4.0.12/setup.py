e22be6a5fce6b6a6fe8ab54e54363f4277daf6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:23 +0000

    Add package sakdas/4.0.12/sakdas-4.0.12.tar.gz

---
+++ packages/sakdas/sakdas-4.0.12.tar.gz/sakdas-4.0.12/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sakdas", # Replace with your own username
+    version="4.0.12",
+    author="Sakda Loetpipatwanich",
+    author_email="sakda.loet@gmail.com",
+    description="A data profiling and data quality auditing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sakdaloe/sakdas",
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
