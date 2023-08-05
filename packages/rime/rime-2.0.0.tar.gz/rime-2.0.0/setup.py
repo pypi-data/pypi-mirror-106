99040daecb161313cfb4508bf1a8871dd659af68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:38 +0000

    Add package rime/2.0.0/rime-2.0.0.tar.gz

---
+++ packages/rime/rime-2.0.0.tar.gz/rime-2.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='rime',
+    version='2.0.0',
+    scripts=['bin/rime', 'bin/rime_init'],
+    packages=['rime', 'rime.basic', 'rime.basic.targets', 'rime.basic.util',
+              'rime.core', 'rime.plugins', 'rime.plugins.judge_system',
+              'rime.plugins.plus', 'rime.plugins.summary', 'rime.util'],
+    package_dir={'rime': 'rime'},
+    install_requires=['six', 'Jinja2'],
+    tests_require=['pytest', 'mock'],
+    package_data={'rime': ['plugins/summary/*.ninja']},
+)
