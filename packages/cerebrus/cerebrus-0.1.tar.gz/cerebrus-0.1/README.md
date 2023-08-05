a54435d43fd83935b0087d2f43aa5e5c81e43e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:18 +0000

    Add package cerebrus/0.1/cerebrus-0.1.tar.gz

---
+++ packages/cerebrus/cerebrus-0.1.tar.gz/cerebrus-0.1/README.md
@@ -0,0 +1,10 @@
+pkgpy - cross platform cross language package manager
+
+### installation
+(run as admin)
+```
+pip install pkgpy
+python
+import pkgpy
+pkgpy.install()
+```
---
+++ packages/cerebrus/cerebrus-0.1.tar.gz/cerebrus-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cerebrus',
+      version='0.1',
+      description='cerebrus',
+      url='http://github.com/project-cerebrus',
+      author='Eris9/Sakurai07',
+      author_email='blzzardst0rm@gmail.com',
+      license='MIT',
+      packages=['cerebrus'],
+      zip_safe=False)
