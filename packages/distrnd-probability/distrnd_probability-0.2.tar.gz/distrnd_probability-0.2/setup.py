6f7b3eefb80e25bc17d396fc2fc3eef95a11e419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:08 +0000

    Add package distrnd-probability/0.2/distrnd_probability-0.2.tar.gz

---
+++ packages/distrnd-probability/distrnd_probability-0.2.tar.gz/distrnd_probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distrnd_probability',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distrnd_probability'],
+      author='Varaprasad Gadi',
+      author_email='gvp3682@gmail.com',
+      zip_safe=False)
