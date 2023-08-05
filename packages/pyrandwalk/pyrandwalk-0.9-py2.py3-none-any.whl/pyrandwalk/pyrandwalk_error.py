25ea1950146c6f1e0133f6f5deda79bc01e1991e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:54 +0000

    Add package pyrandwalk/0.9/pyrandwalk-0.9-py2.py3-none-any.whl

---
+++ packages/pyrandwalk/pyrandwalk-0.9-py2.py3-none-any.whl/pyrandwalk/pyrandwalk_error.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""pyrandwalk errors."""
+
+
+class pyrandwalkStateError(Exception):
+    """State error class."""
+
+    pass
+
+
+class pyrandwalkTransitionsError(Exception):
+    """Transitions matrix error class."""
+
+    pass
+
+
+class pyrandwalkVectorError(Exception):
+    """Vector error class."""
+
+    pass
