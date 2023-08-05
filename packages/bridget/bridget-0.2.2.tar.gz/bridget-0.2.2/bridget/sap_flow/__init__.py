30b577a04065b333bf8ce88ef0488fbf53acd195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:50 +0000

    Add package bridget/0.2.2/bridget-0.2.2.tar.gz

---
+++ packages/bridget/bridget-0.2.2.tar.gz/bridget-0.2.2/bridget/sap_flow/__init__.py
@@ -0,0 +1,11 @@
+"""
+Sap flow
+========
+
+The sapflow packages includes helpful functions to process and analyze
+sap flow data.
+
+[DESCRIPTION]
+
+"""
+from .sap_flow import sap_velocity_heat_ratio, sap_velocity_granier, sap_flow
