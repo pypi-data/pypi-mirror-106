3d299f53c4818a1cc97e458307d6eae2b4b2093a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:38 +0000

    Add package hajkr-youtube-scraper/0.0.1/hajkr_youtube_scraper-0.0.1-py3-none-any.whl

---
+++ packages/hajkr-youtube-scraper/hajkr_youtube_scraper-0.0.1-py3-none-any.whl/youtube-scraper/__init__.py
@@ -0,0 +1,17 @@
+import argparse
+from crawler import Crawler
+from utils import write_to_csv
+
+parser = argparse.ArgumentParser()
+parser.add_argument('channel_url', type=str, help="Youtube channel URL")
+parser.add_argument('--filename', type=str, help="Output file", default='output/videos.csv')
+parser.add_argument('--stop_video_id', type=str, help="Youtube video ID that indicates to stop crawling", default=None)
+args = parser.parse_args()
+
+print(args)
+
+crawler = Crawler(args.channel_url, stop_id=args.stop_video_id)
+crawled_videos = crawler.crawl()
+
+write_to_csv(crawled_videos, args.filename)
+print(f"{len(crawled_videos)} videos saved to {args.filename}")
---
+++ packages/hajkr-youtube-scraper/hajkr_youtube_scraper-0.0.1-py3-none-any.whl/youtube-scraper/crawler.py
@@ -0,0 +1,136 @@
+import time
+from pprint import pprint
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+class ElementsLengthChanges(object):
+    def __init__(self, selector):
+        self.selector = selector
+        self.initial_length = None
+        self.driver = None
+
+    def __call__(self, driver):
+        self.driver = driver
+        self.set_initial_length()
+        return self.count_elements() > self.initial_length
+
+    def set_initial_length(self):
+        if self.initial_length is None:
+            self.initial_length = self.count_elements()
+
+    def count_elements(self):
+        return self.driver.find_elements_by_css_selector(self.selector)
+
+
+def build_browser():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    return webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+
+
+def extract_video_id(url):
+    parsed = urlparse.urlparse(url)
+    return parse_qs(parsed.query)['v'][0]
+
+
+def url_to_thumbnail(url):
+    video_id = extract_video_id(url)
+    return f"https://i3.ytimg.com/vi/{video_id}/hqdefault.jpg"
+
+
+VIDEOS_SELECTOR = 'ytd-grid-renderer.ytd-item-section-renderer #items > ytd-grid-video-renderer'
+VIDEO_TITLE_SELECTOR = 'ytd-video-primary-info-renderer h1'
+VIDEO_DESCRIPTION_SELECTOR = 'ytd-video-secondary-info-renderer #description'
+VIDEO_AUTHOR_SELECTOR = 'ytd-video-secondary-info-renderer ytd-channel-name a'
+VIDEO_PUBLISHED_AT_SELECTOR = 'ytd-video-primary-info-renderer #date yt-formatted-string'
+
+
+class Crawler:
+    def __init__(self, channel_url, stop_id=None):
+        self.channel_url = channel_url
+        self.browser = build_browser()
+        self.stop_id = stop_id
+
+    def __del__(self):
+        self.browser.close()
+
+    def crawl(self):
+        print(self.channel_url)
+
+        self.navigate_to_videos()
+        self.load_all_pages()
+
+        print("Extracting video urls...")
+        video_urls = self.extract_video_urls()
+
+        crawled_videos = []
+        for url in video_urls:
+            print(url)
+
+            self.navigate_to_video(url)
+            crawled_videos.append(self.extract_video_attributes(url))
+
+        return crawled_videos
+
+    def extract_video_urls(self):
+        urls = []
+        for element in self.find_video_elements():
+            url = element.find_element_by_css_selector('ytd-thumbnail a').get_attribute('href')
+            if self.is_stop_url(url):
+                break
+
+            urls.append(url)
+
+        return urls
+
+    def extract_video_attributes(self, url):
+        return {
+            "url": url,
+            "title": self.browser.find_element_by_css_selector(VIDEO_TITLE_SELECTOR).text,
+            "description": self.browser.find_element_by_css_selector(VIDEO_DESCRIPTION_SELECTOR).text,
+            "author": self.browser.find_element_by_css_selector(VIDEO_AUTHOR_SELECTOR).text,
+            "published_at": self.browser.find_element_by_css_selector(VIDEO_PUBLISHED_AT_SELECTOR).text,
+            "thumbnail": url_to_thumbnail(url),
+        }
+
+    def count_video_elements(self):
+        return len(self.find_video_elements())
+
+    def find_video_elements(self):
+        return self.browser.find_elements_by_css_selector(VIDEOS_SELECTOR)
+
+    def navigate_to_videos(self):
+        videos_url = f'{self.channel_url}/videos'
+        self.browser.get(videos_url)
+        self.browser.find_element_by_css_selector('button').click()
+
+    def navigate_to_video(self, url):
+        self.browser.get(url)
+        WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, VIDEO_TITLE_SELECTOR))
+        )
+
+    def load_all_pages(self):
+        while True:
+            try:
+                self.load_next_page()
+                print(self.count_video_elements())
+            except TimeoutException:
+                break
+
+    def load_next_page(self):
+        self.browser.execute_script("window.scrollTo(0, Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight))")
+        WebDriverWait(self.browser, 5).until(ElementsLengthChanges(VIDEOS_SELECTOR))
+
+    def is_stop_url(self, url):
+        video_id = extract_video_id(url)
+        return video_id == self.stop_id
