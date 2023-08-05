db79268e84b420196533384fbcfc440a91b0d2e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:27 +0000

    Add package baayes/0.1.7/baayes-0.1.7.tar.gz

---
+++ packages/baayes/baayes-0.1.7.tar.gz/baayes-0.1.7/baayes/PARAMETERS.py
@@ -0,0 +1,7 @@
+from sklearn.metrics import recall_score, precision_score, accuracy_score, f1_score
+
+# def metric_am(y_true, y_pred):
+#     # return 0.8*recall_score(y_true, y_pred) + 0.2*precision_score(y_true, y_pred)
+#     return accuracy_score(y_true, y_pred)
+
+# METRIC=metric_am
