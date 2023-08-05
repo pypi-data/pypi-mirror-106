b970c570d18473096cdae4f083095f72a27b8ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:11 +0000

    Add package py-kingbot/0.1/py-kingbot-0.1.tar.gz

---
+++ packages/py-kingbot/py-kingbot-0.1.tar.gz/py-kingbot-0.1/setup.py
@@ -0,0 +1,43 @@
+import re
+
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+name = "py-kingbot"
+author = "TeamDevil"
+author_email = "De.codes.official@gmail.com"
+version="0.1"
+description = "A Secure and Powerful Python-PyrogramBased Library For Kingbot ."
+license = "GNU AFFERO GENERAL PUBLIC LICENSE (v3)"
+url = "https://github.com/edguru/kinguser"
+download_url="https://github.com/thearyanag/kingbot/archive/refs/tags/v1.0.zip"
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=author_email,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=url,
+    license=license,
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pyrogram",
+        "redis",
+        "python-decouple==3.3",
+        "TgCrypto==1.2.2",
+        "python-dotenv==0.15.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
