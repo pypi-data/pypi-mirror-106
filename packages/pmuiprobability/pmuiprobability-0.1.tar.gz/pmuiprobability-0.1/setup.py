7b407d3c50b8426cb7a4133629bdc387fb9428a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:34 +0000

    Add package pmuiprobability/0.1/pmuiprobability-0.1.tar.gz

---
+++ packages/pmuiprobability/pmuiprobability-0.1.tar.gz/pmuiprobability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pmuiprobability',
+      version='0.1',
+      description='Convenient probability distributions',
+      packages=['pmuiprobability'],
+      author = 'Phil Mui',
+      author_email = 'thephilmui@gmail.com',
+      zip_safe=False)
