c3cbea25ed41b472b5ee5ef8d5daf35149812f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:46 +0000

    Add package text-diff/0.0.4/text_diff-0.0.4-py2.py3-none-any.whl

---
+++ packages/text-diff/text_diff-0.0.4-py2.py3-none-any.whl/text_diff/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Text Diff."""
+
+__author__ = "Rémi Delbouys"
+__email__ = "remi.delbouys@laposte.net"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.4"
+
+
+def get_module_version():
+    return __version__
+
+
+from .extract_diff import (  # noqa: F401
+    AddedLine,
+    DiffLine,
+    EditOperation,
+    Mask,
+    ModifiedLine,
+    RemovedLine,
+    TextDifferences,
+    UnchangedLine,
+    text_differences,
+)
