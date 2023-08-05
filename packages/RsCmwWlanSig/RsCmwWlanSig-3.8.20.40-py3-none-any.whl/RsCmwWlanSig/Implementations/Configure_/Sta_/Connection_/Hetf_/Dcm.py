c42162bea46d97b56f76e34d1cd03acc586192bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:39 +0000

    Add package RsCmwWlanSig/3.8.20.40/RsCmwWlanSig-3.8.20.40-py3-none-any.whl

---
+++ packages/RsCmwWlanSig/RsCmwWlanSig-3.8.20.40-py3-none-any.whl/RsCmwWlanSig/Implementations/Configure_/Sta_/Connection_/Hetf_/Dcm.py
@@ -0,0 +1,33 @@
+from ......Internal.Core import Core
+from ......Internal.CommandsGroup import CommandsGroup
+from ......Internal import Conversions
+from ...... import repcap
+
+
+# noinspection PyPep8Naming,PyAttributeOutsideInit,SpellCheckingInspection
+class Dcm:
+	"""Dcm commands group definition. 1 total commands, 0 Sub-groups, 1 group commands"""
+
+	def __init__(self, core: Core, parent):
+		self._core = core
+		self._base = CommandsGroup("dcm", core, parent)
+
+	def set(self, dcm: bool, station=repcap.Station.Default) -> None:
+		"""SCPI: CONFigure:WLAN:SIGNaling<instance>:STA<s>:CONNection:HETF:DCM \n
+		Snippet: driver.configure.sta.connection.hetf.dcm.set(dcm = False, station = repcap.Station.Default) \n
+		Specifies whether the HE TB response uses dual carrier modulation (DCM) . \n
+			:param dcm: OFF | ON
+			:param station: optional repeated capability selector. Default value: Nr1 (settable in the interface 'Sta')"""
+		param = Conversions.bool_to_str(dcm)
+		station_cmd_val = self._base.get_repcap_cmd_value(station, repcap.Station)
+		self._core.io.write(f'CONFigure:WLAN:SIGNaling<Instance>:STA{station_cmd_val}:CONNection:HETF:DCM {param}')
+
+	def get(self, station=repcap.Station.Default) -> bool:
+		"""SCPI: CONFigure:WLAN:SIGNaling<instance>:STA<s>:CONNection:HETF:DCM \n
+		Snippet: value: bool = driver.configure.sta.connection.hetf.dcm.get(station = repcap.Station.Default) \n
+		Specifies whether the HE TB response uses dual carrier modulation (DCM) . \n
+			:param station: optional repeated capability selector. Default value: Nr1 (settable in the interface 'Sta')
+			:return: dcm: OFF | ON"""
+		station_cmd_val = self._base.get_repcap_cmd_value(station, repcap.Station)
+		response = self._core.io.query_str(f'CONFigure:WLAN:SIGNaling<Instance>:STA{station_cmd_val}:CONNection:HETF:DCM?')
+		return Conversions.str_to_bool(response)
