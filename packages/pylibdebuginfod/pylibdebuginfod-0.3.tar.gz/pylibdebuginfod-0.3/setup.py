88eecd1374a23e904e901f5db3107a42b19e8c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:47 +0000

    Add package pylibdebuginfod/0.3/pylibdebuginfod-0.3.tar.gz

---
+++ packages/pylibdebuginfod/pylibdebuginfod-0.3.tar.gz/pylibdebuginfod-0.3/setup.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2021 Andreas Ziegler <andreas.ziegler@fau.de>
+#
+# SPDX-License-Identifier: MIT
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'pylibdebuginfod',
+    description = 'Python bindings for libdebuginfod',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'Andreas Ziegler',
+    author_email = 'andreas.ziegler@fau.de',
+    url = 'https://github.com/rupran/pylibdebuginfod',
+    version = '0.3',
+    license = 'MIT',
+    packages = [
+        'libdebuginfod'
+    ],
+    install_requires = [
+        'pyelftools'
+    ],
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
