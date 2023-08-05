b6de8dbdee3b82a54656c3985f7ba56bc9d44ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:30 +0000

    Add package static-ffmpeg/1.0.7/static_ffmpeg-1.0.7.tar.gz

---
+++ packages/static-ffmpeg/static_ffmpeg-1.0.7.tar.gz/static_ffmpeg-1.0.7/static_ffmpeg/run.py
@@ -0,0 +1,33 @@
+"""
+    Entry point for running the ffmpeg executable.
+"""
+
+
+import os
+import sys
+
+import static_ffmpeg
+
+PCKG_PATH = static_ffmpeg.__path__[0]  # type: ignore  # mypy issue #1422
+
+
+def get_platform_executable_or_raise():
+    """Either get the executable or raise an error"""
+    if sys.platform == "win32":
+        return os.path.join(PCKG_PATH, "win32", "ffmpeg.exe")
+    if sys.platform == "linux":
+        return os.path.join(PCKG_PATH, "linux", "ffmpeg")
+    if sys.platform == "darwin":
+        return os.path.join(PCKG_PATH, "macos_x64", "ffmpeg")
+    raise OSError(f"Please implement static_ffmpeg for {sys.platform}")
+
+
+def main():
+    """Entry point for running static_ffmpeg, which delegates to ffmpeg."""
+    ffmpeg_exe = get_platform_executable_or_raise()
+    str_args = " ".join(sys.argv[1:])
+    if sys.platform != 'win32':
+        assert os.access(ffmpeg_exe, os.X_OK)
+    cmd = f"{ffmpeg_exe} {str_args}"
+    rtn = os.system(cmd)
+    sys.exit(rtn)
