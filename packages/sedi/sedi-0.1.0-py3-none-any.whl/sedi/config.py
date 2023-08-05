a8ead8556c5a363dedf95bf23dcca83fbbb5f66d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:27 +0000

    Add package sedi/0.1.0/sedi-0.1.0-py3-none-any.whl

---
+++ packages/sedi/sedi-0.1.0-py3-none-any.whl/sedi/config.py
@@ -0,0 +1,7 @@
+TIPS = 'Usage: sedi <keyword> <save_path>'
+
+# Request header
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 Edg/90.0.818.62'
+
+# Search engine urls
+BAIDU_URL = 'https://image.baidu.com/search/acjson?tn=resultjson_com&logid=11295493557250147196&ipn=rj&ct=201326592&is=&fp=result&queryWord={}&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=&z=&ic=&hd=&latest=&copyright=&word={}&s=&se=&tab=&width=&height=&face=&istype=&qc=&nc=1&fr=&expermode=&force=&pn={}&rn=30&gsm=b4&1621091871197='
