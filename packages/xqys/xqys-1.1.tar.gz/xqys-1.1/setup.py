dce3379c2cb23055dba99394a7f12a9f4f1af6eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:15 +0000

    Add package xqys/1.1/xqys-1.1.tar.gz

---
+++ packages/xqys/xqys-1.1.tar.gz/xqys-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xqys',
+    version='1.1',
+    author='Mohamed Medhat',
+    author_email='mohamed.medhat8334@gmail.com',
+    description='Deep Learning Framework',
+    url="https://github.com/moh833/XQYS",
+    packages=find_packages(),
+    install_requires=["numpy", "matplotlib"],
+)
---
+++ packages/xqys/xqys-1.1.tar.gz/xqys-1.1/xqys/__init__.py
@@ -0,0 +1,2 @@
+from xqys.neural_network import NeuralNet
+from xqys.neural_network import load_model
