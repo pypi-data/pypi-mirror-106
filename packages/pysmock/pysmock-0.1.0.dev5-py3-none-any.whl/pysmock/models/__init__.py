4169eb9dde0a8edc35b7e7a226ab4c2e38b20432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:58 +0000

    Add package pysmock/0.1.0.dev5/pysmock-0.1.0.dev5-py3-none-any.whl

---
+++ packages/pysmock/pysmock-0.1.0.dev5-py3-none-any.whl/pysmock/models/__init__.py
@@ -0,0 +1,9 @@
+from . import Contact
+from . import License
+from . import  Info
+from . import  Request
+from . import  Response
+from . import  APIRequest
+from . import  MockSetup
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
