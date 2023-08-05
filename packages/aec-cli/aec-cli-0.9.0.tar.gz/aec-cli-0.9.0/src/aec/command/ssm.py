b1d78008f77a76f267c6349ac0aac084608715e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:24 +0000

    Add package aec-cli/0.9.0/aec-cli-0.9.0.tar.gz

---
+++ packages/aec-cli/aec-cli-0.9.0.tar.gz/aec-cli-0.9.0/src/aec/command/ssm.py
@@ -0,0 +1,37 @@
+from typing import Any, Dict, List, Optional
+
+import boto3
+
+import aec.util.tags as util_tags
+from aec.util.config import Config
+
+
+def describe(config: Config) -> List[Dict[str, Any]]:
+    """Describe instances running the SSM agent."""
+
+    instances_names = describe_instances_names(config)
+
+    client = boto3.client("ssm", region_name=config.get("region", None))
+
+    response = client.describe_instance_information()
+
+    return [
+        {
+            "ID": i["InstanceId"],
+            "Name": instances_names.get(i["InstanceId"], None),
+            "PingStatus": i["PingStatus"],
+            "Platform": f'{i["PlatformName"]} {i["PlatformVersion"]}',
+            "AgentVersion": i["AgentVersion"],
+        }
+        for i in response["InstanceInformationList"]
+    ]
+
+
+def describe_instances_names(config: Config) -> Dict[str, Optional[str]]:
+    """List EC2 instance names in the region."""
+
+    ec2_client = boto3.client("ec2", region_name=config.get("region", None))
+
+    response = ec2_client.describe_instances()
+
+    return {i["InstanceId"]: util_tags.get_value(i, "Name") for r in response["Reservations"] for i in r["Instances"]}
