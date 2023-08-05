5287ee3b349ed329e83dd4e7cefb40e0b60f9262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:42 +0000

    Add package scalene/1.3.4.2/scalene-1.3.4.2-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/scalene/scalene-1.3.4.2-cp39-cp39-macosx_11_0_arm64.whl/scalene/replacement_fork.py
@@ -0,0 +1,22 @@
+import os
+from scalene.scalene_profiler import Scalene
+from scalene.scalene_signals import ScaleneSignals
+
+
+@Scalene.shim
+def replacement_fork(scalene: Scalene) -> None:
+    """
+    Raises a signal when a process is the child after
+    a fork system call.
+    """
+    orig_fork = os.fork
+
+    def fork_replacement() -> int:
+        result = orig_fork()
+        if result == 0:
+            os.kill(os.getpid(), ScaleneSignals.fork_signal)
+        else:
+            scalene.add_child_pid(result)
+        return result
+
+    os.fork = fork_replacement
