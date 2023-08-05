7f5d967d39bbc853362bd1002beb062280c59667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:40 +0000

    Add package mangadex/1.0.0/mangadex-1.0.0.tar.gz

---
+++ packages/mangadex/mangadex-1.0.0.tar.gz/mangadex-1.0.0/mangadex/__init__.py
@@ -0,0 +1,10 @@
+from .errors import (ApiError, ApiClientError, MangaError, TagError, ChapterError, AuthorError, ScanlationGroupError, UserError)
+
+from .models import (Manga, Tag, Chapter, User, ScanlationGroup, Author)
+
+from .api import (Api)
+
+__author__ = 'Eduardo Ceja'
+__version__ = "1.0.0"
+__license__ = "MIT"
+__copytight__ = "Copyright (c) 2021 Eduardo Ceja"
