2a3beabbe72056b57ed29db710440ef651a92c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:32 +0000

    Add package tikotmusicaly/1.0.0/tikotmusicaly-1.0.0-py3-none-any.whl

---
+++ packages/tikotmusicaly/tikotmusicaly-1.0.0-py3-none-any.whl/tikotmusicaly.py
@@ -0,0 +1,7 @@
+class musicaly():
+	def account(artistid):
+		if artistid == musicalyaccounts[0]:
+			print("Hello TikOt!")
+			print("Your tracks:\nShort chill")
+	def version():
+		print("TikOt Musicaly API 1.0.0")
