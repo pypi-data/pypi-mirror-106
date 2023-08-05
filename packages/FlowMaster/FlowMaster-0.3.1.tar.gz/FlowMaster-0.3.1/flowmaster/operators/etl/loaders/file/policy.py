3170b78f9a4dfe4fc3eb6f95c483a50d983616ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:22 +0000

    Add package FlowMaster/0.3.1/FlowMaster-0.3.1.tar.gz

---
+++ packages/FlowMaster/FlowMaster-0.3.1.tar.gz/FlowMaster-0.3.1/flowmaster/operators/etl/loaders/file/policy.py
@@ -0,0 +1,37 @@
+from typing import Literal, Optional
+
+from pydantic import BaseModel, Field
+
+from flowmaster.operators.base.policy import BasePolicy
+from flowmaster.operators.etl.transform.policy import (
+    BaseTransformPolicy,
+    DTypeLiteralT,
+    ErrorPolicyLiteralT,
+)
+from flowmaster.setttings import FILE_STORAGE_DIR
+
+
+class FileLoadPolicy(BasePolicy):
+    save_mode: Literal["a", "w"]
+    file_name: str = "{{provider}} {{storage}}  {{name}}.tsv"
+    path: str = FILE_STORAGE_DIR
+    encoding: str = "UTF-8"
+    sep: str = "\t"
+    newline: str = "\n"
+    with_columns: bool = True
+    add_data_before: str = ""
+    add_data_after: str = ""
+    concurrency: int = 1
+
+
+class FileTransformPolicy(BaseTransformPolicy):
+    class ColumnSchema(BaseModel):
+        name: Optional[str] = None
+        dtype: Optional[DTypeLiteralT] = None
+        errors: Optional[ErrorPolicyLiteralT] = None
+        dt_format: Optional[str] = None
+        allow_null: Optional[bool] = None
+        null_values: Optional[list] = None
+        clear_values: Optional[list] = None
+
+    column_schema: dict[str, ColumnSchema] = Field(default_factory=dict)
---
+++ packages/FlowMaster/FlowMaster-0.3.1.tar.gz/FlowMaster-0.3.1/flowmaster/operators/etl/providers/abstract.py
@@ -0,0 +1,79 @@
+import datetime as dt
+import logging
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Iterator, Optional, Type
+
+import jinja2
+
+if TYPE_CHECKING:
+    from flowmaster.operators.base.policy import PydanticModelT
+    from flowmaster.operators.etl.policy import ETLFlowConfig
+    from flowmaster.operators.etl.transform import Transform
+    from flowmaster.operators.etl.dataschema import ExportContext
+
+
+class ExportAbstract(ABC):
+    def __init__(
+        self, config: "ETLFlowConfig", logger: Optional[logging.Logger] = None
+    ):
+        self.config = config
+        self.logger = logger or logging.getLogger(__name__)
+
+    @classmethod
+    def validate_params(cls, **params: dict) -> None:
+        ...
+
+    def collect_params(
+        self, start_period: dt.datetime, end_period: dt.datetime, **params
+    ) -> dict:
+        new_params = {}
+        for key, value in params.items():
+            if value is not None:
+                if isinstance(value, list):
+                    value = ",".join(map(str, value))
+
+                new_params[key] = value
+
+                if isinstance(value, str):
+                    new_params[key] = jinja2.Template(value).render(
+                        **{
+                            "start_period": start_period,
+                            "end_period": end_period,
+                            "datetime": dt.datetime.now(),
+                        }
+                    )
+
+        self.validate_params(**new_params)
+
+        return new_params
+
+    @abstractmethod
+    def __call__(self, *args, **kwargs) -> Iterator["ExportContext"]:
+        ...
+
+
+class ProviderAbstract(ABC):
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        ...
+
+    @property
+    @abstractmethod
+    def policy_model(self) -> "PydanticModelT":
+        ...
+
+    @property
+    @abstractmethod
+    def export_class(self) -> Type[ExportAbstract]:
+        ...
+
+    @property
+    def transform_class(self) -> Type["Transform"]:
+        from flowmaster.operators.etl.transform.service import Transform
+
+        return Transform
+
+    def __init__(self, *args, **kwargs):
+        self.Export = self.export_class(*args, **kwargs)
+        self.Transform = self.transform_class(*args, **kwargs)
