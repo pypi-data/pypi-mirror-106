4002e4d3147a15750e5dbbb357a40b07cc55bdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:28 +0000

    Add package angerr/0.0.5/angerr-0.0.5-py3-none-any.whl

---
+++ packages/angerr/angerr-0.0.5-py3-none-any.whl/anger.py
@@ -0,0 +1,24 @@
+class GodDammit(Exception):
+  def __init__(self, message="look i can't show your anger if you don't enter the anger amount.  make sure it is a int too, lmfao. *can't believe you don't even know how to code lol*"):
+    self.message = message
+    super().__init__(self.message)
+
+def anger(amount : int = None):
+  """The `Anger` function.  Allows you to express anger major when your coding doesn't work.
+
+  Paramaters:
+    amount (int): the amount of your angerrrrrrrr
+  
+  """
+  if amount == None:
+    print("WHY IS CODING SO HARD")
+  else:
+    if type(amount) != int:
+      raise GodDammit
+    else:
+      for i in range(amount):
+        print(f"{i + 1}. WHY IS CODING SO HARD")
+      amount_list = [a for a in str(amount)]
+      if 69 in amount_list or 420 in amount_list:
+        print("nice")
+      print("finally done raging.  are you?")
