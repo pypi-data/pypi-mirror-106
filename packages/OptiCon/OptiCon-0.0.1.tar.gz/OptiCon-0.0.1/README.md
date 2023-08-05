11515230be80f955e28eaccb6d0982da6b1120a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:58 +0000

    Add package OptiCon/0.0.1/OptiCon-0.0.1.tar.gz

---
+++ packages/OptiCon/OptiCon-0.0.1.tar.gz/OptiCon-0.0.1/README.md
@@ -0,0 +1,1 @@
+<b>Opti</b>mized <b>Con</b>nector is a modified version of MinecraftWS focused on optimized performance and compatibility with IndigoMod.
---
+++ packages/OptiCon/OptiCon-0.0.1.tar.gz/OptiCon-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import *
+
+setup(
+    name='OptiCon',
+    version='0.0.1',
+    packages=['OptiCon'],
+    url='https://github.com/IndigoMod/OptiCon',
+    license='MIT',
+    author='beta5051',
+    author_email='AcaiBerii@protonmail.com',
+    description='MinecraftWS fork that provides optimal performance and compatibility with IndigoMod.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['websockets>=8.1']
+)
