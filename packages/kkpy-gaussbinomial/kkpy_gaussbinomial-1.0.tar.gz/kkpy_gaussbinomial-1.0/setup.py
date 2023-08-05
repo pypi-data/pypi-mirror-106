af92709fcd6af841e2f2d4a000ab401cbdde2d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:51 +0000

    Add package kkpy-gaussbinomial/1.0/kkpy_gaussbinomial-1.0.tar.gz

---
+++ packages/kkpy-gaussbinomial/kkpy_gaussbinomial-1.0.tar.gz/kkpy_gaussbinomial-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kkpy_gaussbinomial',
+      version='1.0',
+      description='Gaussian/binomial distributions',
+      packages=['kkpy_gaussbinomial'],
+      author = 'Kamila Kolpashnikova',
+      author_email = 'kamilakolpashnikova@gmail.com',
+      zip_safe=False)
