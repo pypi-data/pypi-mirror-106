05349d7a9210f3e701877c21194d44cc074f9c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:58 +0000

    Add package user-id-nester/1.0.1/user_id_nester-1.0.1.tar.gz

---
+++ packages/user-id-nester/user_id_nester-1.0.1.tar.gz/user_id_nester-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name 		='user_id_nester',
+	version		='1.0.1',
+	py_modules	=['nester'],
+	author		='user-id',
+	author_email	='Keefernix@gmail.com',
+	description	='a simple script for printing nested lists',
+	)
