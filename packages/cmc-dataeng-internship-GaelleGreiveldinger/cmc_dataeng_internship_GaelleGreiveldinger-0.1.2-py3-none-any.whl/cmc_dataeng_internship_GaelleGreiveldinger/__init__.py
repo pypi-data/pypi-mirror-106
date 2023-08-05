1d64180379340c062a5b8d9aa486704cbe803abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:19 +0000

    Add package cmc-dataeng-internship-GaelleGreiveldinger/0.1.2/cmc_dataeng_internship_GaelleGreiveldinger-0.1.2-py3-none-any.whl

---
+++ packages/cmc-dataeng-internship-GaelleGreiveldinger/cmc_dataeng_internship_GaelleGreiveldinger-0.1.2-py3-none-any.whl/cmc_dataeng_internship_GaelleGreiveldinger/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 17 22:44:12 2021
+
+@author: Gaëlle Greiveldinger
+"""
+
---
+++ packages/cmc-dataeng-internship-GaelleGreiveldinger/cmc_dataeng_internship_GaelleGreiveldinger-0.1.2-py3-none-any.whl/cmc_dataeng_internship_GaelleGreiveldinger/exercise13.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 17 22:30:51 2021
+
+@author: Gaëlle Greiveldinger
+"""
+
+def moto():
+    return "Coding for our patients"
