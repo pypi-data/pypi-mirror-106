22cb0f090ac7e8d6be60bf979a26419330d957ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:53 +0000

    Add package hwhpykit/1.0.8/hwhpykit-1.0.8-py3-none-any.whl

---
+++ packages/hwhpykit/hwhpykit-1.0.8-py3-none-any.whl/hwhpykit/mq/KakfaManager.py
@@ -0,0 +1,48 @@
+
+
+# -*- coding: utf-8 -*-
+
+from kafka import KafkaProducer, KafkaConsumer
+from kafka.errors import kafka_errors
+import traceback
+import json
+
+
+def producer_demo():
+    # 假设生产的消息为键值对（不是一定要键值对），且序列化方式为json
+    producer = KafkaProducer(
+        bootstrap_servers=['localhost:9092'],
+        key_serializer=lambda k: json.dumps(k).encode(),
+        value_serializer=lambda v: json.dumps(v).encode())
+    # 发送三条消息
+    for i in range(0, 3):
+        future = producer.send(
+            'test',
+            key='count_num',  # 同一个key值，会被送至同一个分区
+            value=str(i),
+            partition=1)  # 向分区1发送消息
+        print("send {}".format(str(i)))
+        try:
+            future.get(timeout=10) # 监控是否发送成功
+        except kafka_errors:  # 发送失败抛出kafka_errors
+            traceback.format_exc()
+
+
+def consumer_demo():
+    consumer = KafkaConsumer(
+        'test',
+        bootstrap_servers='localhost:9092',
+        group_id='test'
+    )
+    for message in consumer:
+        print("receive, key: {}, value: {}".format(
+            json.loads(message.key.decode()),
+            json.loads(message.value.decode())
+            )
+        )
+
+
+
+if __name__ == '__main__':
+    # producer_demo()
+    consumer_demo()
