cc62a71898e18434e8075879809d76208251b146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:37 +0000

    Add package mypy-boto3-iotdeviceadvisor/1.17.73/mypy-boto3-iotdeviceadvisor-1.17.73.tar.gz

---
+++ packages/mypy-boto3-iotdeviceadvisor/mypy-boto3-iotdeviceadvisor-1.17.73.tar.gz/mypy-boto3-iotdeviceadvisor-1.17.73/mypy_boto3_iotdeviceadvisor/literals.py
@@ -0,0 +1,30 @@
+"""
+Type annotations for iotdeviceadvisor service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_iotdeviceadvisor.literals import StatusType
+
+    data: StatusType = "CANCELED"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("StatusType", "SuiteRunStatusType")
+
+
+StatusType = Literal[
+    "CANCELED", "ERROR", "FAIL", "PASS", "PASS_WITH_WARNINGS", "PENDING", "RUNNING"
+]
+SuiteRunStatusType = Literal[
+    "CANCELED", "ERROR", "FAIL", "PASS", "PASS_WITH_WARNINGS", "PENDING", "RUNNING"
+]
