792c10f5edf66c20ef085e3f12340badd00c7455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:00 +0000

    Add package elara/0.5.0/elara-0.5.0.tar.gz

---
+++ packages/elara/elara-0.5.0.tar.gz/elara-0.5.0/elara/exceptions.py
@@ -0,0 +1,35 @@
+"""
+Copyright (c) 2021, Saurabh Pujari
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree.
+"""
+
+
+# Add all custom exception classes here
+
+
+class FileAccessError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"Error -> {self.message}"
+
+
+class FileKeyError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"Error -> {self.message}"
+
+
+class LoadChecksumError(Exception):
+    pass
+
+
+class LoadIncompatibleDB(Exception):
+    pass
