a029a8c58471342f46a892373fb9ca6b5e7474e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:11 +0000

    Add package postgresplus/0.0.3/postgresplus-0.0.3.tar.gz

---
+++ packages/postgresplus/postgresplus-0.0.3.tar.gz/postgresplus-0.0.3/postgresplus/main.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+class editor():
+    def __init__(self, db, user, password, host, port):
+        self.db = db
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.con = psycopg2.connect(database = db, user = user, password = password, host = host, port = port)
+
+    def create_table(self, tablename, **kwargs):
+        cur = self.con.cursor()
+        cur.execute(f"DROP TABLE IF EXISTS {tablename}")
+        cur.execute(f"CREATE TABLE {tablename} ()")
+        if kwargs != None:
+            for row in kwargs["rows"]:
+                cur.execute(f"ALTER TABLE {tablename} ADD COLUMN {row}")
+        self.con.commit()
+
+    def drop_table(self, tablename):
+        cur = self.con.cursor()
+        cur.execute(f"DROP TABLE IF EXISTS {tablename}")
+        self.con.commit()
+    
+    def run(self, command):
+        cur = self.con.cursor()
+        cur.execute(f"{command}")
+        self.con.commit()
+    
