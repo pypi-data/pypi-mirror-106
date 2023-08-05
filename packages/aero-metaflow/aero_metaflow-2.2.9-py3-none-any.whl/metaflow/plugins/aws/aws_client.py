e5996d057bd732067a90cbf95b0318b322282986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:46 +0000

    Add package aero-metaflow/2.2.9/aero_metaflow-2.2.9-py3-none-any.whl

---
+++ packages/aero-metaflow/aero_metaflow-2.2.9-py3-none-any.whl/metaflow/plugins/aws/aws_client.py
@@ -0,0 +1,17 @@
+import os
+
+def get_aws_client(module, with_error=False, params={}):
+    from metaflow.exception import MetaflowException
+    import requests
+    os.environ['AWS_PROFILE'] = 'aero'
+
+    try:
+        import boto3
+        from botocore.exceptions import ClientError
+    except (NameError, ImportError):
+        raise MetaflowException(
+            "Could not import module 'boto3'. Install boto3 first.")
+
+    if with_error:
+        return boto3.client(module, **params), ClientError
+    return boto3.client(module, **params)
