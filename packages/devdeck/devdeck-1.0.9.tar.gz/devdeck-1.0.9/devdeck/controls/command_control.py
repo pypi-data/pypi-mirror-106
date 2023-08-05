db006449197d2060f16736d04521d2f5bd5b87d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:35 +0000

    Add package devdeck/1.0.9/devdeck-1.0.9.tar.gz

---
+++ packages/devdeck/devdeck-1.0.9.tar.gz/devdeck-1.0.9/devdeck/controls/command_control.py
@@ -0,0 +1,14 @@
+import os
+from subprocess import Popen, DEVNULL
+
+from devdeck_core.controls.deck_control import DeckControl
+
+
+class CommandControl(DeckControl):
+    def initialize(self):
+        with self.deck_context() as context:
+            with context.renderer() as r:
+                r.image(os.path.expanduser(self.settings['icon'])).end()
+
+    def pressed(self):
+        Popen(self.settings['command'], stdout=DEVNULL, stderr=DEVNULL)
