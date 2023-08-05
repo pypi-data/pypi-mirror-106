a6b92cb194adbd3a87d10c5b7df16ab0d2ce80b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:13 +0000

    Add package capi-vis/2.1.0/capi-vis-2.1.0.tar.gz

---
+++ packages/capi-vis/capi-vis-2.1.0.tar.gz/capi-vis-2.1.0/capi/src/implementation/visibility_graphs/code/constants/constants.hpp
@@ -0,0 +1,15 @@
+//
+// Created by James.Balajan on 7/04/2021.
+//
+
+#ifndef CAPI_CONSTANTS_HPP
+#define CAPI_CONSTANTS_HPP
+
+#endif // CAPI_CONSTANTS_HPP
+
+static constexpr double EPSILON_TOLERANCE = 0.0000000000001;
+static constexpr double EPSILON_TOLERANCE_SQUARED = EPSILON_TOLERANCE * EPSILON_TOLERANCE;
+static constexpr double MAX_LONGITUDE = 180.0;
+static constexpr double MIN_LONGITUDE = -180.0;
+static constexpr double MAX_LATITUDE = 90.0;
+static constexpr double MIN_LATITUDE = -90.0;
