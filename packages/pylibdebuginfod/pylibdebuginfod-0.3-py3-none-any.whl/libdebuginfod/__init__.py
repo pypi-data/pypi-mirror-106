c4dc652b049942a7c8cec985d00bce6c9d5ab885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:46 +0000

    Add package pylibdebuginfod/0.3/pylibdebuginfod-0.3-py3-none-any.whl

---
+++ packages/pylibdebuginfod/pylibdebuginfod-0.3-py3-none-any.whl/libdebuginfod/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2021 Andreas Ziegler <andreas.ziegler@fau.de>
+#
+# SPDX-License-Identifier: MIT
+
+__version__ = '0.3'
+
+from libdebuginfod.debuginfod import DebugInfoD, ProgressFunction, \
+                                     get_buildid_from_path
+
+__all__ = ['DebugInfoD', 'ProgressFunction']
