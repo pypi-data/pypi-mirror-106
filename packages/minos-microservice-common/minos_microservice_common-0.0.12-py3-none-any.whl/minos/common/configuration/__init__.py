91713ae9631bb625e3771533ddcf421e3853f148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:12 +0000

    Add package minos-microservice-common/0.0.12/minos_microservice_common-0.0.12-py3-none-any.whl

---
+++ packages/minos-microservice-common/minos_microservice_common-0.0.12-py3-none-any.whl/minos/common/configuration/__init__.py
@@ -0,0 +1,20 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+
+from .config import (
+    BROKER,
+    COMMAND,
+    COMMANDS,
+    ENDPOINT,
+    EVENT,
+    EVENTS,
+    REST,
+    SERVICE,
+    MinosConfig,
+    MinosConfigAbstract,
+)
