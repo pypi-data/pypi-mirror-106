f5ed9004b1fc26959c4d80af71c92170733b249d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:36 +0000

    Add package Mathics3/2.2.0/Mathics3-2.2.0-py3.9.egg

---
+++ packages/Mathics3/Mathics3-2.2.0-py3.9.egg/mathics/autoload/settings.m
@@ -0,0 +1,19 @@
+(*****************************)
+(* Settings for Mathics Core *)
+(*****************************)
+
+Settings`$TraceGet::usage = "If this Boolean variable is set True, 'Get' traces the lines it reads that start a new expression";
+Settings`$TraceGet = False
+Unprotect[Settings`$TraceGet]
+
+
+Settings`$PreferredBackendMethod::usage = "Set this do whether to use mpmath, numpy or Sympy for numeric and symbolic constants and methods when there is a choice";
+Settings`$PreferredBackendMethod = "sympy"
+Unprotect[Settings`$PreferredBackendMethod]
+
+(* FIXME: remove this when all front-ends are adjusted to set this. *)
+(* Some packages like Feyncalc, test for whether a they are being used
+inside a notbook. *)
+System`$Notebooks::usage = "Set True if the Mathics is being used with a notebook-based front end.";
+System`$Notebooks = False;
+Unprotect[System`$Notebooks];
