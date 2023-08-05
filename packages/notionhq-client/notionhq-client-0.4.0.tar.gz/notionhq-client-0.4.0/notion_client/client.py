a6eb9603e745677814d4d844b4bf277ad073baff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:55 +0000

    Add package notionhq-client/0.4.0/notionhq-client-0.4.0.tar.gz

---
+++ packages/notionhq-client/notionhq-client-0.4.0.tar.gz/notionhq-client-0.4.0/notion_client/client.py
@@ -0,0 +1,160 @@
+import logging
+from abc import abstractclassmethod
+from dataclasses import dataclass
+from typing import Any, Dict, Optional, Union
+
+import httpx
+from httpx import Request, Response
+
+from notion_client.api_endpoints import (
+    BlocksEndpoint,
+    DatabasesEndpoint,
+    PagesEndpoint,
+    SearchEndpoint,
+    UsersEndpoint,
+)
+from notion_client.errors import (
+    APIErrorResponseBody,
+    APIResponseError,
+    HTTPResponseError,
+    RequestTimeoutError,
+    is_api_error_code,
+    is_timeout_error,
+)
+from notion_client.logging import make_console_logger
+from notion_client.typing import SyncAsync
+
+
+@dataclass
+class ClientOptions:
+    auth: Optional[str] = None
+    timeout_ms: int = 60_000
+    base_url: str = "https://api.notion.com"
+    log_level: int = logging.WARNING
+    logger: Optional[logging.Logger] = None
+    notion_version: str = "2021-05-13"
+
+
+class BaseClient:
+    def __init__(
+        self,
+        client: Union[httpx.Client, httpx.AsyncClient],
+        options: Optional[Union[Dict[str, Any], ClientOptions]] = None,
+        **kwargs: Any,
+    ) -> None:
+        if options is None:
+            options = ClientOptions(**kwargs)
+        elif isinstance(options, dict):
+            options = ClientOptions(**options)
+
+        self.logger = options.logger or make_console_logger()
+        self.logger.setLevel(options.log_level)
+
+        self.client = client
+        self.client.base_url = httpx.URL(options.base_url + "/v1/")
+        self.client.timeout = httpx.Timeout(timeout=options.timeout_ms / 1_000)
+        self.client.headers = httpx.Headers(
+            {
+                "Notion-Version": options.notion_version,
+                "User-Agent": "ramnes/notion-sdk-py@0.4.0",
+            }
+        )
+        if options.auth:
+            self.client.headers["Authorization"] = f"Bearer {options.auth}"
+
+        self.blocks = BlocksEndpoint(self)
+        self.databases = DatabasesEndpoint(self)
+        self.users = UsersEndpoint(self)
+        self.pages = PagesEndpoint(self)
+        self.search = SearchEndpoint(self)
+
+    def _build_request(
+        self,
+        method: str,
+        path: str,
+        query: Optional[Dict[Any, Any]] = None,
+        body: Optional[Dict[Any, Any]] = None,
+    ) -> Request:
+        self.logger.info(f"{method} {self.client.base_url}{path}")
+        return self.client.build_request(method, path, params=query, json=body)
+
+    def _parse_response(self, response: Response) -> Any:
+        try:
+            response.raise_for_status()
+        except httpx.TimeoutException as error:
+            if is_timeout_error(error):
+                raise RequestTimeoutError()
+            raise
+        except httpx.HTTPStatusError as error:
+            body = error.response.json()
+            code = body.get("code")
+            if is_api_error_code(code):
+                body = APIErrorResponseBody(code=code, message=body["message"])
+                raise APIResponseError(error.response, body)
+            raise HTTPResponseError(error.response)
+
+        return response.json()
+
+    @abstractclassmethod
+    def request(
+        self,
+        path: str,
+        method: str,
+        query: Optional[Dict[Any, Any]] = None,
+        body: Optional[Dict[Any, Any]] = None,
+        auth: Optional[str] = None,
+    ) -> SyncAsync[Any]:
+        pass
+
+
+class Client(BaseClient):
+    client: httpx.Client
+
+    def __init__(
+        self,
+        options: Optional[Union[Dict[Any, Any], ClientOptions]] = None,
+        client: Optional[httpx.Client] = None,
+        **kwargs: Any,
+    ) -> None:
+        if client is None:
+            client = httpx.Client()
+        super().__init__(client, options, **kwargs)
+
+    def request(
+        self,
+        path: str,
+        method: str,
+        query: Optional[Dict[Any, Any]] = None,
+        body: Optional[Dict[Any, Any]] = None,
+        auth: Optional[str] = None,
+    ) -> Any:
+        request = self._build_request(method, path, query, body)
+        response = self.client.send(request)
+        return self._parse_response(response)
+
+
+class AsyncClient(BaseClient):
+    client: httpx.AsyncClient
+
+    def __init__(
+        self,
+        options: Optional[Union[Dict[str, Any], ClientOptions]] = None,
+        client: Optional[httpx.AsyncClient] = None,
+        **kwargs: Any,
+    ) -> None:
+        if client is None:
+            client = httpx.AsyncClient()
+        super().__init__(client, options, **kwargs)
+
+    async def request(
+        self,
+        path: str,
+        method: str,
+        query: Optional[Dict[Any, Any]] = None,
+        body: Optional[Dict[Any, Any]] = None,
+        auth: Optional[str] = None,
+    ) -> Any:
+        request = self._build_request(method, path, query, body)
+        async with self.client as client:
+            response = await client.send(request)
+        return self._parse_response(response)
---
+++ packages/notionhq-client/notionhq-client-0.4.0.tar.gz/notionhq-client-0.4.0/notion_client/errors.py
@@ -0,0 +1,106 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+import httpx
+
+
+class RequestTimeoutError(Exception):
+    code = "notionhq_client_request_timeout"
+
+    def __init__(self, message: str = "Request to Notion API has timed out") -> None:
+        super().__init__(message)
+        self.name = "RequestTimeoutError"
+
+    @staticmethod
+    def is_request_timeout_error(e: Exception) -> bool:
+        return (
+            isinstance(e, RequestTimeoutError)
+            and hasattr(e, "code")
+            and e.code == RequestTimeoutError.code
+        )
+
+
+class HTTPResponseError(Exception):
+    code: str = "notionhq_client_response_error"
+    status: int
+    headers: httpx.Headers
+    body: str
+
+    def __init__(self, response: httpx.Response, message: Optional[str] = None) -> None:
+        super().__init__(
+            message
+            or f"Request to Notion API failed with status: {response.status_code}"
+        )
+        self.status = response.status_code
+        self.headers = response.headers
+        self.body = response.text
+
+    def is_http_response_error(e: Exception) -> bool:
+        return (
+            isinstance(e, HTTPResponseError)
+            and hasattr(e, "code")
+            and e.code == HTTPResponseError.code
+        )
+
+
+class APIErrorCode(str, Enum):
+    Unauthorized = "unauthorized"
+    RestrictedResource = "restricted_resource"
+    ObjectNotFound = "object_not_found"
+    RateLimited = "rate_limited"
+    InvalidJSON = "invalid_json"
+    InvalidRequestURL = "invalid_request_url"
+    InvalidRequest = "invalid_request"
+    ValidationError = "validation_error"
+    ConflictError = "conflict_error"
+    InternalServerError = "internal_server_error"
+    ServiceUnavailable = "service_unavailable"
+
+
+@dataclass
+class APIErrorResponseBody:
+    code: APIErrorCode
+    message: str
+
+
+class APIResponseError(HTTPResponseError):
+    code: APIErrorCode
+
+    def __init__(self, response: httpx.Response, body: APIErrorResponseBody) -> None:
+        super().__init__(response, body.message)
+        self.code = body.code
+
+    @staticmethod
+    def is_api_response_error(e: Exception) -> bool:
+        return (
+            isinstance(e, APIResponseError)
+            and hasattr(e, "code")
+            and is_api_error_code(e.code)
+        )
+
+
+# Type Guards
+
+
+def is_api_error_code(code: str) -> bool:
+    codes = [error_code.value for error_code in APIErrorCode]
+    return type(code) == str and code in codes
+
+
+def is_timeout_error(e: Exception) -> bool:
+    return (
+        isinstance(e, httpx.TimeoutException)
+        and hasattr(e, "request")
+        and isinstance(e.request, httpx.Request)
+    )
+
+
+def is_http_error(e: Exception) -> bool:
+    return (
+        isinstance(e, httpx.HTTPStatusError)
+        and hasattr(e, "request")
+        and hasattr(e, "response")
+        and isinstance(e.request, httpx.Request)
+        and isinstance(e.response, httpx.Response)
+    )
