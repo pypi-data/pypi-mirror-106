d8c31f8b71288b91fbfaefe314168c0477225062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:21 +0000

    Add package lincoln/0.0.2/lincoln-0.0.2-py3-none-any.whl

---
+++ packages/lincoln/lincoln-0.0.2-py3-none-any.whl/lincoln/__init__.py
@@ -0,0 +1,5 @@
+from .robotarm import RobotArm
+from .mobilerobot import MobileRobot
+
+from .visualize import Visualize
+from .inout import InOut
---
+++ packages/lincoln/lincoln-0.0.2-py3-none-any.whl/lincoln/mobilerobot.py
@@ -0,0 +1,3 @@
+class MobileRobot (object) :
+    def __init__ (self):
+        print('hi im a mobile robot')
