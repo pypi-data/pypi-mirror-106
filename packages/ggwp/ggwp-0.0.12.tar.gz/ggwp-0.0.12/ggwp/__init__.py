bf0554650df5053c7e74304414c38688ad81331a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:57 +0000

    Add package ggwp/0.0.12/ggwp-0.0.12.tar.gz

---
+++ packages/ggwp/ggwp-0.0.12.tar.gz/ggwp-0.0.12/ggwp/__init__.py
@@ -0,0 +1,8 @@
+# from folder.file import class
+from .DataModel import *
+from .EzCheck import *
+from .EzCohort import *
+from .EzCustomerMovement import *
+from .EzRFM import *
+from .EzBasketEvolving import *
+from .EzLog import *
