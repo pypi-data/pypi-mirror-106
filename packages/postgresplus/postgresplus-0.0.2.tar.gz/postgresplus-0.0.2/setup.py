96b00ded5f456e16c461ded9e45e8511561c5425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:08 +0000

    Add package postgresplus/0.0.2/postgresplus-0.0.2.tar.gz

---
+++ packages/postgresplus/postgresplus-0.0.2.tar.gz/postgresplus-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = {
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Education",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+}
+
+setup(
+    name="postgresplus",
+    version="0.0.2",
+    description="Psycopg2 wrapper.",
+    long_description=open("README.txt").read() + "\n\n" + open("CHANGELOG.txt").read(),
+    url="",
+    author="Connor McHarg",
+    author_email="",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="databases",
+    packages=find_packages(),
+    install_requires=["psycopg2"]
+)
