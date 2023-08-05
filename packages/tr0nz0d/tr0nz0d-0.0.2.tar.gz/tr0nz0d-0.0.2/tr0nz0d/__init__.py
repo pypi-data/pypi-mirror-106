66d85191870645aff8d0e396206c7d97b6141fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:36 +0000

    Add package tr0nz0d/0.0.2/tr0nz0d-0.0.2.tar.gz

---
+++ packages/tr0nz0d/tr0nz0d-0.0.2.tar.gz/tr0nz0d-0.0.2/tr0nz0d/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+TR0NZ0D Utils Library
+~~~~~~~~~~~~~~~~~~~
+
+Biblioteca criada para facilitar o acesso e utilização de ferramentas comuns.
+
+:copyright: (c) 2021 TR0NZ0D
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'tr0nz0d'
+__author__ = 'TR0NZ0D'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 TR0NZ0D'
+__version__ = '0.0.2'
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+from collections import namedtuple
+import logging
+
+# Imports
+
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=0, minor=0, micro=2, releaselevel='Production/Stable', serial=0)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
