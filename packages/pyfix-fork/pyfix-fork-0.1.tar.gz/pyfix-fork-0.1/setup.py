dff1f62208ae50cf67c28f6025768081bb4e9aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:45 +0000

    Add package pyfix-fork/0.1/pyfix-fork-0.1.tar.gz

---
+++ packages/pyfix-fork/pyfix-fork-0.1.tar.gz/pyfix-fork-0.1/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'pyfix',
+    'author': 'Tom Fewster',
+    'url': 'https://github.com/wannabegeek/PyFIX/',
+    'download_url': 'https://github.com/wannabegeek/PyFIX/',
+    'author_email': 'tom@wanabegeek.com',
+    'maintainer': 'Eddy Pronk',
+    'maintainer_email': 'epronk@muftor.com',
+    'version': '0.1',
+    'install_requires': [''],
+    'packages': ['pyfix', 'pyfix/FIX44'],
+    'scripts': [],
+    'name': 'pyfix-fork'
+}
+
+setup(**config)
