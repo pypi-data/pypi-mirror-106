d306bf0230fd84dff670fe5ab2bd6d6312e25953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:36 +0000

    Add package mkctest1/0.2/mkctest1-0.2.tar.gz

---
+++ packages/mkctest1/mkctest1-0.2.tar.gz/mkctest1-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='mkctest1',
+      version='0.2',
+      long_description = long_description,
+      long_description_content_type="text/markdown",
+      description='Taiwan No. 1',
+      url='http://github.com/storborg/funniest',
+      author='Michael Chen',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['mkctest1'],
+      zip_safe=False)
