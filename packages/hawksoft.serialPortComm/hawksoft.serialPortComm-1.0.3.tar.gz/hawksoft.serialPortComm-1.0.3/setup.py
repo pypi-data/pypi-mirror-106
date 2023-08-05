103180af76418d2effa03892c5769f75feded1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:24 +0000

    Add package hawksoft.serialPortComm/1.0.3/hawksoft.serialPortComm-1.0.3.tar.gz

---
+++ packages/hawksoft.serialPortComm/hawksoft.serialPortComm-1.0.3.tar.gz/hawksoft.serialPortComm-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='hawksoft.serialPortComm',
+      version='1.0.3',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # 多文件模块写法
+      author="xingyongkang",
+      author_email="xingyongkang@cqu.edu.cn",
+      description="Provides many useful command lines in python",
+      long_description=open('./README.md', encoding='utf-8').read(),
+      long_description_content_type = "text/markdown",
+      #long_description="http://gitee.comg/xingyongkang",
+      license="MIT",
+      url = "https://gitee.com/xingyongkang/serialportcomm",
+      include_package_data=True,
+      platforms="any",
+      #install_requires=['pyserial','pykonw'],
+      install_requires=['pykonw'],
+      keywords='serial port comm',
+       entry_points={
+          'console_scripts': [
+              'traffic=hawksoft.trafficSign:main'
+          ]
+      },
+)
