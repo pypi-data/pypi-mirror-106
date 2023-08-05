6cae4f8766ee6d60d8c525a1263b189eb3bf244a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:35 +0000

    Add package py-Ultroid/26.4b0/py_Ultroid-26.4b0-py3-none-any.whl

---
+++ packages/py-Ultroid/py_Ultroid-26.4b0-py3-none-any.whl/pyUltroid/functions/ytdl.py
@@ -0,0 +1,47 @@
+import os
+import time
+
+from telethon.tl.types import DocumentAttributeAudio
+from youtubesearchpython.__future__ import VideosSearch
+
+from .all import uploader
+
+# search youtube
+
+
+async def get_yt_link(query):
+    vid_ser = VideosSearch(query, limit=1)
+    res = await vid_ser.next()
+    results = res["result"]
+    for i in results:
+        link = i["link"]
+    return link
+
+
+async def download_yt(xx, event, link, ytd):
+    st = time.time()
+    info = ytd.extract_info(link, False)
+    duration = round(info["duration"] / 60)
+    title = info["title"]
+    singer = info["uploader"]
+    try:
+        ytd.download([link])
+    except Exception as e:
+        return await xx.edit(f"**ERROR**:\n`{e}`")
+    kk = os.path.join(f"{info['id']}.mp3")
+    res = await uploader(kk, kk, st, xx, "Uploading...")
+    await event.client.send_file(
+        event.chat_id,
+        file=res,
+        caption=title,
+        supports_streaming=True,
+        attributes=[
+            DocumentAttributeAudio(
+                duration=int(duration),
+                title=str(title),
+                performer=str(singer),
+            )
+        ],
+    )
+    os.remove(kk)
+    await xx.delete()
