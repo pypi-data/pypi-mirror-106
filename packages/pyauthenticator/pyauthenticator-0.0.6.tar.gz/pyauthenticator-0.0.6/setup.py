c603cfbee2d97cd81c64ce449ac365b8cecc7b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:05 +0000

    Add package pyauthenticator/0.0.6/pyauthenticator-0.0.6.tar.gz

---
+++ packages/pyauthenticator/pyauthenticator-0.0.6.tar.gz/pyauthenticator-0.0.6/setup.py
@@ -0,0 +1,29 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+
+setup(
+    name='pyauthenticator',
+    version=versioneer.get_version(),
+    description='Similar to the Google authenticator just written in python.',
+    url='https://github.com/jan-janssen/pyauthenticator',
+    author='Jan Janssen',
+    author_email='jan.janssen@outlook.com',
+    license='BSD',
+    packages=find_packages(exclude=["*tests*"]),
+    install_requires=[
+        'otpauth>=1.0.1',
+        'qrcode>=6.1',
+        'pyzbar>=0.1.8',
+        'pillow>=8.1.2'
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+    entry_points={
+        "console_scripts": [
+            'pyauthenticator=pyauthenticator.__main__:command_line_parser'
+        ]
+    }
+)
