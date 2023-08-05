f0ab3cfcaa621b00375768ccbafa0f835a82ece4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:34 +0000

    Add package udacity-test-package-noel/0.1/udacity_test_package_noel-0.1.tar.gz

---
+++ packages/udacity-test-package-noel/udacity_test_package_noel-0.1.tar.gz/udacity_test_package_noel-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacity_test_package_noel',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacity_test_package_noel'],
+      author = 'Noel Markham',
+      author_email = 'nmarkham96@gmail.com',
+      zip_safe=False)
