18082e311d30cdfc5a3a022c33c67f52ee43abc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:54 +0000

    Add package nvitop/0.3.0.2/nvitop-0.3.0.2-py3-none-any.whl

---
+++ packages/nvitop/nvitop-0.3.0.2-py3-none-any.whl/nvitop/core/utils.py
@@ -0,0 +1,81 @@
+# This file is part of nvitop, the interactive NVIDIA-GPU process viewer.
+# License: GNU GPL version 3.
+
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
+# pylint: disable=invalid-name
+
+import time
+
+
+__all__ = ['NA', 'NaType', 'bytes2human', 'timedelta2human', 'Snapshot']
+
+
+class NotApplicableType(str):
+    def __new__(cls):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super().__new__(cls, 'N/A')
+        return cls._instance
+
+
+# isinstance(NA, str)       -> True
+# NA == 'N/A'               -> True
+# NA is NotApplicableType() -> True (NotApplicableType is a singleton class)
+NaType = NotApplicableType
+NA = NotApplicable = NotApplicableType()
+
+
+def bytes2human(x):
+    if x is None or x == NA:
+        return NA
+
+    if not isinstance(x, int):
+        x = round(float(x))
+
+    if x < (1 << 10):
+        return '{}B'.format(x)
+    if x < (1 << 20):
+        return '{}KiB'.format(x >> 10)
+    return '{}MiB'.format(x >> 20)
+
+
+def timedelta2human(dt):
+    if dt is None or dt == NA:
+        return NA
+
+    if dt.days >= 4:
+        return '{:.1f} days'.format(dt.days + dt.seconds / 86400)
+
+    hours, seconds = divmod(86400 * dt.days + dt.seconds, 3600)
+    if hours > 0:
+        return '{:d}:{:02d}:{:02d}'.format(hours, *divmod(seconds, 60))
+    return '{:d}:{:02d}'.format(*divmod(seconds, 60))
+
+
+class Snapshot(object):
+    def __init__(self, real, **items):
+        self.real = real
+        self.timestamp = time.monotonic()
+        for key, value in items.items():
+            setattr(self, key, value)
+
+    def __str__(self):
+        keys = set(self.__dict__.keys())
+        keys.remove('real')
+        keys.remove('timestamp')
+        return '{}{}(\n    {}\n)'.format(
+            self.real.__class__.__name__, self.__class__.__name__,
+            ', \n    '.join('{}={!r}'.format(key, getattr(self, key)) for key in ['real', *sorted(keys)])
+        )
+
+    __repr__ = __str__
+
+    def __getattr__(self, name):
+        try:
+            return super().__getattr__(name)
+        except AttributeError:
+            attribute = getattr(self.real, name)
+            if callable(attribute):
+                attribute = attribute()
+
+            setattr(self, name, attribute)
+            return attribute
