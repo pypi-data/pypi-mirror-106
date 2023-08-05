d05496a4d33d90aee446c3df765a5b546f3bc098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:21 +0000

    Add package amlt/0.0.1/amlt-0.0.1.tar.gz

---
+++ packages/amlt/amlt-0.0.1.tar.gz/amlt-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+INSTALL_REQUIRES = [
+]
+
+setup(
+    name='amlt',
+    description='Reserved name for internal package',
+    author='Hannes Schulz',
+    email='hannes.schulz@microsoft.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    version="0.0.1",
+    install_requires=INSTALL_REQUIRES,
+)
