49d2a3bf2fc7b4eec3e11aedb5100af0b055a409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:21 +0000

    Add package M6UF4Cine/0.9/M6UF4Cine-0.9.tar.gz

---
+++ packages/M6UF4Cine/M6UF4Cine-0.9.tar.gz/M6UF4Cine-0.9/cine/Cine.py
@@ -0,0 +1,4 @@
+class Cine:
+    def __init__(self, pelicula, precio):
+        self.pelicula = pelicula
+        self.precio = precio
---
+++ packages/M6UF4Cine/M6UF4Cine-0.9.tar.gz/M6UF4Cine-0.9/cine/Pelicula.py
@@ -0,0 +1,6 @@
+class Pelicula:
+    def __init__(self, titulo, duracion, edadMinima, director):
+        self.titulo = titulo
+        self.duracion = duracion
+        self.edadMinima = edadMinima
+        self.director = director
