2efc032be501ecdc12fc0e17534d89d42d2d80d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:20 +0000

    Add package zxtouch/0.0.6/zxtouch-0.0.6.tar.gz

---
+++ packages/zxtouch/zxtouch-0.0.6.tar.gz/zxtouch-0.0.6/setup.py
@@ -0,0 +1,15 @@
+from os import path
+from setuptools import setup
+
+setup(
+    name='zxtouch',
+    version='0.0.6',
+    description='iOS Automation Framework iOS Touch Simulation Library',
+    url='https://github.com/xuan32546/IOS13-SimulateTouch/tree/0.0.6/layout/usr/lib/python3.7/site-packages/zxtouch',
+    author='xuan32546, marzzzello',
+    author_email='zxtouch@07f.de',
+    license='GPL-3.0',
+    packages=['zxtouch'],
+    python_requires='>=3.7',
+    zip_safe=False,
+)
