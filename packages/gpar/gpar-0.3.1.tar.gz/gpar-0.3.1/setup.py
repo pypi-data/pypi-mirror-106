d856c160e447dfe82c8fe2ee1846e33dd22bfe3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:29 +0000

    Add package gpar/0.3.1/gpar-0.3.1.tar.gz

---
+++ packages/gpar/gpar-0.3.1.tar.gz/gpar-0.3.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+requirements = [
+    "numpy>=1.16",
+    "torch",
+    "wbml>=0.3",
+    "plum-dispatch>=1",
+    "backends>=1",
+    "backends-matrix>=1",
+    "stheno>=1.1",
+    "varz>=0.6",
+]
+
+setup(
+    packages=find_packages(exclude=["docs"]),
+    python_requires=">=3.6",
+    install_requires=requirements,
+    include_package_data=True,
+)
