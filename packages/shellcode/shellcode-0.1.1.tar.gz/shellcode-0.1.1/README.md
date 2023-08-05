d56af8ae04e42df136069a494dcfdc978e43fbaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:40 +0000

    Add package shellcode/0.1.1/shellcode-0.1.1.tar.gz

---
+++ packages/shellcode/shellcode-0.1.1.tar.gz/shellcode-0.1.1/README.md
@@ -0,0 +1,17 @@
+# shellcode
+
+## Motivation
+
+In the course of pawning n00bs often the need arises to introspect some (generated) shellcode or check
+it for sanity.
+
+This tools does exactly that.
+
+## Usage
+```
+echo "<some shellcode>" | python3 -m shellcode 
+```
+
+## License
+
+This project is licensed under the GPL-3 license.
---
+++ packages/shellcode/shellcode-0.1.1.tar.gz/shellcode-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['shellcode']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['capstone>=4.0.2,<5.0.0', 'typer>=0.3.2,<0.4.0']
+
+setup_kwargs = {
+    'name': 'shellcode',
+    'version': '0.1.1',
+    'description': 'CLI to turn shellcode back to asm.',
+    'long_description': '# shellcode\n\n## Motivation\n\nIn the course of pawning n00bs often the need arises to introspect some (generated) shellcode or check\nit for sanity.\n\nThis tools does exactly that.\n\n## Usage\n```\necho "<some shellcode>" | python3 -m shellcode \n```\n\n## License\n\nThis project is licensed under the GPL-3 license.\n',
+    'author': '4thel00z',
+    'author_email': '4thel00z@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
