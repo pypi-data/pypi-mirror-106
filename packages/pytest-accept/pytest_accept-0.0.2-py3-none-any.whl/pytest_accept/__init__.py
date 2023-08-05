d94e56a364c2be87508382b1c8826eff0db3a333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:56 +0000

    Add package pytest-accept/0.0.2/pytest_accept-0.0.2-py3-none-any.whl

---
+++ packages/pytest-accept/pytest_accept-0.0.2-py3-none-any.whl/pytest_accept/__init__.py
@@ -0,0 +1,14 @@
+from .doctest_plugin import (
+    pytest_runtest_makereport,
+    pytest_sessionfinish,
+    pytest_addoption,
+    pytest_configure,
+)
+
+
+__all__ = [
+    pytest_runtest_makereport,
+    pytest_sessionfinish,
+    pytest_addoption,
+    pytest_configure,
+]
