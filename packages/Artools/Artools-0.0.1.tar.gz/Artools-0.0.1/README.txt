ee8f473be4e4525c3fd14ba5f6e81bff3fc9631f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:33 +0000

    Add package Artools/0.0.1/Artools-0.0.1.tar.gz

---
+++ packages/Artools/Artools-0.0.1.tar.gz/Artools-0.0.1/README.txt
@@ -0,0 +1,2 @@
+    Data_sub : Define a box between lon_min lon_max lat_min and lat_max and 
+    extract the data in the box and drop everything else
---
+++ packages/Artools/Artools-0.0.1.tar.gz/Artools-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Artools',
+    packages=find_packages(),
+    version='0.0.1',
+    description='My first Python library',
+    author='Arthur Prigent',
+    author_email='artprigent@gmail.com',
+    license='MIT',
+    install_requires=['']
+)
