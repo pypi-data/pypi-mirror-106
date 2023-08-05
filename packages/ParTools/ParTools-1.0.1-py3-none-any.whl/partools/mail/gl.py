dcd1a6b89c0b34c1975c361fb906efe311fd1913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:14 +0000

    Add package ParTools/1.0.1/ParTools-1.0.1-py3-none-any.whl

---
+++ packages/ParTools/ParTools-1.0.1-py3-none-any.whl/partools/mail/gl.py
@@ -0,0 +1,13 @@
+RECIPIENTS = 'recipients.txt'
+GMAIL_HOST = 'smtp.gmail.com'
+GMAIL_PORT = '465'
+NO_AUTH_HOST = 'host.no_auth.com'
+INTERNAL_STR = '@ubs.com'
+TEST = False
+
+S_MISSING_CFI = """The email couldn't be sent because the confidential file was not found.
+Before running the gmail or no_auth functions, you have to run the init_mail function and set a confidential.txt file.
+This file must be saved at the root path using the example provided in the initialised folder."
+See quickstart/mail for further guidance."""
+
+S_MISSING = "{} file missing ({}). See quickstart/mail for guidance."
