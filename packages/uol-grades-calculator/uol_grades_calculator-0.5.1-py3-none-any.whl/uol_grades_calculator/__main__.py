e63e363854e401f064bb654b9929a2e4acdc15cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package uol-grades-calculator/0.5.1/uol_grades_calculator-0.5.1-py3-none-any.whl

---
+++ packages/uol-grades-calculator/uol_grades_calculator-0.5.1-py3-none-any.whl/uol_grades_calculator/__main__.py
@@ -0,0 +1,9 @@
+"""
+Allows calling `python -m uol_grades_calculator` from the root of the project.
+"""
+
+# Local imports
+from uol_grades_calculator.cli import cli
+
+if __name__ == "__main__":
+    cli()
