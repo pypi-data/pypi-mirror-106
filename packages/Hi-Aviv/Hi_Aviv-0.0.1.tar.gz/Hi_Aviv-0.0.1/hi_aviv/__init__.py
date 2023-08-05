e3f4140ce3263b050b24d3f5b4f6a90098c72100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:11 +0000

    Add package Hi-Aviv/0.0.1/Hi_Aviv-0.0.1.tar.gz

---
+++ packages/Hi-Aviv/Hi_Aviv-0.0.1.tar.gz/Hi_Aviv-0.0.1/hi_aviv/__init__.py
@@ -0,0 +1,17 @@
+from main import *
+
+root(strat, "Dd")
+# צריך לעשות אימפורט ל-hi_aviv
+# from color class import hi_aviv
+# ואז hi_aviv נקודה (שם של צבע) וכו'
+
+
+# delete dist folder
+# run py setup.py sdist
+# make sure dist folder created with
+# dir
+# run py -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# enter username (Biton55)
+# enter pass (Idan05423)
+
+# To run it -> from color_printer import *
