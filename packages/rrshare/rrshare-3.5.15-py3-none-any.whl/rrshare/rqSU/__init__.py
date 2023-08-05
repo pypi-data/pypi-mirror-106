a8e87482142028204286a57686658bf73a5d9073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:20 +0000

    Add package rrshare/3.5.15/rrshare-3.5.15-py3-none-any.whl

---
+++ packages/rrshare/rrshare-3.5.15-py3-none-any.whl/rrshare/rqSU/__init__.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+
+
+#save stock_day, swl_day
+from rrshare.rqSU.save_tusharepro_pg import (
+                                            #rq_save_stock_day_pg,
+                                             rq_save_swl_day_pg,
+                                             rq_save_stock_list_pg,
+                                             rq_save_stock_day_adj_fillna_pg
+                                             )
+from rrshare.rqSU.save_industry_swl_list import rq_save_swl_industry_list_stock_belong
+
+
