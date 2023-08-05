9ae1777451bfd3bc8b5e0fd65edbf988a9d7cca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:39 +0000

    Add package HolmesV/2021.5.6a12/HolmesV-2021.5.6a12.tar.gz

---
+++ packages/HolmesV/HolmesV-2021.5.6a12.tar.gz/HolmesV-2021.5.6a12/mycroft/configuration/locations.py
@@ -0,0 +1,52 @@
+# Copyright 2018 Mycroft AI Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+from time import sleep
+from os.path import join, dirname, expanduser, exists
+from xdg import BaseDirectory as XDG
+
+DEFAULT_CONFIG = join(dirname(__file__), 'mycroft.conf')
+SYSTEM_CONFIG = os.environ.get('MYCROFT_SYSTEM_CONFIG',
+                               '/etc/mycroft/mycroft.conf')
+# Make sure we support the old location still
+# Deprecated and will be removed eventually
+OLD_USER_CONFIG = join(expanduser('~'), '.mycroft/mycroft.conf')
+USER_CONFIG = join(XDG.xdg_config_home, 'mycroft', 'mycroft.conf')
+
+REMOTE_CONFIG = "mycroft.ai"
+WEB_CONFIG_CACHE = join(XDG.xdg_config_home, 'mycroft', 'web_cache.json')
+
+
+def __ensure_folder_exists(path):
+    """ Make sure the directory for the specified path exists.
+
+        Arguments:
+            path (str): path to config file
+     """
+    directory = dirname(path)
+    if not exists(directory):
+        try:
+            os.makedirs(directory)
+        except:
+            sleep(0.2)
+            if not exists(directory):
+                try:
+                    os.makedirs(directory)
+                except Exception as e:
+                    from mycroft.util.log import LOG
+                    LOG.exception(e)
+
+
+__ensure_folder_exists(WEB_CONFIG_CACHE)
+__ensure_folder_exists(USER_CONFIG)
