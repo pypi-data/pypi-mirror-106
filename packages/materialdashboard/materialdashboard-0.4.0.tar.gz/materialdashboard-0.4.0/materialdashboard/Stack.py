8253b6e4dbfdf9a44225856a39c6b95a3a5fe152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:31 +0000

    Add package materialdashboard/0.4.0/materialdashboard-0.4.0.tar.gz

---
+++ packages/materialdashboard/materialdashboard-0.4.0.tar.gz/materialdashboard-0.4.0/materialdashboard/Stack.py
@@ -0,0 +1,44 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class Stack(Component):
+    """A Stack component.
+Material-UI Stack.
+
+Keyword arguments:
+- children (a list of or a singular dash component, string or number; optional): The content of the component.
+- typography (string; optional): The **`typography`** property  is shorthand for the CSS properties **`font-family`**, **`font-weight`**, **`font-size`**, **`line-height`**, **`letter-spacing`** and **`text-transform``**.
+It takes the values defined under `theme.typography` and spreads them on the element.
+
+**Initial value**: `0`
+
+| Chrome | Firefox | Safari |  Edge  |  IE   |
+| :----: | :-----: | :----: | :----: | :---: |
+| **2**  |  **1**  | **1**  | **12** | **5.5** |
+- divider (boolean | number | string | dict | list; optional): Add an element between each child.
+- className (string; optional)
+- id (string; optional): The ID of this component, used to identify dash components in callbacks.
+The ID needs to be unique across all of the components in an app.
+- classes (dict; optional): Override or extend the styles applied to the component.
+- style (dict; optional)"""
+    @_explicitize_args
+    def __init__(self, children=None, typography=Component.UNDEFINED, divider=Component.UNDEFINED, className=Component.UNDEFINED, id=Component.UNDEFINED, classes=Component.UNDEFINED, style=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'typography', 'divider', 'className', 'id', 'classes', 'style']
+        self._type = 'Stack'
+        self._namespace = 'materialdashboard'
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['children', 'typography', 'divider', 'className', 'id', 'classes', 'style']
+        self.available_wildcard_properties =            []
+
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        for k in []:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(Stack, self).__init__(children=children, **args)
