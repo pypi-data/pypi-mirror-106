d8ecec51c521c3745a48a8d2d418346995463eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:39 +0000

    Add package APIFlask/0.6.1/APIFlask-0.6.1.tar.gz

---
+++ packages/APIFlask/APIFlask-0.6.1.tar.gz/APIFlask-0.6.1/apiflask/__init__.py
@@ -0,0 +1,27 @@
+# flake8: noqa
+from marshmallow import pre_load as before_load
+from marshmallow import post_load as after_load
+from marshmallow import pre_dump as before_dump
+from marshmallow import post_dump as after_dump
+from marshmallow import validates as validate
+from marshmallow import validates_schema as validate_schema
+from marshmallow import ValidationError as ValidationError
+
+from . import fields as fields
+from . import validators as validators
+from .app import APIFlask as APIFlask
+from .blueprint import APIBlueprint as APIBlueprint
+from .decorators import auth_required as auth_required
+from .decorators import doc as doc
+from .decorators import input as input
+from .decorators import output as output
+from .exceptions import abort as abort
+from .exceptions import HTTPError as HTTPError
+from .schemas import Schema as Schema
+from .schemas import PaginationSchema as PaginationSchema
+from .security import HTTPBasicAuth as HTTPBasicAuth
+from .security import HTTPTokenAuth as HTTPTokenAuth
+from .helpers import get_reason_phrase as get_reason_phrase
+from .helpers import pagination_builder as pagination_builder
+
+__version__ = '0.6.1'
---
+++ packages/APIFlask/APIFlask-0.6.1.tar.gz/APIFlask-0.6.1/tests/conftest.py
@@ -0,0 +1,22 @@
+import os
+import pytest
+
+from apiflask import APIFlask
+
+
+@pytest.fixture
+def app():
+    app = APIFlask(__name__)
+    return app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def test_apps(monkeypatch):
+    monkeypatch.syspath_prepend(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_apps'))
+    )
