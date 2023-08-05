5eeaeb03bc5ab4d03d9f5fea0447688202d3cc1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:10 +0000

    Add package recoverpy/1.0.4/recoverpy-1.0.4-py3-none-any.whl

---
+++ packages/recoverpy/recoverpy-1.0.4-py3-none-any.whl/recoverpy/__init__.py
@@ -0,0 +1,52 @@
+import os
+import logging
+import yaml
+
+import recoverpy.errors as ERRORS
+import recoverpy.window_handler as WINDOW_HANDLER
+import recoverpy.logger as LOGGER
+import recoverpy.saver as SAVER
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def verify_terminal_conf():
+    """Fix for outdated terminals not compatible with curses."""
+
+    term = os.environ["TERM"]
+
+    if term != "xterm-256color":
+        os.environ["TERM"] = "xterm-256color"
+
+
+def parse_configuration():
+    """Sets logging and saving parameters based on yaml conf file."""
+
+    with open(os.path.join(os.path.dirname(__file__), "config.yaml")) as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+
+    if config["save_directory"] == "":
+        raise ERRORS.NoSavePath
+    elif not os.path.isdir(config["save_directory"]):
+        raise ERRORS.InvalidSavePath
+    else:
+        SAVER.set_save_path(config["save_directory"])
+
+    if config["enable_logging"]:
+        LOGGER.enable_logging()
+    else:
+        return
+
+    if config["log_directory"] == "":
+        LOGGER.disable_logging()
+    elif not os.path.isdir(config["log_directory"]):
+        raise ERRORS.InvalidLogPath
+    else:
+        LOGGER.set_log_file_path(config["log_directory"])
+
+
+def main():
+    verify_terminal_conf()
+    parse_configuration()
+
+    WINDOW_HANDLER.open_parameters_menu()
