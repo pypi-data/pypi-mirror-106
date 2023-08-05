ad6baec6b5716aa704eb54f8c2d0f9e47efc26f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:42 +0000

    Add package yt-dlg/1.5.1/yt_dlg-1.5.1-py3-none-any.whl

---
+++ packages/yt-dlg/yt_dlg-1.5.1-py3-none-any.whl/youtube_dl_gui/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from .info import (
+    __author__,
+    __appname__,
+    __packagename__,
+    __contact__,
+    __license__,
+    __projecturl__,
+    __licensefull__,
+    __description__,
+    __descriptionfull__,
+)
+from .version import __version__
