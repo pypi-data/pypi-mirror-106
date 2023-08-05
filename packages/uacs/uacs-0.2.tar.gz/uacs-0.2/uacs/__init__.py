830aaaa9535c2e1a4e8b1dd8c242301d8cab8a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:23 +0000

    Add package uacs/0.2/uacs-0.2.tar.gz

---
+++ packages/uacs/uacs-0.2.tar.gz/uacs-0.2/uacs/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import function
+import time
+from uacs.uacs import *
+running = True
+
+
+def run_daemon():
+    print("Starting...")
+    function.restore(True)
+    print("Started !")
+    while running:
+        time.sleep(30)
+        print("New Verification")
+        function.check_service(True)
+
+    print("Stopped")
+
+
+if __name__ == '__main__':
+    run_daemon()
