1b810004f0260cb07da5dcddeec32b56c28fd8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:25 +0000

    Add package fzypy/0.0.2/fzypy-0.0.2-cp36-cp36m-macosx_10_14_x86_64.whl

---
+++ packages/fzypy/fzypy-0.0.2-cp36-cp36m-macosx_10_14_x86_64.whl/fzy/__init__.py
@@ -0,0 +1,11 @@
+from fzy.choices import Choice
+from fzy.search import search
+from fzy.match import match_bytes, match
+from fzy.__about__ import __version__
+
+__all__ = [
+    "search",
+    "Choice",
+    "match_bytes",
+    "match",
+]
