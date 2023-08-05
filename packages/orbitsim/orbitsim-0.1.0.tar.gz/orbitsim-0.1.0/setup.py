c97a7b4bf543057febf50351fcbfb8292b42d45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:44 +0000

    Add package orbitsim/0.1.0/orbitsim-0.1.0.tar.gz

---
+++ packages/orbitsim/orbitsim-0.1.0.tar.gz/orbitsim-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+setup(
+    name='orbitsim',
+    version='0.1.0',
+    description='3D orbit simulation visualisation tool',
+    long_description=readme,
+    author='Alexander Minchin',
+    author_email='alexander.w.minchin@gmail.com',
+    url='https://github.com/alexander-minchin/orbitsim',
+    license=license,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={'':'src'},
+    packages=find_packages(where="src",exclude=('tests', 'docs')),
+    install_requires=requirements,
+    python_requires=">=3.6"
+)
