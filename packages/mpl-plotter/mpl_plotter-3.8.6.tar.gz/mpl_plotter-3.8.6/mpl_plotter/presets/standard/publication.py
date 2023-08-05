3547f2742238d88bdc264670da02b32175a2bdd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:37 +0000

    Add package mpl-plotter/3.8.6/mpl_plotter-3.8.6.tar.gz

---
+++ packages/mpl-plotter/mpl_plotter-3.8.6.tar.gz/mpl_plotter-3.8.6/mpl_plotter/presets/standard/publication.py
@@ -0,0 +1,23 @@
+preset = {
+## Figure, axes
+"figsize" : (6, 6),
+"aspect" : 1,
+"scale": None,
+## Spines
+"spines_removed" : (0, 0, 1, 1),
+## Pads
+"demo_pad_plot" : True,
+## Labels
+"x_label_size" : 20,
+"y_label_size" : 20,
+## Ticks
+"x_tick_number" : 3,
+"y_tick_number" : 3,
+## Tick labels
+"x_tick_label_size" : 15,
+"y_tick_label_size" : 15,
+"fine_tick_locations" : True,
+"y_tick_ndecimals" : 3,
+## Legend
+"legend_size" : 15,
+}
