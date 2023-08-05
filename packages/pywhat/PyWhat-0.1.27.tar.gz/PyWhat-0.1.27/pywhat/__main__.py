bb352275ff18256824e90f43569d9e9fd5d79b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:30 +0000

    Add package pywhat/0.1.27/PyWhat-0.1.27.tar.gz

---
+++ packages/pywhat/PyWhat-0.1.27.tar.gz/PyWhat-0.1.27/pywhat/__main__.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+"""
+Name That Hash: Name the hash type.
+"""
+
+import sys
+
+if __name__ == "__main__":
+    major = sys.version_info[0]
+    minor = sys.version_info[1]
+
+    python_version = (
+        str(sys.version_info[0])
+        + "."
+        + str(sys.version_info[1])
+        + "."
+        + str(sys.version_info[2])
+    )
+
+    if major != 3 or (major == 3 and minor < 6):
+        print(
+            f"What requires Python 3.6+, you are using {python_version}. Please install a higher Python version."
+        )
+        sys.exit(1)
+
+    from PyWhat import what
+
+    what.main()
