6611ed3819363f9061b283b8e23b2ce0f3c802e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:12 +0000

    Add package minos-microservice-common/0.0.12/minos_microservice_common-0.0.12.tar.gz

---
+++ packages/minos-microservice-common/minos_microservice_common-0.0.12.tar.gz/minos_microservice_common-0.0.12/minos/common/__init__.py
@@ -0,0 +1,126 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+
+__version__ = "0.0.12"
+
+from .broker import (
+    MinosBaseBroker,
+)
+from .configuration import (
+    BROKER,
+    COMMAND,
+    COMMANDS,
+    ENDPOINT,
+    EVENT,
+    EVENTS,
+    REST,
+    SERVICE,
+    MinosConfig,
+    MinosConfigAbstract,
+)
+from .database import (
+    PostgreSqlMinosDatabase,
+)
+from .exceptions import (
+    EmptyMinosModelSequenceException,
+    MinosAttributeValidationException,
+    MinosConfigDefaultAlreadySetException,
+    MinosConfigException,
+    MinosException,
+    MinosImportException,
+    MinosMalformedAttributeException,
+    MinosMessageException,
+    MinosModelAttributeException,
+    MinosModelException,
+    MinosParseAttributeException,
+    MinosProtocolException,
+    MinosRepositoryAggregateNotFoundException,
+    MinosRepositoryDeletedAggregateException,
+    MinosRepositoryException,
+    MinosRepositoryManuallySetAggregateIdException,
+    MinosRepositoryManuallySetAggregateVersionException,
+    MinosRepositoryNonProvidedException,
+    MinosRepositoryUnknownActionException,
+    MinosReqAttributeException,
+    MinosTypeAttributeException,
+    MultiTypeMinosModelSequenceException,
+)
+from .importlib import (
+    classname,
+    import_module,
+)
+from .messages import (
+    MinosBaseRequest,
+    MinosBaseResponse,
+    MinosRequest,
+    MinosResponse,
+    MinosRPCBodyRequest,
+    MinosRPCHeadersRequest,
+    MinosRPCResponse,
+)
+from .meta import (
+    classproperty,
+    property_or_classproperty,
+    self_or_classmethod,
+)
+from .model import (
+    ARRAY,
+    BOOLEAN,
+    BYTES,
+    CUSTOM_TYPES,
+    DATE,
+    DECIMAL,
+    DOUBLE,
+    ENUM,
+    FIXED,
+    FLOAT,
+    INT,
+    LONG,
+    MAP,
+    NULL,
+    PYTHON_ARRAY_TYPES,
+    PYTHON_IMMUTABLE_TYPES,
+    PYTHON_LIST_TYPES,
+    PYTHON_NULL_TYPE,
+    PYTHON_TYPE_TO_AVRO,
+    STRING,
+    TIME_MILLIS,
+    TIMESTAMP_MILLIS,
+    UUID,
+    Aggregate,
+    Command,
+    CommandReply,
+    Decimal,
+    Enum,
+    Event,
+    Fixed,
+    MinosModel,
+    MissingSentinel,
+    ModelField,
+    ModelRef,
+)
+from .protocol import (
+    MinosAvroProtocol,
+    MinosAvroValuesDatabase,
+    MinosBinaryProtocol,
+    MinosJsonBinaryProtocol,
+)
+from .repository import (
+    MinosInMemoryRepository,
+    MinosRepository,
+    MinosRepositoryAction,
+    MinosRepositoryEntry,
+    PostgreSqlMinosRepository,
+)
+from .setup import (
+    MinosSetup,
+)
+from .storage import (
+    MinosStorage,
+    MinosStorageLmdb,
+)
