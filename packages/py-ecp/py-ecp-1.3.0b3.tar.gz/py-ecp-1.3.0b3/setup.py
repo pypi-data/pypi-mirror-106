b9dd269b0e1538dcec1d4cb3716bcfae155bd5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:14 +0000

    Add package py-ecp/1.3.0b3/py-ecp-1.3.0b3.tar.gz

---
+++ packages/py-ecp/py-ecp-1.3.0b3.tar.gz/py-ecp-1.3.0b3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-ecp",
+    version="1.3.0b3",
+    author="Conqu3red",
+    description="ECP programming language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ecp-lang/ecp-interpreter",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "tabulate",
+        "astor"
+    ]
+)
