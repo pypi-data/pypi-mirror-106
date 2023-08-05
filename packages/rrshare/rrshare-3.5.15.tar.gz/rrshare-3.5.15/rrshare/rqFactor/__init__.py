31529f9edbbbcf8de69c8245d21b5625518ccafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:20 +0000

    Add package rrshare/3.5.15/rrshare-3.5.15.tar.gz

---
+++ packages/rrshare/rrshare-3.5.15.tar.gz/rrshare-3.5.15/rrshare/rqFactor/__init__.py
@@ -0,0 +1,4 @@
+from rrshare.rqFactor.swl_rs_valuation_pg import update_swl_rs_valuation
+
+from rrshare.rqFactor.stock_RS_OH_MA import (stock_RT_HH_MA_pre, stock_RT_HH_MA_last, stock_RS_OH_MA_new, stock_select_PRS_new)
+
---
+++ packages/rrshare/rrshare-3.5.15.tar.gz/rrshare-3.5.15/rrshare/rqUpdate/update_swl_rs_valuation.py
@@ -0,0 +1,9 @@
+from rrshare.rqFactor import update_swl_rs_valuation
+
+
+def record_swl_rs_valuation():
+    update_swl_rs_valuation()
+
+
+if __name__ == '__main__':
+    record_swl_rs_valuation()
