072bff21bef757c0e805fcfb269a13b1f1d78d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:07 +0000

    Add package trochilidae/2.0.3.0/trochilidae-2.0.3.0-py3-none-any.whl

---
+++ packages/trochilidae/trochilidae-2.0.3.0-py3-none-any.whl/trochilidae/interoperable_get_arg_spec.py
@@ -0,0 +1,9 @@
+import inspect
+from sys import version_info
+
+__all__ = ["interoperable_get_arg_spec"]
+
+if version_info[0] >= 3:
+    interoperable_get_arg_spec = inspect.getfullargspec
+else:
+    interoperable_get_arg_spec = inspect.getargspec
