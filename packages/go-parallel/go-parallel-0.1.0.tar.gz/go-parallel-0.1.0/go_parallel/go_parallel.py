a489b89ae2d3dd24b370cba7d36de0fa9af6b34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:11 +0000

    Add package go-parallel/0.1.0/go-parallel-0.1.0.tar.gz

---
+++ packages/go-parallel/go-parallel-0.1.0.tar.gz/go-parallel-0.1.0/go_parallel/go_parallel.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from multiprocessing import Pipe, connection, Process
+from threading import Thread
+from typing import Union
+
+
+@dataclass
+class ParallelRunner:
+    _recv_conn: connection.Connection = None
+    _send_conn: connection.Connection = None
+    _result: object = None
+    _p: Union[Thread, Process] = None
+    _method: str = 'multiprocessing'
+
+    def __post_init__(self):
+        self._recv_conn, self._send_conn = Pipe(duplex=False)
+
+    def _executor(self, conn: connection.Connection, f, args):
+        res = f(*args)
+        conn.send(res)
+        conn.close()
+
+    def start(self, func, args):
+        self._p = Process(target=self._executor, args=(self._send_conn, func, args))
+        # self.p = Thread(target=self.executor, args=(self.send_conn, func, args))
+        self._p.start()
+
+    def _join(self):
+        self._p.join()
+
+    def get_result(self) -> object:
+        if self._result is None:
+            self._join()
+            self._result = self._recv_conn.recv()
+            self._recv_conn.close()
+        return self._result
+
+
+def gorun(func, *args) -> ParallelRunner:
+    """
+    Run the function 'func' with the parameters '*args'.
+    Example :
+
+        runner = gorun(f, 5)
+        result = runner.get_result()
+
+    :param func: the function that needs to be executed asynchronously.
+    :param args: arguments that need to be passed to `func`
+    :return: Executes `func` asynchronously and returns an object of ParallelRunner.
+    """
+    runner = ParallelRunner()
+    runner.start(func, args)
+    return runner
