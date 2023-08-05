c4331587b58a2674eeabb0d12e6d8a51cb9db5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:18 +0000

    Add package mapizy-property-analytics/0.1.2/mapizy_property_analytics-0.1.2.tar.gz

---
+++ packages/mapizy-property-analytics/mapizy_property_analytics-0.1.2.tar.gz/mapizy_property_analytics-0.1.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import os
+from setuptools import find_packages, setup
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+setup(
+    name='mapizy_property_analytics',
+    version='0.1.2',    
+    description='Python API for Mapizy Property Analytics',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://mapizy-studio.com',
+    author='Milad Ghorbani',
+    author_email='mi.ghorbani.g@gmail.com',
+    license='MIT',
+    packages=find_packages(include=['mapizy_property_analytics']),
+    install_requires=['requests',
+                      'datetime'                  
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
