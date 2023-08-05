e1f9d2efca840683399efdc049cd2e8d14bcf15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:24 +0000

    Add package spherical-dev/0.2.3/spherical_dev-0.2.3-py3-none-any.whl

---
+++ packages/spherical-dev/spherical_dev-0.2.3-py3-none-any.whl/spherical/dev/flake.py
@@ -0,0 +1,11 @@
+import invoke
+
+
+@invoke.task
+def flake(ctx):
+    ctx.run((
+        'flake8 --max-line-length=127'
+        ' --inline-quotes="single"'
+        ' --multiline-quotes="single"'
+        ' --docstring-quotes="double"'
+    ))
