d1db48020698f8e6fe1a1f5654fee8a1ef7ce0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:35 +0000

    Add package pyepipolicy/0.2/pyepipolicy-0.2.tar.gz

---
+++ packages/pyepipolicy/pyepipolicy-0.2.tar.gz/pyepipolicy-0.2/pyepipolicy/__init__.py
@@ -0,0 +1,3 @@
+from pyepipolicy.Config import Config
+from pyepipolicy.SimulationResults import SimulationResults
+from pyepipolicy.Session import Session
---
+++ packages/pyepipolicy/pyepipolicy-0.2.tar.gz/pyepipolicy-0.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name='pyepipolicy',
+    packages=['pyepipolicy'],
+    version='0.2',
+    license='MIT',
+    description='EpiPolicy Python Connector',
+    author='HudaLab NYUAD',
+    author_email='hudalab.nyuad@gmail.com',
+    url='https://github.com/EpiPolicy/pyepipolicy',
+    download_url='https://github.com/EpiPolicy/pyepipolicy/archive/refs/tags/0.2.tar.gz',
+    keywords=['Epidemic', 'EpidemicSimulator', 'EpiPolicy', 'Connector'],
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
