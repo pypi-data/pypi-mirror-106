e40b10e8722866d6fb18c24256c11ca0d21fea76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:33 +0000

    Add package django-simple-queue/0.1.3/django-simple-queue-0.1.3.tar.gz

---
+++ packages/django-simple-queue/django-simple-queue-0.1.3.tar.gz/django-simple-queue-0.1.3/django_simple_queue/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.shortcuts import reverse
+from django.utils.safestring import mark_safe
+from django_simple_queue.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            self.readonly_fields = [field.name for field in obj.__class__._meta.fields]
+        return self.readonly_fields
+
+    def status_page_link(self, obj):
+        return mark_safe("<a href='{}?task_id={}', target='_blank'>{}</a>".format(
+            reverse('django_simple_queue:task'),
+            obj.id,
+            obj.get_status_display(),
+        ))
+    status_page_link.short_description = "Status"
+
+    ordering = ['-created', ]
+    list_display = ('id', 'created', 'modified', 'task', 'status_page_link')
+    list_filter = ('status', 'created', )
+    search_fields = ('id', 'task', 'output')
