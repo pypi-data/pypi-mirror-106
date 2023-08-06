88ab5e870aa59df3095c1160de459cb8c714bee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:16 +0000

    Add package service-checker/0.1.0.dev4/service_checker-0.1.0.dev4.tar.gz

---
+++ packages/service-checker/service_checker-0.1.0.dev4.tar.gz/service_checker-0.1.0.dev4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="service_checker",  # Replace with your own username
+    version="0.1.0.dev4",
+    author="adesso mobile solutions GmbH",
+    author_email="it-operations@adesso-mobile.de",
+    description="Check services with commands and trigger commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adesso-mobile/service_checker",
+    project_urls={
+        "Bug Tracker": "https://github.com/adesso-mobile/service_checker/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pyyaml","prometheus_client"],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "service_checker=service_checker:run",
+        ],
+    },
+)
