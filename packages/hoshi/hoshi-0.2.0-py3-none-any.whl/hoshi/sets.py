4dea926bab833205d2e9d0ee9728d37c048e0bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:00 +0000

    Add package hoshi/0.2.0/hoshi-0.2.0-py3-none-any.whl

---
+++ packages/hoshi/hoshi-0.2.0-py3-none-any.whl/hoshi/sets.py
@@ -0,0 +1,13 @@
+indian_languages = [
+    'en_IN',  # English
+    'hi_IN',  # Hindi
+    'te_IN',  # Telugu
+    'ta_IN',  # Tamil
+    'bn_IN',  # Bengali
+    'pa_IN',  # Punjabi
+    'ur_IN',  # Urdu
+    'kn_IN',  # Kannada
+    'or_IN',  # Oriya
+    'gu_IN',  # Gujarati
+    'ml_IN',  # Malayalam
+]
