03bb093fa0517a1d36bb2089c5b9a27e60e9cec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:55 +0000

    Add package opics/0.1.8/opics-0.1.8-py2.py3-none-any.whl

---
+++ packages/opics/opics-0.1.8-py2.py3-none-any.whl/opics/__init__.py
@@ -0,0 +1,34 @@
+"""Top-level package for OPICS."""
+
+from opics import libraries
+from opics.network import Network
+from opics.utils import universal_sparam_filereader
+from opics.utils import LUT_processor
+from opics.utils import LUT_reader
+from opics.utils import netlistParser
+from opics.globals import c, f
+import opics.libraries.ebeam as ebeam
+
+__author__ = "Jaspreet Jhoja"
+__email__ = "Jaspreet@siepic.com"
+__version__ = "0.1.8"
+
+# initialize OPICS package
+
+name = "opics"
+
+__all__ = [
+    "Network",
+    "libraries",
+    "globals",
+    "ebeam",
+    "LUT_processor",
+    "LUT_reader",
+    "netlistParser",
+    "universal_sparam_filereader",
+    "c",
+    "f",
+]
+
+
+print("OPICS version", __version__)
