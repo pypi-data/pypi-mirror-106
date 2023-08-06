13f9dfb233367e94ba75fbe4d146d0aa6e7d681b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:32 +0000

    Add package evalout/0.0.1/evalout-0.0.1.tar.gz

---
+++ packages/evalout/evalout-0.0.1.tar.gz/evalout-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+setuptools.setup(
+      name='evalout',
+      version='0.0.1',
+      description='evalout',
+      url='https://gitee.com/AlwaysLee/cgf-ml_sdk-python.git',
+      author='cgf',
+      author_email='512070806@qq.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False
+)
