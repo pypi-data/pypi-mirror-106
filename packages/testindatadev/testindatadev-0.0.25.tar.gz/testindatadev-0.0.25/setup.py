8dd00f5f2c68d722b4ecef76daa40a9cd9f1bce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:29 +0000

    Add package testindatadev/0.0.25/testindatadev-0.0.25.tar.gz

---
+++ packages/testindatadev/testindatadev-0.0.25.tar.gz/testindatadev-0.0.25/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "testindatadev",
+    version = "0.0.25",
+    keywords = ["云测", "数据集","yuncedata", "testin", "testindatadev"],
+    description = "云测数据 数据集管理平台pythonSDK",
+    long_description = "README.md",
+    license = "MIT Licence",
+
+    url = "http://ai.testin.cn/",
+    author = "hide-in-code",
+    author_email = "hejinlong@testin.cn",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = [
+        "click",
+        "minio",
+        "qiniu",
+        "requests",
+    ]
+)
