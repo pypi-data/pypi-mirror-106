feae2db0e2d619831e9b75e929868486d72894fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:11 +0000

    Add package sui/0.1.11/sui-0.1.11-py3-none-any.whl

---
+++ packages/sui/sui-0.1.11-py3-none-any.whl/sui/dl/__init__.py
@@ -0,0 +1,10 @@
+"""sui.dl
+Deep learning algorithm implementations
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+from .afm import AFM
+# from .bert4rec import BERT4Rec
+from .pnn import PNN
+
+# __all__ = ['AFM', 'BERT4Rec', 'PNN']
+# __all__ = ['AFM', 'PNN']
---
+++ packages/sui/sui-0.1.11-py3-none-any.whl/sui/toolbox/__init__.py
@@ -0,0 +1,6 @@
+"""sui.toolbox
+Tool kits for data processing
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+from .top_k import top_k
+from .build_series_data import build_session, random_walk
