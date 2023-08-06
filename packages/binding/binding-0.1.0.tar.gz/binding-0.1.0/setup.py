031b16c481e3bf917212b7ebf9fb8d35df5d30f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:54 +0000

    Add package binding/0.1.0/binding-0.1.0.tar.gz

---
+++ packages/binding/binding-0.1.0.tar.gz/binding-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['binding']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'binding',
+    'version': '0.1.0',
+    'description': 'Bindable properties for Python',
+    'long_description': '# Binding\n\nBindable properties for Python.\n\n# Install\n\n```bash\npython3 -m pip install binding\n```\n\n# Usage\n\n```python\nimport binding\n\n...\n```\n\n# Dependencies\n\nTo archive this nice API we have to utilize `curse` from the [forbiddenfruit](https://pypi.org/project/forbiddenfruit/) package.\n',
+    'author': 'Zauberzeug GmbH',
+    'author_email': 'info@zauberzeug.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/zauberzeug/binding',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
