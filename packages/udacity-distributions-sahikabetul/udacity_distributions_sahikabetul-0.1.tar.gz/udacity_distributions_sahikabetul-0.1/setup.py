d8be3ccb64fbd257d09dff86d7e27fbe51028951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:12 +0000

    Add package udacity-distributions-sahikabetul/0.1/udacity_distributions_sahikabetul-0.1.tar.gz

---
+++ packages/udacity-distributions-sahikabetul/udacity_distributions_sahikabetul-0.1.tar.gz/udacity_distributions_sahikabetul-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacity_distributions_sahikabetul',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['udacity_distributions_sahikabetul'],
+      author = 'Sahika Betul',
+      author_email = 'sahika.betul@gmail.com',
+      zip_safe=False)
