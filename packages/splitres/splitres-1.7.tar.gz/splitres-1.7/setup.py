84ededda2a9332594f5aacfe793c566c8ebd04cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:36 +0000

    Add package splitres/1.7/splitres-1.7.tar.gz

---
+++ packages/splitres/splitres-1.7.tar.gz/splitres-1.7/setup.py
@@ -0,0 +1,16 @@
+"""File for build project in python-packet"""
+
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+setup(
+    name='splitres',
+    version='1.7',
+    packages=find_packages(),
+    description='Function calculates the min, max, average and sum of '
+                'power consumption value and writes this data to a file.',
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    url='https://gitwork.ru/barabass/splitres.git',
+    author='German Borovkov',
+    zip_safe=False,
+)
