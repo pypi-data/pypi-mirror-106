ad78bc5210da77da6ba1c750251f6a1b6145af61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:55 +0000

    Add package ggwp/0.0.38/ggwp-0.0.38.tar.gz

---
+++ packages/ggwp/ggwp-0.0.38.tar.gz/ggwp-0.0.38/ggwp/EzUtils/EzUtils.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+
+import configparser
+
+config_path  = './EzUtils/config.cfg'
+parser = configparser.ConfigParser()
+parser.read(config_path)
+version = parser['default']['version']
+
+# from ggwp.config.config import EzConfig
+
+# version = EzConfig().__version__
+
+class EzUtils:
+    def __init__(self):
+        self.__version__ = version
+    
+    def hello_world(self):
+        print("hello world!!")
+
+    def get_q1(self,x):
+        return x.quantile(.25)
+
+    def get_q3(self,x):
+        return x.quantile(.75)
