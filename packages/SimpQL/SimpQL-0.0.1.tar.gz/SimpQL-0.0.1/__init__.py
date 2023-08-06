d62072a61679485c4fe0ff70c9ab7fd582171d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:19 +0000

    Add package SimpQL/0.0.1/SimpQL-0.0.1.tar.gz

---
+++ packages/SimpQL/SimpQL-0.0.1.tar.gz/SimpQL-0.0.1/__init__.py
@@ -0,0 +1,33 @@
+import mysql.connector
+
+connection = None
+cursor = None
+
+def initalise(dbHost, dbUser, dbPassword, dbName):
+	"""
+		Initialises the connection to the database.
+	"""
+
+	global connection, cursor
+
+	connection = mysql.connector.connect(
+		host = dbHost,
+		user = dbUser,
+		password = dbPassword,
+		database = dbName
+	)
+
+	cursor = connection.cursor()
+
+def select_all(table, where = None):
+	"""
+		Selects all values in 'table'
+	"""
+	if where == None:
+		where = ""
+	else:
+		where = " WHERE " + where
+
+	print(f"SELECT * FROM {table}{where}")
+	cursor.execute(f"SELECT * FROM {table}{where}")
+	return cursor.fetchall()
