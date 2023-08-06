7e67d2ce5554f6b74793a12c5906718438691d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:38 +0000

    Add package floodgate/2.0.0/floodgate-2.0.0.tar.gz

---
+++ packages/floodgate/floodgate-2.0.0.tar.gz/floodgate-2.0.0/floodgate/flask/guard.py
@@ -0,0 +1,58 @@
+from functools import wraps
+from json import dumps
+
+from ..gates.catch_all import Gate
+from ..gates.units import SECONDS
+from ..gates.util import raise_package_error
+
+try:
+    from flask import Response, request
+except ImportError:
+    raise_package_error("flask")
+
+error = dumps(
+    {"error": "You have been rate limited, try again in %S% seconds"}
+).encode()
+
+
+def default_error_response(t):
+    x = f"{int(t)}"
+    return Response(
+        error.replace(b"%S%", x.encode()),
+        status=429,
+        headers={"x-rate-limit": "1", "x-time-left": x},
+    )
+
+
+def guard(
+    request_count=10,
+    *,
+    per=3,
+    units=SECONDS,
+    limit_response=default_error_response,
+    ip_resolver="heroku",
+    ban_time=0,
+):
+    time_interval = per * units
+
+    kwargs = {
+        "per": time_interval,
+        "units": units,
+        "request_count": request_count,
+        "ban_time": ban_time,
+        "ip_resolver": ip_resolver,
+    }
+
+    guard = Gate(**kwargs)
+
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            is_banned, time_left = guard.guard(request)
+            if is_banned:
+                return limit_response(time_left)
+            return fn(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
