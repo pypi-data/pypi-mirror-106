072474f478f0c3ae71d716b443adc71008cab1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:52 +0000

    Add package pangeo-xesmf/0.4.0/pangeo-xesmf-0.4.0.tar.gz

---
+++ packages/pangeo-xesmf/pangeo-xesmf-0.4.0.tar.gz/pangeo-xesmf-0.4.0/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import os
+
+DISTNAME = "pangeo-xesmf"
+DESCRIPTION = "Universal Regridder for Geospatial Data"
+AUTHOR = 'Jiawei Zhuang'
+AUTHOR_EMAIL = 'jiaweizhuang@g.harvard.edu'
+URL = 'https://github.com/pangeo-data/xESMF'
+LICENSE = 'MIT'
+PYTHON_REQUIRES = '>=3.6'
+USE_SCM_VERSION={'version_scheme': 'post-release', 'local_scheme': 'dirty-tag'}
+SETUP_REQUIRES=['setuptools_scm', 'setuptools>=30.3.0']
+
+# https://github.com/rtfd/readthedocs.org/issues/5512#issuecomment-475024373
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    INSTALL_REQUIRES = []
+else:
+    INSTALL_REQUIRES = ['esmpy>=8.0.0', 'xarray', 'numpy', 'scipy']
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Topic :: Scientific/Engineering',
+]
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name=DISTNAME,
+      license=LICENSE,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      classifiers=CLASSIFIERS,
+      description=DESCRIPTION,
+      long_description=readme(),
+      python_requires=PYTHON_REQUIRES,
+      install_requires=INSTALL_REQUIRES,
+      url=URL,
+      packages=find_packages(),
+      use_scm_version=USE_SCM_VERSION,
+      setup_requires=SETUP_REQUIRES)
