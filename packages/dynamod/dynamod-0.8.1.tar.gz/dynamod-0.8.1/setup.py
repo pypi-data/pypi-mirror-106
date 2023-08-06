c09fe369f6fb27950a4f8f2933833ddd85a725b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:14 +0000

    Add package dynamod/0.8.1/dynamod-0.8.1.tar.gz

---
+++ packages/dynamod/dynamod-0.8.1.tar.gz/dynamod-0.8.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dynamod',
+    version='0.8.1',
+    packages=['dynamod', 'dynamod.parser'],
+    url='https://github.com/andromed2/dynamod',
+    license='GNU GPLv3',
+    author='Andreas Neumann',
+    author_email='andromed@web.de',
+    description='Dynamod – a tool for modelling infectious diseases'
+)
