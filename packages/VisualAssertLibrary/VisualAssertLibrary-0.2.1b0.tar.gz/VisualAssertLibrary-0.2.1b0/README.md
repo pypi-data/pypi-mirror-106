d93d210f8b3e228d66431736c063fd6ad8663363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:04 +0000

    Add package VisualAssertLibrary/0.2.1b0/VisualAssertLibrary-0.2.1b0.tar.gz

---
+++ packages/VisualAssertLibrary/VisualAssertLibrary-0.2.1b0.tar.gz/VisualAssertLibrary-0.2.1b0/README.md
@@ -0,0 +1,4 @@
+# robotframework-visual-assert-library
+Robot Framework library for visual assert of web elements
+
+VisualAssert is a Robot Framework Library. It allows visually verify and assert web page or its elements renedered in browser in selenium tests.
---
+++ packages/VisualAssertLibrary/VisualAssertLibrary-0.2.1b0.tar.gz/VisualAssertLibrary-0.2.1b0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='VisualAssertLibrary',
+      version='0.2.1b',
+      description='Robot Framework Visual Assert Library',
+      packages=['VisualAssertLibrary'],
+      author='Dmytro Bondarchuk',
+      author_email='d.bondarchuk.spv@gmail.com',
+      zip_safe=False)
