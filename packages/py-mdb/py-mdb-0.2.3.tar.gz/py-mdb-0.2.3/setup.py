a940982e80dac2f12814c4d8c2997b43e48e687f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:40 +0000

    Add package py-mdb/0.2.3/py-mdb-0.2.3.tar.gz

---
+++ packages/py-mdb/py-mdb-0.2.3.tar.gz/py-mdb-0.2.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='py-mdb',
+    version='0.2.3',
+    author='Ryan Zembrodt',
+    author_email='ryan.zembrodt@gmail.com',
+    description='Package for parsing IMDb datasets and scraping web pages',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/zembrodt/pymdb',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6.0, <3.10.0',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    keywords = ['imdb', 'pymdb', 'movie', 'film', 'tv', 'series', 'show', 'episode', 
+    'database', 'db', 'dataset', 'web scrape', 'scrape', 'actor', 'actress', 'director',
+    'writer', 'person', 'title', 'company', 'rating'
+    ],
+    install_requires=[
+        'requests>=2.25.1',
+        'selectolax>=0.2.11'
+    ],
+)
