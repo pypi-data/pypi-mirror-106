b1f6ec8dc3ecc87fdf9064c47cac3e8dc09a50d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:39 +0000

    Add package simple-azure-utils/0.0.17/simple_azure_utils-0.0.17.tar.gz

---
+++ packages/simple-azure-utils/simple_azure_utils-0.0.17.tar.gz/simple_azure_utils-0.0.17/simple_azure_utils/__init__.py
@@ -0,0 +1,4 @@
+from .azure_data_lake_storage import AzureStorageAccount
+from .service_principal import ServicePrincipal
+from .app_configuration import AppConfiguration
+from .app_configuration import load_setting
