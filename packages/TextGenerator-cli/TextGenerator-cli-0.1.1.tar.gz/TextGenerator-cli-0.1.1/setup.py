03aa6a10ff84d76dee791fb58f2d941358b95255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:28 +0000

    Add package TextGenerator-cli/0.1.1/TextGenerator-cli-0.1.1.tar.gz

---
+++ packages/TextGenerator-cli/TextGenerator-cli-0.1.1.tar.gz/TextGenerator-cli-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+from TextGenerator import __version__
+
+setup(
+    name='TextGenerator-cli',
+    version=__version__,
+    description='マルコフ連鎖を使った文章自動生成プログラム+CLI',
+    description_content_type='',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/eggplants/TextGenerator-cli',
+    author='eggplants',
+    packages=find_packages(),
+    python_requires='>=3.5',
+    include_package_data=True,
+    license='MIT',
+    install_requires=[
+        'mecab-python3 @ '
+        'git+https://github.com/SamuraiT/mecab-python3.git#egg=mecab-python3',
+        'chardet'],
+    entry_points={
+        'console_scripts': [
+            'textgen=TextGenerator.main:main'
+        ]
+    }
+)
