a5756a94284ec495168bee6787ad70925a11122d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:48 +0000

    Add package ystools/0.1.2/ystools-0.1.2.tar.gz

---
+++ packages/ystools/ystools-0.1.2.tar.gz/ystools-0.1.2/setup.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ystools",
+    version="0.1.2",
+    url='https://github.com.cnpmjs.org/zzyyww/ys_tools.git',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=['qiniu','opencv-python']
+)
