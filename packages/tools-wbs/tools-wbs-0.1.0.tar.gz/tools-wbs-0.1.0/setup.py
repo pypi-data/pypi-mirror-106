64d54acc6c8cf580be54cb3870d9e75651512235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:28 +0000

    Add package tools-wbs/0.1.0/tools-wbs-0.1.0.tar.gz

---
+++ packages/tools-wbs/tools-wbs-0.1.0.tar.gz/tools-wbs-0.1.0/setup.py
@@ -0,0 +1,27 @@
+# -*- coding:UTF-8 -*-
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="tools-wbs",
+ version="0.1.0",
+ author="act",
+ author_email="wang.bensen@detvista.com",
+ license="Apache License",
+ url="https://gitee.com/OTLXM/packages-for-us",
+ packages=["tool"],
+ install_requires=["cx_Oracle <= 7.0.0 ", "traceback2 <= 1.4.0"],
+ classifiers=[
+        "Environment :: Web Environment",
+ "Intended Audience :: Developers",
+ "Operating System :: OS Independent",
+ "Topic :: Text Processing :: Indexing",
+ "Topic :: Utilities",
+ "Topic :: Internet",
+ "Topic :: Software Development :: Libraries :: Python Modules",
+ "Programming Language :: Python",
+ "Programming Language :: Python :: 3.8"
+ ],
+)
