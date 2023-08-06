38b10f708349e17adeb5c1ca368a08bf5d20b3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:55 +0000

    Add package anacreonlib/2.0.0a10/anacreonlib-2.0.0a10-py3-none-any.whl

---
+++ packages/anacreonlib/anacreonlib-2.0.0a10-py3-none-any.whl/anacreonlib/types/type_hints.py
@@ -0,0 +1,30 @@
+from enum import Enum
+from typing import Literal, Tuple
+
+TechLevel = Literal[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+Location = Tuple[float, float]
+"""
+A location is a tuple of 2 numbers
+    - x pos
+    - y pos
+"""
+
+Circle = Tuple[float, float, float]
+"""
+A Circle is a tuple of 3 numbers: 
+    - x pos of center
+    - y pos of center
+    - radius, in lightyears
+"""
+
+
+class BattleObjective(str, Enum):
+    INVASION = "invasion"
+    SPACE_SUPREMACY = "spaceSupremacy"
+
+class SiegeStatus(str, Enum):
+    ATTACK_FAILING = "attackFailing"
+    ATTACK_WINNING = "attackWinning"
+    DEFENSE_FALIING = "defenseFailing"
+    DEFENSE_WINNING = "defenseWinning"
