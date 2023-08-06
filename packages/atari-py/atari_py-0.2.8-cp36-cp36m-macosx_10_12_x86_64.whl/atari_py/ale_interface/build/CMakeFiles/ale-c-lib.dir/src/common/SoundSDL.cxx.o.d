38ef9dc089c6bc2d96a7b85c6dcde4d3dcab6466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:30 +0000

    Add package atari-py/0.2.8/atari_py-0.2.8-cp36-cp36m-macosx_10_12_x86_64.whl

---
+++ packages/atari-py/atari_py-0.2.8-cp36-cp36m-macosx_10_12_x86_64.whl/atari_py/ale_interface/build/CMakeFiles/ale-c-lib.dir/src/common/SoundSDL.cxx.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/ale-c-lib.dir/src/common/SoundSDL.cxx.o: \
+  /private/var/folders/bb/n7t3rs157850byt_jfdcq9k80000gn/T/pip-req-build-x7c1q8d2/build/lib.macosx-10.9-x86_64-3.6/atari_py/ale_interface/src/common/SoundSDL.cxx
---
+++ packages/atari-py/atari_py-0.2.8-cp36-cp36m-macosx_10_12_x86_64.whl/atari_py/ale_interface/build/CMakeFiles/ale-c-lib.dir/src/external/TinyMT/tinymt32.c.o.d
@@ -0,0 +1,19 @@
+CMakeFiles/ale-c-lib.dir/src/external/TinyMT/tinymt32.c.o: \
+  /private/var/folders/bb/n7t3rs157850byt_jfdcq9k80000gn/T/pip-req-build-x7c1q8d2/build/lib.macosx-10.9-x86_64-3.6/atari_py/ale_interface/src/external/TinyMT/tinymt32.c \
+  /private/var/folders/bb/n7t3rs157850byt_jfdcq9k80000gn/T/pip-req-build-x7c1q8d2/build/lib.macosx-10.9-x86_64-3.6/atari_py/ale_interface/src/external/TinyMT/tinymt32.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/8.1.0/include/stdint.h \
+  /usr/include/stdint.h /usr/include/sys/_types/_int8_t.h \
+  /usr/include/sys/_types/_int16_t.h /usr/include/sys/_types/_int32_t.h \
+  /usr/include/sys/_types/_int64_t.h /usr/include/_types/_uint8_t.h \
+  /usr/include/_types/_uint16_t.h /usr/include/_types/_uint32_t.h \
+  /usr/include/_types/_uint64_t.h /usr/include/sys/_types.h \
+  /usr/include/sys/cdefs.h /usr/include/sys/_symbol_aliasing.h \
+  /usr/include/sys/_posix_availability.h /usr/include/machine/_types.h \
+  /usr/include/i386/_types.h /usr/include/sys/_pthread/_pthread_types.h \
+  /usr/include/sys/_types/_intptr_t.h \
+  /usr/include/sys/_types/_uintptr_t.h /usr/include/_types/_intmax_t.h \
+  /usr/include/_types/_uintmax_t.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/8.1.0/include/inttypes.h \
+  /usr/include/inttypes.h /usr/include/Availability.h \
+  /usr/include/AvailabilityInternal.h /usr/include/_types.h \
+  /usr/include/sys/_types/_wchar_t.h
