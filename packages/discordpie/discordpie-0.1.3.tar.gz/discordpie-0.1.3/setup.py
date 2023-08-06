07c5bc69b42e3a0262198ad53669f4fb3d70b6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:49 +0000

    Add package discordpie/0.1.3/discordpie-0.1.3.tar.gz

---
+++ packages/discordpie/discordpie-0.1.3.tar.gz/discordpie-0.1.3/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.3'
+PACKAGE_NAME = 'discordpie'
+AUTHOR = 'Pycord_Devs'
+AUTHOR_EMAIL = 'aerotechnologyservice@gmail.com'
+URL = 'https://github.com/mfslink/pycord'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'An advance discord library'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'aiohttp'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
