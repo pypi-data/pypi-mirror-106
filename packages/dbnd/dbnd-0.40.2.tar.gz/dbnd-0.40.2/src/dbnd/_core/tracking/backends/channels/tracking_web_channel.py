638202866bc1ea8d4c7437bb9295508a536ae933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:26 +0000

    Add package dbnd/0.40.2/dbnd-0.40.2.tar.gz

---
+++ packages/dbnd/dbnd-0.40.2.tar.gz/dbnd-0.40.2/src/dbnd/_core/tracking/backends/channels/tracking_web_channel.py
@@ -0,0 +1,52 @@
+from dbnd._core.current import get_databand_context
+from dbnd._core.errors.base import (
+    DatabandAuthenticationError,
+    DatabandConnectionException,
+    DatabandUnauthorizedApiError,
+    TrackerPanicError,
+    TrackerRecoverError,
+)
+from dbnd._core.tracking.backends.channels.abstract_channel import TrackingChannel
+from dbnd._core.tracking.backends.channels.marshmallow_mixin import MarshmallowMixin
+from dbnd._core.utils.basics.memoized import cached
+
+
+class TrackingWebChannel(MarshmallowMixin, TrackingChannel):
+    """Json API client implementation."""
+
+    @property
+    @cached()
+    def client(self):
+        return get_databand_context().databand_api_client
+
+    def _handle(self, name, data):
+        try:
+            return self.client.api_request("tracking/%s" % name, data)
+
+        except DatabandConnectionException as e:
+            # connection problems are not recoverable for web tracker
+            raise TrackerPanicError(
+                "Failed to connect the tracking api",
+                e,
+                help_msg="As a temporal workaround you can disable WEB tracking using --disable-web-tracker",
+            )
+
+        except (DatabandAuthenticationError, DatabandUnauthorizedApiError) as e:
+            # authentication problems are not recoverable for web tracker
+            raise TrackerPanicError(
+                "Authentication error accrued",
+                e,
+                help_msg="Check your credential information",
+            )
+
+        except TypeError as e:
+            # probably problems with data trying to send - can recover
+            raise TrackerRecoverError("Failed to send the data", e)
+
+        # unknown exception are not handled
+
+    def is_ready(self):
+        return self.client.is_ready()
+
+    def __str__(self):
+        return "Web"
