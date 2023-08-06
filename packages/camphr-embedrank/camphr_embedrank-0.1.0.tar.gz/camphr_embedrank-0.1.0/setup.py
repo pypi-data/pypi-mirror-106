b3b6a1fad1a71cf84a05f13862d04ca0c86e8858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:11 +0000

    Add package camphr-embedrank/0.1.0/camphr_embedrank-0.1.0.tar.gz

---
+++ packages/camphr-embedrank/camphr_embedrank-0.1.0.tar.gz/camphr_embedrank-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['camphr_embedrank']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['camphr_core>=0.1.0,<0.2.0',
+ 'numpy>=1.20.3,<2.0.0',
+ 'scikit-learn>=0.22,<0.25',
+ 'typing-extensions>=3.7.4']
+
+setup_kwargs = {
+    'name': 'camphr-embedrank',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'tamuhey',
+    'author_email': 'tamuhey@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/PKSHATechnology-Research/camphr',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
