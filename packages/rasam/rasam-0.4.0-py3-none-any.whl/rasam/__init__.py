5c71f3b00679e2029edbb885db6628e638e8e1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:47 +0000

    Add package rasam/0.4.0/rasam-0.4.0-py3-none-any.whl

---
+++ packages/rasam/rasam-0.4.0-py3-none-any.whl/rasam/__init__.py
@@ -0,0 +1,4 @@
+from rasam.components.regex import RegexEntityExtractor
+from rasam.components.url import URLEntityExtractor
+from rasam.decorators import action
+from rasam.importers.placeholder import PlaceholderImporter
