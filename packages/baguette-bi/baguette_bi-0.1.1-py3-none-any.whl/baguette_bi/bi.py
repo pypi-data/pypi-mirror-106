feddd4b5dab55f15401e2fbff1010989e6a05b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:10 +0000

    Add package baguette-bi/0.1.1/baguette_bi-0.1.1-py3-none-any.whl

---
+++ packages/baguette-bi/baguette_bi-0.1.1-py3-none-any.whl/baguette_bi/bi.py
@@ -0,0 +1,5 @@
+from baguette_bi.core.chart import AltairChart
+from baguette_bi.core.connections.vega_datasets import VegaDatasetsConnection
+from baguette_bi.core.folder import Folder
+
+__all__ = ["VegaDatasetsConnection", "AltairChart", "Folder"]
