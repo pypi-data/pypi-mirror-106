0f82eee09726461765c7a446f09c9b24c5ea52c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:59 +0000

    Add package prog-edu-assistant-tools/0.3/prog_edu_assistant_tools-0.3-py3-none-any.whl

---
+++ packages/prog-edu-assistant-tools/prog_edu_assistant_tools-0.3-py3-none-any.whl/prog_edu_assistant_tools/notebook.py
@@ -0,0 +1,5 @@
+"""notebook --- a library for handling Jupyter Notebooks (.ipynb files).
+
+This python package provides functions for transforming master Jupyter notebooks
+into student notebooks and autograder tests.
+"""
