cb911cbe312944618348941c8cbb87b427979c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:45 +0000

    Add package generate-docs/2.0.0/generate_docs-2.0.0.tar.gz

---
+++ packages/generate-docs/generate_docs-2.0.0.tar.gz/generate_docs-2.0.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="generate_docs",
+    version="2.0.0",
+    author="The Renegade Coder",
+    author_email="jeremy.grifski@therenegadecoder.com",
+    description="A docs generation package for the Sample Programs repo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheRenegadeCoder/sample-programs-docs-generator",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": [
+            'wikig = generate_docs.generator:main_wiki',
+            'wikir = generate_docs.generator:main_readmes',
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+)
