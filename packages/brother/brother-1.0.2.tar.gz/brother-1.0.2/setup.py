b2dfb3b4f1d78075fbe3e1eb79530ab95a11b869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:32 +0000

    Add package brother/1.0.2/brother-1.0.2.tar.gz

---
+++ packages/brother/brother-1.0.2.tar.gz/brother-1.0.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="brother",
+    version="1.0.2",
+    author="Maciej Bieniek",
+    description="Python wrapper for getting data from Brother laser and inkjet \
+        printers via SNMP.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://github.com/bieniu/brother",
+    license="Apache-2.0 License",
+    packages=["brother"],
+    python_requires=">=3.6",
+    install_requires=list(val.strip() for val in open("requirements.txt")),
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=("pytest-runner"),
+    tests_require=list(val.strip() for val in open("requirements-test.txt")),
+)
