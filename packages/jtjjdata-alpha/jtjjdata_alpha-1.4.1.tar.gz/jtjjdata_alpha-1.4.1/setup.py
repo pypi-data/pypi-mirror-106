68b70052a185dfee7296977fd60e606be8eb43cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:30 +0000

    Add package jtjjdata-alpha/1.4.1/jtjjdata_alpha-1.4.1.tar.gz

---
+++ packages/jtjjdata-alpha/jtjjdata_alpha-1.4.1.tar.gz/jtjjdata_alpha-1.4.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='jtjjdata_alpha',
+      version='1.4.1',
+      packages=['DataLoader'],
+      maintainer='wcc',
+      maintainer_email='wangcc@csc.com.cn',
+      url="https://github.com/jimmy")
