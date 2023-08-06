f99dff4f6ee1dfb9826d245856865af6ee33a5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:15 +0000

    Add package atari-py/0.2.7/atari_py-0.2.7-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/atari-py/atari_py-0.2.7-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/atari_py/ale_interface/build/CMakeFiles/ale-c-lib.dir/src/common/SoundSDL.cxx.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/ale-c-lib.dir/src/common/SoundSDL.cxx.o: \
+ /tmp/pip-req-build-xzii1iyc/build/lib.linux-x86_64-3.6/atari_py/ale_interface/src/common/SoundSDL.cxx \
+ /usr/include/stdc-predef.h
