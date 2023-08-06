736ee20966a45397570b6b0091d9c5ced1d3dbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:19 +0000

    Add package dijkstra-algorithm/0.1.0/dijkstra_algorithm-0.1.0-py3-none-any.whl

---
+++ packages/dijkstra-algorithm/dijkstra_algorithm-0.1.0-py3-none-any.whl/dijkstra_algorithm/dijkstra_strategy.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+
+import heapq
+import math
+
+
+class UseDijkstraAlgorithm():
+    def __init__(self, dijkstra_algorithm: DijkstraAlgorithmStrategy) -> None:
+        self._dijkstra_algorithm = dijkstra_algorithm
+
+    @property
+    def dijkstra_algorithm(self) -> DijkstraAlgorithmStrategy:
+        return self._dijkstra_algorithm
+
+    @dijkstra_algorithm.setter
+    def dijkstra_algorithm(self, dijkstra_algorithm: DijkstraAlgorithmStrategy) -> None:
+        self._dijkstra_algorithm = dijkstra_algorithm
+
+    def search_shortest_path(self, matrix: List[List], vertex: int) -> List[int]:
+        return self._dijkstra_algorithm.search_shortest_path(matrix, vertex)
+
+
+class DijkstraAlgorithmStrategy(ABC):
+    @abstractmethod
+    def search_shortest_path(self, matrix: List[List], vertex: int) -> List[int]:
+        pass
+
+
+class NaiveDijkstraAlgorithmStrategy(DijkstraAlgorithmStrategy):
+    def _get_min_node(self, shortest_paths, checked_vertex):
+        vertex = False
+        max_weight = max(shortest_paths)
+
+        for idx_vtx, weight in enumerate(shortest_paths):
+
+            if weight < max_weight and idx_vtx not in checked_vertex:
+                max_weight = weight
+                vertex = idx_vtx
+
+        return vertex
+
+    def search_shortest_path(self, matrix: List[List], vertex: int) -> List[int]:
+        shortest_paths = [math.inf] * len(matrix)
+        shortest_paths[vertex] = 0
+        checked_vertex = {vertex}
+
+        while vertex is not False:
+
+            for i, weight in enumerate(matrix[vertex]):
+
+                if i not in checked_vertex and weight != 0:
+                    set_weight = shortest_paths[vertex] + weight
+
+                    if set_weight < shortest_paths[i]:
+                        shortest_paths[i] = set_weight
+
+            vertex = self._get_min_node(shortest_paths, checked_vertex)
+
+            if vertex is not False:
+                checked_vertex.add(vertex)
+
+        return shortest_paths
+
+
+class SetDijkstraAlgorithmStrategy(DijkstraAlgorithmStrategy):
+    def search_shortest_path(self, matrix: List[List], vertex: int) -> List[int]:
+        ver_count = len(matrix)
+        dist = [math.inf] * ver_count
+        dist[vertex] = 0
+        visited = set()
+        pq = [(0, vertex)]
+
+        while len(pq) != 0:
+            current_len, ver = heapq.heappop(pq)
+
+            if current_len > dist[ver] or ver in visited:
+                continue
+
+            for i in range(ver_count):
+                to_ver = i
+                current_len = matrix[ver][to_ver]
+
+                if dist[to_ver] > current_len + dist[ver] and current_len > 0:
+                    dist[to_ver] = current_len + dist[ver]
+                    heapq.heappush(pq, (dist[to_ver], to_ver))
+
+            visited.add(ver)
+
+        return dist
---
+++ packages/dijkstra-algorithm/dijkstra_algorithm-0.1.0-py3-none-any.whl/dijkstra_algorithm/work_with_data/generate_data.py
@@ -0,0 +1,45 @@
+from multiprocessing import Pool
+from random import randint
+
+from dijkstra_algorithm.config import TESTING_DATA
+
+
+def create_itarable_with_combination(start: int, end: int, step: int, count: int) -> list:
+    for i in range(start, end, step):
+        for j in range(1, count + 1):
+            yield i, j
+
+
+def create_adjacency_matrix(size: int, number: int):
+    with open(TESTING_DATA + f"{size}_{number}.txt", "w") as file:
+        file.write(str(size) + "\n")
+        adjacency_matrix = [[randint(0, 2 ** 5) for i in range(size)] for j in range(size)]
+
+        for i in range(size):
+            adjacency_matrix[i][i] = 0
+
+        for i in range(size):
+            for j in range(size):
+                will_be_num = randint(0, 10)
+                if not will_be_num:
+                    adjacency_matrix[i][j] = 0
+                adjacency_matrix[j][i] = adjacency_matrix[i][j]
+
+        for elements in range(size):
+            for element in range(len(adjacency_matrix[elements])):
+                separ = " "
+                if element == len(adjacency_matrix[elements]) - 1:
+                    separ = "\n"
+                file.write(str(adjacency_matrix[elements][element]) + separ)
+
+        file.write(str(randint(0, size - 1)))
+
+
+def generate_data_func(start: int, end: int, step: int, count: int):
+    with Pool() as pool:
+        pool.starmap(
+            create_adjacency_matrix,
+            list(create_itarable_with_combination(
+                start, end, step, count
+            ))
+        )
