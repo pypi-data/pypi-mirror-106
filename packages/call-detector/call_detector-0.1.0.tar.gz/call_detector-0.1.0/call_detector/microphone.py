de35331fc0ba74146d070d6e7d0c558805e43e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:08 +0000

    Add package call-detector/0.1.0/call_detector-0.1.0.tar.gz

---
+++ packages/call-detector/call_detector-0.1.0.tar.gz/call_detector-0.1.0/call_detector/microphone.py
@@ -0,0 +1,52 @@
+import asyncio
+import logging
+from copy import deepcopy
+
+import pulsectl_asyncio
+
+from .timer import timer
+
+
+class Microphone:
+    APP_NAME = "call_detector"
+
+    _LOGGER = logging.getLogger(f"{__name__}.{__qualname__}")
+
+    def __init__(self, queue):
+        self._users = {}
+        self._queue = queue
+
+    async def run(self):
+        self._LOGGER.info("Running.")
+
+        async with pulsectl_asyncio.PulseAsync(self.APP_NAME) as pulse:
+            await self._get_sources(pulse)
+            await self._publish()
+            asyncio.create_task(timer(self._publish, 60))
+
+            async for event in pulse.subscribe_events("source_output"):
+                if event.t not in ["new", "remove"]:
+                    continue
+
+                if event.t == "new":
+                    source = await pulse.source_output_info(event.index)
+                    self._users[event.index] = source.proplist["application.process.binary"]
+
+                if event.t == "remove":
+                    source = self._users[event.index]
+                    del self._users[event.index]
+
+                await self._publish()
+
+    async def _publish(self):
+        await self._queue.put(
+            {
+                "source": "microphone",
+                "apps": deepcopy(list(self._users.values())),
+            }
+        )
+
+    async def _get_sources(self, pulse):
+        sources = await pulse.source_output_list()
+        for source in sources:
+            self._users[source.index] = source.proplist["application.process.binary"]
