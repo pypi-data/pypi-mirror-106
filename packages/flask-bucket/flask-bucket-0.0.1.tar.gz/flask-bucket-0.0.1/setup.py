f29b794c4c5518c2d2a046b787b4b0f1000529eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:40 +0000

    Add package flask-bucket/0.0.1/flask-bucket-0.0.1.tar.gz

---
+++ packages/flask-bucket/flask-bucket-0.0.1.tar.gz/flask-bucket-0.0.1/setup.py
@@ -0,0 +1,38 @@
+from typing import Text
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='flask-bucket',
+    version='0.0.1',
+    url='https://github.com/owen-eternal/flask-bucket',
+    license='BSD',
+    author='Owen O. Phakade',
+    author_email='olwethuphakade89@gmail.com',
+    description='Create flask modules using a flask shell command',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=['flask_bucket'],
+    package_dir={'': 'flask-bucket'},
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    extras_require={"dev" : [
+        "pytest>=3.7"     
+    ]},
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
