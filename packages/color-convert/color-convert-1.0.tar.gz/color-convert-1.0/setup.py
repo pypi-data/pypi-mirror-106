4b7e6c439fc2a2f27e430556bf5e7c2a16f57184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:09 +0000

    Add package color-convert/1.0/color-convert-1.0.tar.gz

---
+++ packages/color-convert/color-convert-1.0.tar.gz/color-convert-1.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+package = {
+    "name": "color-convert",
+    "version": "1.0",
+    "description": "Color Conversion",
+    "keywords": [
+        "color",
+        "color-convert",
+        "convert"
+    ],
+    "homepage": "https://github.com/zhenzi0322/color-convert",
+    "author": {
+        "name": "Yu Zhen",
+        "email": "82131529@qq.com"
+    }
+}
+
+with open("README.rst", encoding="utf-8") as data:
+    long_description = data.read()
+
+setup(
+    name=package['name'],
+    version=package["version"],
+    url=package["homepage"],
+    description=package["description"],
+    long_description=long_description,
+    license='MIT License',
+    author=package["author"]["name"],
+    author_email=package["author"]["email"],
+    keywords=package["keywords"],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux'
+    ],
+    zip_safe=False
+)
