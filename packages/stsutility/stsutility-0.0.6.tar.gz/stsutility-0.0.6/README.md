e77796e05cbe7110da91aa91c5aa87dedea4a700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:40 +0000

    Add package stsutility/0.0.6/stsutility-0.0.6.tar.gz

---
+++ packages/stsutility/stsutility-0.0.6.tar.gz/stsutility-0.0.6/README.md
@@ -0,0 +1,10 @@
+# 636 Python Utility Package
+
+```bash
+python3 -m pip install stsutility
+```
+
+```python
+import stsutility as ut
+print( ut.divide( float( 3.3 ) , 5 , 7 ) )
+```
---
+++ packages/stsutility/stsutility-0.0.6.tar.gz/stsutility-0.0.6/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+VERSION='0.0.6'
+setuptools.setup(
+	name='stsutility',
+	version=VERSION,
+	author='636',
+	author_email='win.stitch.23@gmail.com',
+	description='636 Utility Package',
+	url='https://github.com/0187773933/stsutility',
+	packages=setuptools.find_packages(),
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+	    'Operating System :: OS Independent',
+	],
+	python_requires='>=3.6',
+	setup_requires=['numpy','pandas','Pint'],
+	install_requires=[
+		'json',
+		'pathlib',
+		'time',
+		'Pint',
+		'math',
+		'decimal',
+		'operator',
+		'pandas',
+		'numpy',
+		'pprint',
+	],
+)
+
