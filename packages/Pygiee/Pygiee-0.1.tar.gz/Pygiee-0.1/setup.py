bfb266beea60b62b4af7eb342a8519b822a65f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:56 +0000

    Add package Pygiee/0.1/Pygiee-0.1.tar.gz

---
+++ packages/Pygiee/Pygiee-0.1.tar.gz/Pygiee-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Pygiee',
+      version='0.1',
+      description='clinical trial information retriver',
+      url='',
+      author='Leo',
+      author_email='',
+      license='MIT',
+      packages=['Pygiee'],
+      zip_safe=False)
