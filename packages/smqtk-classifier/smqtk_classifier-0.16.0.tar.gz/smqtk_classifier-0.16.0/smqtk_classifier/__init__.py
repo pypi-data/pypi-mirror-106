1cc8007593b755d191518dd2a9382e04a91f1b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:40 +0000

    Add package smqtk-classifier/0.16.0/smqtk_classifier-0.16.0.tar.gz

---
+++ packages/smqtk-classifier/smqtk_classifier-0.16.0.tar.gz/smqtk_classifier-0.16.0/smqtk_classifier/__init__.py
@@ -0,0 +1,6 @@
+from .interfaces.classification_element import ClassificationElement  # noqa: F401
+from .interfaces.classifier import Classifier  # noqa: F401
+from .interfaces.supervised import SupervisedClassifier  # noqa: F401
+
+from .classification_element_factory import ClassificationElementFactory  # noqa: F401
+from .classifier_collection import ClassifierCollection  # noqa: F401
