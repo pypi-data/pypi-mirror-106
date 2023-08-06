0a625c28971fa548f267d892ac5269aec8d28d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:02 +0000

    Add package DeepXDE/0.11.1/DeepXDE-0.11.1-py3-none-any.whl

---
+++ packages/DeepXDE/DeepXDE-0.11.1-py3-none-any.whl/deepxde/maps/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+from .bionet import BiONet
+from .fnn import FNN
+from .mfnn import MfNN
+from .mfonet import MfONet
+from .opnn import OpNN
+from .pfnn import PFNN
+from .resnet import ResNet
+
+
+__all__ = ["BiONet", "FNN", "MfNN", "MfONet", "OpNN", "PFNN", "ResNet"]
