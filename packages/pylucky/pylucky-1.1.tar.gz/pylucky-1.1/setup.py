0717ab1147a6ce46905ef44907b2d46d5da8e38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:10 +0000

    Add package pylucky/1.1/pylucky-1.1.tar.gz

---
+++ packages/pylucky/pylucky-1.1.tar.gz/pylucky-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pylucky',# 需要打包的名字,即本模块要发布的名字
+    version='v1.1',#版本
+    description='A simple version of random', # 简要描述
+    py_modules=['pylucky.pylucky'],   #  需要打包的模块
+    author='DY_XiaoDong', # 作者名
+    author_email='xiaodong@indouyin.cn',   # 作者邮件
+    url='https://xiaodong.indouyin.cn/python/modules/pylucky/index.html', # 项目地址,一般是代码托管的网站
+    requires=['random'], # 依赖包,如果没有,可以不要
+    license='MIT'
+)
