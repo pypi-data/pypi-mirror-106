78377c22c79032a770cd200091e922f4a4a44c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:22 +0000

    Add package veides-agent-sdk/0.3.0/veides_agent_sdk-0.3.0-py3-none-any.whl

---
+++ packages/veides-agent-sdk/veides_agent_sdk-0.3.0-py3-none-any.whl/veides/sdk/agent/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.3.0'
+
+from veides.sdk.agent.client import AgentClient
+from veides.sdk.agent.base_client import BaseClient
+from veides.sdk.agent.properties import AgentProperties, ConnectionProperties
