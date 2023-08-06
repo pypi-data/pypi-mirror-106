62f042292faf68bd6430d711696b3ac8b7edf367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:42 +0000

    Add package newbie/1.0.0/newbie-1.0.0.tar.gz

---
+++ packages/newbie/newbie-1.0.0.tar.gz/newbie-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'newbie',
+    version = '1.0.0',
+    py_modules = ['newbie'],
+    author = 'tsunama',
+    author_email = 'tsunama@jamliton.com',
+    url = 'http://www.jamliton.com',
+    description = 'A simple printer of nested lists',
+)
