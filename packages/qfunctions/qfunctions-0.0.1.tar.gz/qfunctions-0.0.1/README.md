fa364176a95facccc612d112ed4228f0a8dc1529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:55 +0000

    Add package qfunctions/0.0.1/qfunctions-0.0.1.tar.gz

---
+++ packages/qfunctions/qfunctions-0.0.1.tar.gz/qfunctions-0.0.1/README.md
@@ -0,0 +1,2 @@
+# qfunctions
+Biblioteca que auxilia o manuseio de funções q-deformadas
---
+++ packages/qfunctions/qfunctions-0.0.1.tar.gz/qfunctions-0.0.1/qfunctions/__init__.py
@@ -0,0 +1,9 @@
+from .canonical import *
+from .trigonometry import *
+from .specials import *
+from .cred import *
+
+######## Numbers important's ########
+
+
+#_exp = functions.q_exp
