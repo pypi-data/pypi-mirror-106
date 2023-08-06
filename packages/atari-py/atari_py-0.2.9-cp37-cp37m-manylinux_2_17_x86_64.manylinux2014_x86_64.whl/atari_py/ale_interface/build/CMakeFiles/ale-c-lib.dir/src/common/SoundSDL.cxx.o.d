137472449744bd1a419c844b91e42cf1c42625d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:28 +0000

    Add package atari-py/0.2.9/atari_py-0.2.9-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/atari-py/atari_py-0.2.9-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/atari_py/ale_interface/build/CMakeFiles/ale-c-lib.dir/src/common/SoundSDL.cxx.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/ale-c-lib.dir/src/common/SoundSDL.cxx.o: \
+ /tmp/pip-req-build-zxpnt102/build/lib.linux-x86_64-3.7/atari_py/ale_interface/src/common/SoundSDL.cxx \
+ /usr/include/stdc-predef.h
---
+++ packages/atari-py/atari_py-0.2.9-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/atari_py/ale_interface/build/CMakeFiles/ale-c-lib.dir/src/emucore/SpeakJet.cxx.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/ale-c-lib.dir/src/emucore/SpeakJet.cxx.o: \
+ /tmp/pip-req-build-zxpnt102/build/lib.linux-x86_64-3.7/atari_py/ale_interface/src/emucore/SpeakJet.cxx \
+ /usr/include/stdc-predef.h
