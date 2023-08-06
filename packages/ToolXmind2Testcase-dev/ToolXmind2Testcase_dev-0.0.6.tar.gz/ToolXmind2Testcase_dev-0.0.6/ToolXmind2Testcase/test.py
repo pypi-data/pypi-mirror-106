29280945acee4e9fd12708ce054268036e3924a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:20 +0000

    Add package ToolXmind2Testcase-dev/0.0.6/ToolXmind2Testcase_dev-0.0.6.tar.gz

---
+++ packages/ToolXmind2Testcase-dev/ToolXmind2Testcase_dev-0.0.6.tar.gz/ToolXmind2Testcase_dev-0.0.6/ToolXmind2Testcase/test.py
@@ -0,0 +1,13 @@
+import sys,click
+
+@click.command()
+@click.option('-x', '--xmindpath', 'xmindpath', required=True, help='xmind file path')
+@click.option('-e', '--excelpath', 'excelpath', required=True, help='excel file path')
+@click.option('-i', '--ignorelayer', 'ignorelayer', required=True)
+def test(xmindpath, excelpath, ignorelayer):
+    print(xmindpath + 'haha' + excelpath + 'haha' + ignorelayer)
+    # if ignorelayer == '0':
+    #     ConvertXmindToExcel().convert(xmindPath, excelPath)
+    # else:
+    #     ConvertXmindToExcel().convert(xmindPath, excelPath, ignorelayer)
+    sys.exit()
