62e8ad19b1f572924e8fe000ae8d74b2e50fcffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:29 +0000

    Add package django-doctor/2.1.5/django_doctor-2.1.5-cp39-cp39-win32.whl

---
+++ packages/django-doctor/django_doctor-2.1.5-cp39-cp39-win32.whl/django_doctor/wizard/precache-manifest.eda46adc4dd1263d39ec88167b5a6907.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1680b495f1cb76b8a57",
+    "url": "/static/js/runtime~main.d1680b49.js"
+  },
+  {
+    "revision": "e0c91dc3c7ce61f25168",
+    "url": "/static/js/main.e0c91dc3.chunk.js"
+  },
+  {
+    "revision": "211ee9cb34aef5736aa3",
+    "url": "/static/js/3.211ee9cb.chunk.js"
+  },
+  {
+    "revision": "212fea1ae2ebb64acf53",
+    "url": "/static/js/2.212fea1a.chunk.js"
+  },
+  {
+    "revision": "5f748b304c04d53ec108",
+    "url": "/static/js/1.5f748b30.chunk.js"
+  },
+  {
+    "revision": "e0c91dc3c7ce61f25168",
+    "url": "/static/css/main.d841e3ae.chunk.css"
+  },
+  {
+    "revision": "212fea1ae2ebb64acf53",
+    "url": "/static/css/2.3f1fae1f.chunk.css"
+  },
+  {
+    "revision": "6a5b9f927a1b107ecf61e0b91eeb94b1",
+    "url": "/index.html"
+  }
+];
