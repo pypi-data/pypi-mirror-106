116445e6c496093bf4477a78d8207ee479c38ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:01 +0000

    Add package infoblox-netmri/3.8.0.0/infoblox-netmri-3.8.0.0.tar.gz

---
+++ packages/infoblox-netmri/infoblox-netmri-3.8.0.0.tar.gz/infoblox-netmri-3.8.0.0/infoblox_netmri/api/remote/models/settings_auth_audit_log_grid_remote.py
@@ -0,0 +1,51 @@
+from ..remote import RemoteModel
+
+
+class SettingsAuthAuditLogGridRemote(RemoteModel):
+    """
+
+
+
+    |  ``id:`` none
+    |  ``attribute type:`` string
+
+    |  ``created_at:`` none
+    |  ``attribute type:`` string
+
+    |  ``unit_name:`` none
+    |  ``attribute type:`` string
+
+    |  ``user_name:`` none
+    |  ``attribute type:`` string
+
+    |  ``event_type:`` none
+    |  ``attribute type:`` string
+
+    |  ``message:`` none
+    |  ``attribute type:`` string
+
+    |  ``field_changes:`` none
+    |  ``attribute type:`` string
+
+    |  ``field_changes_exportable:`` none
+    |  ``attribute type:`` string
+
+    |  ``client_ip:`` none
+    |  ``attribute type:`` string
+
+    |  ``DeviceID:`` none
+    |  ``attribute type:`` string
+
+    """
+
+    properties = ("id",
+                  "created_at",
+                  "unit_name",
+                  "user_name",
+                  "event_type",
+                  "message",
+                  "field_changes",
+                  "field_changes_exportable",
+                  "client_ip",
+                  "DeviceID",
+                  )
