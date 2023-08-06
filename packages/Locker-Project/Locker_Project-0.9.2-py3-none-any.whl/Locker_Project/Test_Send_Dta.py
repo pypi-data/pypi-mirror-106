a94f3ec3eb430b034c6b6b9f9e79b0eab18c9bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:52 +0000

    Add package Locker-Project/0.9.2/Locker_Project-0.9.2-py3-none-any.whl

---
+++ packages/Locker-Project/Locker_Project-0.9.2-py3-none-any.whl/Locker_Project/Test_Send_Dta.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+import time
+
+
+class Test_Send_Dta(threading.Thread):
+    def __init__(self, host, Port, mes):
+        threading.Thread.__init__(self)
+        self.signal = True
+        self.mes = mes
+        self.host = host
+        self.Port = Port
+
+    def run(self):
+        while 1:
+            try:
+                chuoi = '<id>1253</id><type>message</type><data>' + self.mes + '</data>'
+                chuoi = chuoi.encode('utf-8')
+                size = len(chuoi)
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                    sock.connect((self.host, self.Port))
+                    sock.sendall(size.to_bytes(4, byteorder='big'))
+                    sock.sendall(chuoi)
+                    sock.close()
+                    del chuoi
+            except Exception as e:
+                print(str(e))
+                sock.close()
+            time.sleep(2)
+            pass
+
+    def __del__(self):
+        print(self.name, 'Da duoc Delete')
