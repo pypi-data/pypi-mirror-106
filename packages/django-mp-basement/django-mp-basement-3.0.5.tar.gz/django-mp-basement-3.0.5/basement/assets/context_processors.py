cfe4733fd5f024213f0ebc31adb65a60b42835ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:22 +0000

    Add package django-mp-basement/3.0.5/django-mp-basement-3.0.5.tar.gz

---
+++ packages/django-mp-basement/django-mp-basement-3.0.5.tar.gz/django-mp-basement-3.0.5/basement/assets/context_processors.py
@@ -0,0 +1,12 @@
+
+from basement.assets.webp import is_webp_supported
+
+
+def webp(request):
+
+    is_supported = is_webp_supported(request)
+
+    return {
+        'IMG_FORMAT': 'WEBP' if is_supported else 'JPEG',
+        'IS_WEBP_SUPPORTED': is_supported
+    }
