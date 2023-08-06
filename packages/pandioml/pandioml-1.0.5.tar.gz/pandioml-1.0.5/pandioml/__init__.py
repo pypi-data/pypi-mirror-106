f06182e6afeb2a1e6eed35b7063696ba8d0f0a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:50 +0000

    Add package pandioml/1.0.5/pandioml-1.0.5.tar.gz

---
+++ packages/pandioml/pandioml-1.0.5.tar.gz/pandioml-1.0.5/pandioml/__init__.py
@@ -0,0 +1,8 @@
+from . import model
+from . import function
+from . import data
+from . import core
+from . import metrics
+from . import stats
+
+__all__ = ['model', 'function', 'data', 'core', 'metrics', 'stats']
---
+++ packages/pandioml/pandioml-1.0.5.tar.gz/pandioml-1.0.5/setup.py
@@ -0,0 +1,29 @@
+import setuptools, os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pandioml",
+    version="1.0.5",
+    author="Joshua Odmark",
+    author_email="josh@pandio.com",
+    description="Pandio's machine learning library.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/pandio-com/pandioml",
+    packages=setuptools.find_packages(exclude=['tests']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Documentation",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+    ],
+    install_requires=['numpy==1.20.1', 'pulsar-client==2.6.0', 'Faker==8.1.1', 'river==0.7.0', 'scikit-learn==0.24.1',
+                      'pandiocli==1.0.9'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests'
+)
