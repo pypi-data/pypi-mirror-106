96a48bfc0e07e0bf0b11b3d3d36396db5d7fcdb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:39 +0000

    Add package zshpower/0.8.0/zshpower-0.8.0.tar.gz

---
+++ packages/zshpower/zshpower-0.8.0.tar.gz/zshpower-0.8.0/snakypy/zshpower/__init__.py
@@ -0,0 +1,19 @@
+"""
+ZSHPower
+~~~~~~~~
+
+ZSHPower is a theme for ZSH with a manager.
+
+
+For more information, access: 'https://github.com/snakypy/zshpower'
+
+:copyright: Copyright 2020-2021 by Snakypy team, see AUTHORS.
+:license: MIT license, see LICENSE for details.
+"""
+
+from contextlib import suppress
+from pathlib import Path
+
+with suppress(KeyboardInterrupt):
+    HOME = str(Path.home())
+    __version__ = "0.8.0"
