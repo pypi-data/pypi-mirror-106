ce4dd7d0fa1bf05192653c7c17b7d632f9f6c36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:00 +0000

    Add package stsutility/0.0.4/stsutility-0.0.4.tar.gz

---
+++ packages/stsutility/stsutility-0.0.4.tar.gz/stsutility-0.0.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+setuptools.setup(
+	name="stsutility",
+	version="0.0.4",
+	author="636",
+	author_email="win.stitch.23@gmail.com",
+	description="636 Utility Package",
+	url="https://github.com/0187773933/stsutility",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+	setup_requires=['numpy','pandas'],
+	install_requires=[
+		'json',
+		'pathlib',
+		'time',
+		'Pint',
+		'math',
+		'decimal',
+		'operator',
+		'pandas',
+		'numpy',
+		'pprint',
+	],
+)
+
