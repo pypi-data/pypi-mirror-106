dcc2f8181084456a20ff9eba51001dac7437dd53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:09 +0000

    Add package greetings-sdk/0.0.1/greetings_sdk-0.0.1.tar.gz

---
+++ packages/greetings-sdk/greetings_sdk-0.0.1.tar.gz/greetings_sdk-0.0.1/README.md
@@ -0,0 +1,21 @@
+## Greetings SDK
+
+Greetings SDK to say Hello!
+
+## Getting Started
+
+
+### Install
+
+```
+pip install greetings_sdk
+```
+
+
+### How To Use
+
+```python
+import greetings
+greetigs.say_hello("Omar")
+```
+
---
+++ packages/greetings-sdk/greetings_sdk-0.0.1.tar.gz/greetings_sdk-0.0.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="greetings_sdk",
+    version="0.0.1",
+    author="Omar Ilias",
+    author_email="elmimouni.o.i@gmail.com",
+    description="Greetings SDK.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elmiomar/greetings",
+    project_urls={
+        "Bug Tracker": "https://github.com/elmiomar/greetings/issues",
+        "Documentation": "https://github.com/elmiomar/greetings/wiki",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
