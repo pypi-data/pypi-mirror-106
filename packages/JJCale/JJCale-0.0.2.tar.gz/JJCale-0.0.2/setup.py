dac4e56ed1080cdc31570c210db3bd0abe50927b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:19 +0000

    Add package JJCale/0.0.2/JJCale-0.0.2.tar.gz

---
+++ packages/JJCale/JJCale-0.0.2.tar.gz/JJCale-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JJCale',
+    version='0.0.2',
+    description='a simple web framework',
+    long_description='a simple web framework',
+    url='https://github.com/lrhhhhhh/JJCale',
+    author='lrhaoo',
+    license='MIT',
+    keywords=('web', 'framework'),
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'Werkzeug>=2.0.0',
+        'jinja2>=3.0.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
