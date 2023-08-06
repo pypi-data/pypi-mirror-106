51bbaebb6183d0c97e602e2e5739846ee421ffe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:50 +0000

    Add package couchcopy/0.1.0/couchcopy-0.1.0.tar.gz

---
+++ packages/couchcopy/couchcopy-0.1.0.tar.gz/couchcopy-0.1.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# Copyright 2021 Tolteck
+
+import importlib.machinery
+import pathlib
+import setuptools
+
+path = str(pathlib.Path(__file__).parent.absolute())
+couchcopy = importlib.machinery \
+            .SourceFileLoader('couchcopy', path + '/couchcopy').load_module()
+
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='couchcopy',
+    version=couchcopy.__version__,
+    author='Hoel Iris',
+    url='https://github.com/tolteck/couchcopy',
+    description='Backup and restore CouchDB cluster',
+    long_description=readme,
+    license='GPLv3',
+    py_modules=['couchcopy'],
+    entry_points='''
+        [console_scripts]
+        couchcopy = couchcopy:couchcopy
+    ''',
+    install_requires=[
+        'aiocouch >=1.1.0',
+    ],
+    python_requires=">=3.5",
+)
