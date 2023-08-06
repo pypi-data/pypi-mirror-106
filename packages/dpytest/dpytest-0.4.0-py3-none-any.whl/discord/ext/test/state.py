3fbca55c010365c5bdc95f87d7837f23d63e9471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:30 +0000

    Add package dpytest/0.4.0/dpytest-0.4.0-py3-none-any.whl

---
+++ packages/dpytest/dpytest-0.4.0-py3-none-any.whl/discord/ext/test/state.py
@@ -0,0 +1,58 @@
+import asyncio
+import typing
+import discord
+import discord.http as dhttp
+import discord.state as dstate
+
+from . import factories as facts
+from . import backend as back
+
+
+class FakeState(dstate.ConnectionState):
+
+    http: 'back.FakeHttp'  # String because of circular import
+
+    def __init__(self, client: discord.Client, http: dhttp.HTTPClient, user: discord.ClientUser = None, loop: asyncio.AbstractEventLoop = None) -> None:
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        super().__init__(dispatch=client.dispatch,
+                         handlers=None, hooks=None,
+                         syncer=None, http=http,
+                         loop=loop, intents=client.intents,
+                         member_cache_flags=client._connection.member_cache_flags)
+        if user is None:
+            user = discord.ClientUser(state=self, data=facts.make_user_dict("FakeApp", "0001", None))
+            user.bot = True
+        self.user = user
+        self.shard_count = client.shard_count
+        self._get_websocket = lambda x: client.ws
+        self._do_dispatch = True
+
+        real_disp = self.dispatch
+
+        def dispatch(*args, **kwargs):
+            if not self._do_dispatch:
+                return
+            return real_disp(*args, **kwargs)
+
+        self.dispatch = dispatch
+
+    def stop_dispatch(self) -> None:
+        self._do_dispatch = False
+
+    def start_dispatch(self) -> None:
+        self._do_dispatch = True
+
+    # TODO: Respect limit parameters
+    async def query_members(self, guild: discord.Guild, query: str, limit: int, user_ids: int, cache: bool, presences: bool) -> None:
+        guild: discord.Guild = discord.utils.get(self.guilds, id=guild.id)
+        return guild.members
+
+    async def chunk_guild(self, guild: discord.Guild, *, wait: bool = True, cache: typing.Optional[bool] = None):
+        pass
+
+    def _guild_needs_chunking(self, guild: discord.Guild):
+        """
+        Prevents chunking which can throw asyncio wait_for errors with tests under 60 seconds
+        """
+        return False
