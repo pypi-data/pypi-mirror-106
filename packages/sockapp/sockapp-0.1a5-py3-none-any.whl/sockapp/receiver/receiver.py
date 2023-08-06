51ed7252e61dc53fdf8e949f6f5d1ce423543fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:46 +0000

    Add package sockapp/0.1a5/sockapp-0.1a5-py3-none-any.whl

---
+++ packages/sockapp/sockapp-0.1a5-py3-none-any.whl/sockapp/receiver/receiver.py
@@ -0,0 +1,18 @@
+from sockapp.receiver.udp_receiver import UDPReceiver
+from ..constants import *
+from ..utils import error
+from .tcp_receiver import TCPReceiver
+from .udp_receiver import UDPReceiver
+
+
+class Receiver:
+    @staticmethod
+    def get_receiver(port=PORT, protocol=PROTOCOL):
+        if protocol == "TCP":
+            return TCPReceiver(port=port)
+        elif protocol == "UDP":
+            return UDPReceiver(port=port)
+        else:
+            raise error.InvalidSocketProtocol(
+                message=f"Protocol {protocol} not known, cannot create receiver instance!"
+            )
