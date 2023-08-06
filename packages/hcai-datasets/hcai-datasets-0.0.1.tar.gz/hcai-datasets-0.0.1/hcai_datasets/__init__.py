7ddece6d944e4ef3894498983269f1ac62bb8c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:30 +0000

    Add package hcai-datasets/0.0.1/hcai-datasets-0.0.1.tar.gz

---
+++ packages/hcai-datasets/hcai-datasets-0.0.1.tar.gz/hcai-datasets-0.0.1/hcai_datasets/__init__.py
@@ -0,0 +1,8 @@
+'''Image Datasets'''
+from hcai_datasets import hcai_ckplus
+from hcai_datasets import hcai_faces
+
+'''Audio Datasets'''
+from hcai_datasets import hcai_audioset
+from hcai_datasets import hcai_librispeech
+from hcai_datasets import hcai_is2021_ess
