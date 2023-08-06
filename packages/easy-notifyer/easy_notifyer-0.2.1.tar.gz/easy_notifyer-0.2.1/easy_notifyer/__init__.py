a11e349a35d26595fbe6b7b4ed1a31cbac7db874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:59 +0000

    Add package easy-notifyer/0.2.1/easy_notifyer-0.2.1.tar.gz

---
+++ packages/easy-notifyer/easy_notifyer-0.2.1.tar.gz/easy_notifyer-0.2.1/easy_notifyer/__init__.py
@@ -0,0 +1,16 @@
+from easy_notifyer.clients.mailer import Mailer
+from easy_notifyer.clients.telegram import Telegram, TelegramAsync
+from easy_notifyer.handlers.mailer import mailer_reporter
+from easy_notifyer.handlers.telegram import telegram_reporter
+
+
+__version__ = "0.2.1"
+
+
+__all__ = [
+    "Mailer",
+    "Telegram",
+    "TelegramAsync",
+    "mailer_reporter",
+    "telegram_reporter",
+]
