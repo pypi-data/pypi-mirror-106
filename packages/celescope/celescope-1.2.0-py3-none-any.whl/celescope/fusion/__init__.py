fbe2616e04d34c1a081a807e24f5380f03142eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:19 +0000

    Add package celescope/1.2.0/celescope-1.2.0-py3-none-any.whl

---
+++ packages/celescope/celescope-1.2.0-py3-none-any.whl/celescope/fusion/__init__.py
@@ -0,0 +1,2 @@
+__STEPS__ = ['sample', 'barcode', 'cutadapt', "star_fusion", "count_fusion"]
+__ASSAY__ = 'fusion'
---
+++ packages/celescope/celescope-1.2.0-py3-none-any.whl/celescope/snp/__init__.py
@@ -0,0 +1,3 @@
+__STEPS__ = ['sample', 'barcode', 'cutadapt', 'consensus', 'star', 'featureCounts', 
+    'target_metrics', 'snpCalling', 'analysis_snp']
+__ASSAY__ = 'snp'
