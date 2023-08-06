a3aaf6eb7f2087fc077aa50af02ac139ab4a7b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:43 +0000

    Add package ondamonitor/21.5.0/ondamonitor-21.5.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ondamonitor/ondamonitor-21.5.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/om/data_retrieval_layer/__init__.py
@@ -0,0 +1,25 @@
+# This file is part of OM.
+#
+# OM is free software: you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# OM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with OM.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2020 -2021 SLAC National Accelerator Laboratory
+#
+# Based on OnDA - Copyright 2014-2019 Deutsches Elektronen-Synchrotron DESY,
+# a research centre of the Helmholtz Association.
+"""
+OM's ta Retrieval Layer package.
+
+This package contains OM's Data Retrieval Layer, with Data Event Handlers and Data
+Extraction Functions supporting several facilities. Functions and classes for different
+detectors and software frameworks are implemented in separate modules that are imported
+on-demand when OM starts.
+"""
