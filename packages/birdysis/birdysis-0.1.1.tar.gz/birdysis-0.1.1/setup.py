ab3f29ecb29451360a9b09b74d2b33bce464a69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:46 +0000

    Add package birdysis/0.1.1/birdysis-0.1.1.tar.gz

---
+++ packages/birdysis/birdysis-0.1.1.tar.gz/birdysis-0.1.1/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import os
+
+this = os.path.abspath(os.path.dirname(__file__))
+repo = "https://github.com/sweeneyngo/birdysis"
+
+setup(
+
+    # Application name:
+    name="birdysis",
+
+    # Version number (initial):
+    version="0.1.1",
+
+    # Application author details:
+    author="Sweeney Ngo",
+    author_email="sweeneyngo@gmail.com",
+
+    # Packages
+    packages=find_packages('src.*'),
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+
+    python_requires='>=3.6',
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url=repo,
+
+
+    license="LICENSE",
+    description="Retrieves and downloads liked tweets on Twitter.",
+
+    # long_description=Readme,
+    # long_description_content_type="text/markdown",
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "tweepy",
+        "selenium",
+        "cryptography",
+
+    ],
+    entry_points={
+        'console_scripts': [
+                'birdysis=src.__main__:main',
+            ],
+        },
+)
