dde228ce1b1917051eefe4543cfb7d35dd0c49c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:39 +0000

    Add package Pyinger/0.0.5/Pyinger-0.0.5.tar.gz

---
+++ packages/Pyinger/Pyinger-0.0.5.tar.gz/Pyinger-0.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Ping library that will check if server is up or down'
+LONG_DESCRIPTION = 'A package that allows you to check on a servers status. Can get GMAIL notifications if server is down.'
+
+# Setting up
+setup(
+    name="Pyinger",
+    version=VERSION,
+    author="Sean Pelser",
+    author_email="<sean.pelser97@gmail.com>",
+    description=DESCRIPTION,
+    url='https://github.com/seanp97/PyServerChecker',
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['ping3', 'colorama'],
+    keywords=['python', 'ping', 'pinger', 'server', 'server checker', 'pyinger'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
