f91d61cc76ae68781cbcacc3658fda63177b1fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:39 +0000

    Add package baguette/0.3.0/baguette-0.3.0-py3-none-any.whl

---
+++ packages/baguette/baguette-0.3.0-py3-none-any.whl/baguette/forms.py
@@ -0,0 +1,142 @@
+import collections
+import mimetypes
+import typing
+from cgi import parse_header
+from urllib.parse import parse_qs
+
+from .headers import make_headers
+from .utils import split_on_first
+
+
+class Field:
+    def __init__(
+        self,
+        name: str,
+        values: typing.List[typing.Union[str, bytes]],
+        encoding: str = "utf-8",
+    ):
+        self.name = name
+        self.values = [
+            value.decode(encoding) if isinstance(value, bytes) else value
+            for value in values
+        ]
+        if len(self.values) > 0:
+            self.value = self.values[0]
+        else:
+            self.value = None
+
+        self.is_file = False
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class FileField(Field):
+    def __init__(
+        self,
+        name: str,
+        content: typing.Union[str, bytes],
+        filename: typing.Optional[str] = "",
+        content_type: typing.Optional[str] = None,
+        encoding: str = "utf-8",
+    ):
+        self.name = name
+
+        self.is_file = True
+        self.filename = filename
+        self.content = content
+        self.content_type = content_type
+        self.encoding = encoding
+
+        if not self.content_type:
+            self.content_type = (
+                mimetypes.guess_type(self.filename)[0]
+                or "application/octet-stream"
+            )
+
+    @property
+    def text(self) -> typing.Union[str, bytes]:
+        if isinstance(self.content, bytes):
+            try:
+                return self.content.decode(self.encoding)
+            except UnicodeDecodeError:
+                return self.content
+
+        return self.content
+
+    def __str__(self) -> str:
+        return self.text
+
+
+class Form(collections.abc.Mapping):
+    def __init__(
+        self,
+        fields: typing.Optional[typing.Dict[str, Field]] = None,
+        files: typing.Optional[typing.Dict[str, FileField]] = None,
+    ):
+        self.fields: typing.Dict[str, Field] = fields or {}
+        self.files: typing.Dict[str, FileField] = files or {}
+
+    def __getitem__(self, name: str) -> Field:
+        return self.fields[name]
+
+    def __iter__(self) -> typing.Iterator:
+        return iter(self.fields)
+
+    def __len__(self) -> int:
+        return len(self.fields)
+
+    @classmethod
+    def parse(cls, body: bytes) -> "Form":
+        raise NotImplementedError()
+
+
+class URLEncodedForm(Form):
+    @classmethod
+    def parse(cls, body: bytes, encoding: str = "utf-8") -> "URLEncodedForm":
+        raw_fields: typing.Dict[str, typing.List[str]] = parse_qs(
+            body.decode(encoding), encoding=encoding
+        )
+        fields: typing.Dict[str, Field] = {}
+        for name, values in raw_fields.items():
+            fields[name] = Field(name, values, encoding=encoding)
+        return cls(fields)
+
+
+class MultipartForm(Form):
+    @classmethod
+    def parse(
+        cls, body: bytes, boundary: bytes, encoding: str = "utf-8"
+    ) -> "MultipartForm":
+        fields: typing.Dict[str, Field] = {}
+        files: typing.Dict[str, FileField] = {}
+        for part in body.strip(b"\r\n").split(b"".join((b"--", boundary))):
+            part = part.strip(b"\r\n")
+            if part in (b"", b"--"):  # ignore start and end parts
+                continue
+
+            headers, value = split_on_first(part, b"\r\n\r\n")
+            headers = make_headers(headers.decode(encoding))
+            kwargs = parse_header(headers["content-disposition"])[1]
+
+            name = kwargs["name"]
+            is_file = "filename" in kwargs
+            filename = kwargs.get("filename", None)
+
+            if name in fields and not fields[name].is_file:
+                fields[name].values.append(value.decode(encoding))
+            else:
+                if is_file:
+                    fields[name] = files[name] = FileField(
+                        name,
+                        value,
+                        filename=filename,
+                        content_type=parse_header(headers["content-type"])[0],
+                        encoding=encoding,
+                    )
+                else:
+                    fields[name] = Field(
+                        name,
+                        [value],
+                    )
+        return cls(fields, files)
---
+++ packages/baguette/baguette-0.3.0-py3-none-any.whl/baguette/headers.py
@@ -0,0 +1,171 @@
+import itertools
+import typing
+from collections.abc import Mapping, Sequence
+
+from .types import HeadersType
+
+
+class Headers:
+    """Headers implementation for handling :class:`str` or :class:`bytes`
+    names and values.
+
+    .. tip::
+        Use :func:`make_headers` to easily make a header from a :class:`list` or
+        a :class:`dict`.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self._headers: typing.Dict[str, str] = {}
+
+        for name, value in itertools.chain(args, kwargs.items()):
+            if isinstance(name, bytes):
+                name = name.decode("ascii")
+            if isinstance(value, bytes):
+                value = value.decode("ascii")
+            self._headers[name.lower().strip()] = value.strip()
+
+    def get(self, name, default=None):
+        """Gets a header from its name. If not found, returns ``default``.
+
+        Arguments
+        ---------
+            name: :class:`str` or :class:`bytes`
+                Name of the header to get.
+            default: Optional anything
+                Value to return if header not found.
+                Default: ``None``
+
+        Returns
+        -------
+            :class:`str`
+                Header value if found.
+            Anything
+                ``default``'s value.
+        """
+
+        if isinstance(name, bytes):
+            name = name.decode("ascii")
+        return self._headers.get(name.lower(), default)
+
+    def keys(self):
+        """Returns an iterator over the headers names.
+
+        Returns
+        -------
+            Iterator of :class:`str`
+                Iterator over the headers names.
+        """
+
+        return self._headers.keys()
+
+    def items(self):
+        """Returns an iterator over the headers names and values.
+
+        Returns
+        -------
+            Iterator of ``(name, value)`` :class:`tuple`
+                Iterator over the headers names and values.
+        """
+
+        return self._headers.items()
+
+    def raw(self):
+        """Returns the raw headers, a :class:`list` of ``[name, value]`` where
+        ``name`` and ``value`` are :class:`bytes`.
+
+        Returns
+        -------
+            :class:`list` of ``[name, value]`` where ``name`` and ``value`` are\
+            :class:`bytes`
+                Raw headers for the ASGI response.
+        """
+        return [
+            [name.encode("ascii"), value.encode("ascii")]
+            for name, value in self
+        ]
+
+    def __str__(self) -> str:
+        return "\n".join(name + ": " + value for name, value in self)
+
+    def __iter__(self):
+        return iter(self.items())
+
+    def __len__(self):
+        return len(self._headers)
+
+    def __getitem__(self, name):
+        if isinstance(name, bytes):
+            name = name.decode("ascii")
+        return self._headers[name.lower()]
+
+    def __setitem__(self, name, value):
+        if isinstance(name, bytes):
+            name = name.decode("ascii")
+        if isinstance(value, bytes):
+            value = value.decode("ascii")
+        self._headers[name.lower()] = value
+
+    def __delitem__(self, name):
+        if isinstance(name, bytes):
+            name = name.decode("ascii")
+        del self._headers[name.lower()]
+
+    def __contains__(self, name):
+        if isinstance(name, bytes):
+            name = name.decode("ascii")
+        return name.lower() in self._headers
+
+    def __add__(self, other: typing.Union["Headers", typing.Mapping[str, str]]):
+        new = Headers(**self)
+        new += other
+        return new
+
+    def __iadd__(
+        self, other: typing.Union["Headers", typing.Mapping[str, str]]
+    ):
+        for name, value in other.items():
+            if isinstance(name, bytes):
+                name = name.decode("ascii")
+            if isinstance(value, bytes):
+                value = value.decode("ascii")
+            self._headers[name.lower()] = value
+        return self
+
+
+def make_headers(headers: HeadersType = None) -> Headers:
+    """Makes a :class:`Headers` object from a :class:`list` of
+        ``(str, str)`` tuples, a :class:`dict`, or a :class:`Headers` instance.
+
+        Arguments
+        ---------
+            headers: :class:`list` of ``(str, str)`` tuples, \
+            :class:`dict` or :class:`Headers`
+                The raw headers to convert.
+
+        Raises
+        ------
+            :exc:`ValueError`
+                ``type_`` isn't one of: 'plain', 'json', 'html'.
+
+        Returns
+        -------
+            :class:`Headers`
+                The converted headers.
+        """
+
+    if headers is None:
+        headers = Headers()
+    elif isinstance(headers, str):
+        headers = Headers(
+            *[header.split(":") for header in headers.splitlines()]
+        )
+    elif isinstance(headers, Sequence):
+        headers = Headers(*headers)
+    elif isinstance(headers, (Mapping, Headers)):
+        headers = Headers(**headers)
+    else:
+        raise ValueError(
+            "headers must be a list, a dict, a Headers instance or None"
+        )
+
+    return headers
