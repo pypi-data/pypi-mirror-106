a81adb4c3550bcc90c57662945db31c895d54b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:52 +0000

    Add package semantyk/0.0.72/semantyk-0.0.72.tar.gz

---
+++ packages/semantyk/semantyk-0.0.72.tar.gz/semantyk-0.0.72/semantyk/__init__.py
@@ -0,0 +1,17 @@
+'''
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+# Semantyk 
+Package | `semantyk`
+
+Daniel Bakas Amuchastegui\
+August 31, 2017
+
+Copyright © Semantyk 2021. All rights reserved.\
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+'''
+
+# From Import Declarations | Package Imports
+from . import archive
+
+# From Import Declarations | Module Imports
+from .archive import *
