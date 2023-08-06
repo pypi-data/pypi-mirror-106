df9c8751d0103acc15504d314da07312727af965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package mpl-plotter/3.8.7/mpl_plotter-3.8.7-py3-none-any.whl

---
+++ packages/mpl-plotter/mpl_plotter-3.8.7-py3-none-any.whl/tests/tests_3D.py
@@ -0,0 +1,33 @@
+import unittest
+import numpy as np
+from mpl_plotter.three_d import line, scatter, surface
+
+
+class Tests(unittest.TestCase):
+
+    def test_line(self):
+        line(show=True, azim=33, elev=27,)
+
+    def test_scatter(self):
+        scatter(point_size=60,
+                grid=True, grid_lines='-.',
+                cmap='plasma', x_tick_number=5,
+                color_bar=True, show=True,
+                azim=33, elev=27,)
+
+    def test_surface(self):
+        surface(show=True,
+                azim=-122, elev=35,
+                alpha=1,
+                lighting=False, antialiased=False, shade=False,
+                edge_color="black", edges_to_rgba=True, line_width=0.5,
+                rstride=1, cstride=1,
+                blend_edges=True, pane_fill="red")
+
+    def test_wireframe(self):
+        surface(show=True,
+                azim=-122, elev=35,
+                alpha=0,
+                lighting=False, antialiased=False, shade=False,
+                edge_color="red", edges_to_rgba=False, line_width=0.5,
+                rstride=12, cstride=12)
