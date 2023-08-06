59471a828c7690af7974d9aee4d681723acbd0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:36 +0000

    Add package geomytry/1.0.0/geomytry-1.0.0.tar.gz

---
+++ packages/geomytry/geomytry-1.0.0.tar.gz/geomytry-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("DOCS.md") as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name="geomytry",
+    version="1.0.0",
+    description="Useful functions to work with geometry",
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license="MIT",
+    packages=find_packages(),
+    author="Mordy Waldo",
+    python_requires=">=3.0.0",
+    author_email="imky171@gmail.com",
+    keywords=["Geometry", "Math", "Shapes"],
+    url="https://github.com/mordy-python/geomytry",
+    download_url="https://pypi.org/project/geomytry/",
+)
+
+install_requires = []
+
+if __name__ == "__main__":
+    setup(**setup_args, install_requires=install_requires)
