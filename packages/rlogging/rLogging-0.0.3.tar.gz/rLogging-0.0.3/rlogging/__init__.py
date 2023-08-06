8f0476220628c130316b4ecb67be195464ce9e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:37 +0000

    Add package rlogging/0.0.3/rLogging-0.0.3.tar.gz

---
+++ packages/rlogging/rLogging-0.0.3.tar.gz/rLogging-0.0.3/rlogging/__init__.py
@@ -0,0 +1,7 @@
+""" Модуль гибкого логирования """
+
+from . import *
+from .main import get_logger, registration_logger, start_loggers, stop_loggers
+
+# alpha beta release
+__version__ = (0, 0, 3, 'alpha', 0)
