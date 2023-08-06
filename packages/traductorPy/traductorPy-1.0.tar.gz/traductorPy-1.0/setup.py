3e05ea83383e01d60be490dfb34ae8be15ccc1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:27 +0000

    Add package traductorPy/1.0/traductorPy-1.0.tar.gz

---
+++ packages/traductorPy/traductorPy-1.0.tar.gz/traductorPy-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'traductorPy',
+  packages = ['traductorPy'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Traductor web',
+  author = ['José Rodriguez','Stiven Perdomo', 'Juan Camilo Castellanos'],
+  author_email = ['jrm2087@gmail.com','stiven97.perdomo@gmail.com ','camilo.castellanos.puentes@gmail.com'],
+  #url = 'https://github.com/jrm2087/traductorPy', # use the URL to the github repo
+  #download_url = 'https://github.com/jrm2087/traductorPy/tarball/0.1',
+  keywords = ['traductor', 'web'],
+  classifiers = [],
+)
