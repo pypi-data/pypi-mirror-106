974f25b44104a26205a29bcced1c4f65ffdcd557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:23 +0000

    Add package geoLibDPP/1.2/geoLibDPP-1.2.tar.gz

---
+++ packages/geoLibDPP/geoLibDPP-1.2.tar.gz/geoLibDPP-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geoLibDPP',
+    version='1.2',
+    author='Pavel Babrovich',
+    author_email='251223@student.pwr.edu.pl',
+    packages=find_packages()
+)
