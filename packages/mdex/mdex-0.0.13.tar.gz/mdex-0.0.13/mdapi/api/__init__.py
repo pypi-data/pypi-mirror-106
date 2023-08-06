26539e2428d778bddbcf77be871d390954852053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:15 +0000

    Add package mdex/0.0.13/mdex-0.0.13.tar.gz

---
+++ packages/mdex/mdex-0.0.13.tar.gz/mdex-0.0.13/mdapi/api/__init__.py
@@ -0,0 +1,16 @@
+from .account import AccountAPI
+from .auth import AuthAPI
+from .author import AuthorAPI
+from .chapter import ChapterAPI
+from .cover import CoverAPI
+from .group import GroupAPI
+from .list import ListAPI
+from .manga import MangaAPI
+from .misc import MiscAPI
+from .user import UserAPI
+
+
+__all__ = (
+    "AccountAPI", "AuthAPI", "AuthorAPI", "ChapterAPI", "GroupAPI", "ListAPI",
+    "MangaAPI", "MiscAPI", "UserAPI", "CoverAPI"
+)
