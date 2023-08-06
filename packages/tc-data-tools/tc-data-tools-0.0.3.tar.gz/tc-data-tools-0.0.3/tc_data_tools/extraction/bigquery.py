a888de81fa745ab70dbbe33e771a02193b4b69cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:47 +0000

    Add package tc-data-tools/0.0.3/tc-data-tools-0.0.3.tar.gz

---
+++ packages/tc-data-tools/tc-data-tools-0.0.3.tar.gz/tc-data-tools-0.0.3/tc_data_tools/extraction/bigquery.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def df_from_bigquery(query, gcp_project, gcs_bucket, gcs_file_path, local_file_path, mode='csv'):
+    
+    df = pd.read_gbq(query, gcp_project)
+    
+    if mode == 'csv':
+        df.to_csv(local_file_path)
+    elif mode == 'parquet':
+        df.to_parquet(local_file_path)
+        
+    gcs_upload(gcp_project, gcs_bucket, gcs_file_path, local_file_path)
+    
+    return df
---
+++ packages/tc-data-tools/tc-data-tools-0.0.3.tar.gz/tc-data-tools-0.0.3/tc_data_tools/io.py
@@ -0,0 +1,33 @@
+from google.cloud import storage
+
+def gcs_upload(gcp_project, gcs_bucket, gcs_file_path, local_file_path):
+    """
+    Upload a local local to google cloud storage
+    """
+    
+    # create conncetion
+    client = storage.Client(gcp_project)
+    bucket = client.bucket(gcs_bucket)
+    # upload
+    blob = bucket.blob(gcs_file_path)
+    blob.upload_from_filename(local_file_path)
+    
+    # INCLUIR LOGS
+    
+    
+    
+def gcs_download(gcp_project, gcs_bucket, gcs_file_path, local_file_path):
+    """
+    Downloads a google cloud storage file
+    """
+ 
+    # create conncetion
+    storage_client = storage.Client(gcp_project)
+    bucket = storage_client.bucket(gcs_bucket)
+    # download
+    blob = bucket.blob(gcs_file_path)
+    blob.download_to_filename(local_file_path)
+ 
+    # INCLUIR LOGS
+    
+    
