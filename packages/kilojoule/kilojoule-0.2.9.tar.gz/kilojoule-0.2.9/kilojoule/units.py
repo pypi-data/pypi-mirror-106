0ff81fa1d209a943e2eb70cd1f6178924d370849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:01 +0000

    Add package kilojoule/0.2.9/kilojoule-0.2.9.tar.gz

---
+++ packages/kilojoule/kilojoule-0.2.9.tar.gz/kilojoule-0.2.9/kilojoule/units.py
@@ -0,0 +1,47 @@
+from pint import UnitRegistry
+
+units = UnitRegistry()
+units.default_format = ".5~P"
+units.default_LaTeX_format = ":~L"
+Quantity = units.Quantity
+
+# define custom units for dealing with humid air
+# lbmol
+units.define("pound_mole = 453.59237*mol = lbmol")
+# mass of dry air
+units.define("gram_dry_air = [mass_dry_air] = g_a = g_dry_air = ga ")
+units.define(
+    "pound_dry_air = 453.59237 * gram_dry_air = lb_dry_air = lba = lb_a = lbm_a = lbma = lb_dry_air = lbm_dry_air"
+)
+# mass of humid air
+units.define("gram_humid_air = [mass_humid_air] = gha = g_ha = g_humid_air")
+units.define(
+    "pound_humid_air = 453.59237 * gram_humid_air = lb_humid_air = lbha = lbmha = lbm_humid_air"
+)
+# mass of water
+units.define("gram_water = [mass_water] = g_water = gw = g_w")
+units.define(
+    "pound_water = 453.59237 * gram_water = lb_water = lb_w = lbw = lbmw = lbm_w = lbm_water"
+)
+# molecules of dry air
+units.define(
+    "mole_dry_air = [substance_dry_air] = mol_dry_air = mol_a = mola = mol_da = molda"
+)
+units.define(
+    "pound_mole_dry_air = 453.59237 * mol_dry_air = lbmol_dry_air = lbmol_a = lbmola = lbmol_da = lbmolda"
+)
+# molecules of humid air
+units.define(
+    "mole_humid_air = [substance_humid_air] = mol_humid_air = mol_ha = molha = mol_ha = molha"
+)
+units.define(
+    "pound_mole_humid_air = 453.59237 * mol_humid_air = lbmol_humid_air = lbmol_ha = lbmolha = lbmol_ha = lbmolha"
+)
+# molecules of water
+units.define("mole_water = [substance_water] = mol_water = mol_w = molw")
+units.define(
+    "pound_mole_water = 453.59237 * mol_water = lbmol_water = lbmol_w = lbmolw"
+)
+# cubic feet per minute
+units.define("cubic_feet_per_minute = ft^3/min = cfm = CFM")
+
