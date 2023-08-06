a2fcf585e6c1858accaca26d20307d81164bdeae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:23 +0000

    Add package TextStatistic/1.0.5/TextStatistic-1.0.5.tar.gz

---
+++ packages/TextStatistic/TextStatistic-1.0.5.tar.gz/TextStatistic-1.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TextStatistic",
+    version="1.0.5",
+    author="Andrew Puchkov",
+    author_email="andr1502puch@gmail.com",
+    description="Text statistics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ShadowStalker13/TextStatistics",
+    classifiers=[
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Programming Language :: Python :: 3.9",
+    ],
+    package_dir={"": "scr"},
+    packages=setuptools.find_packages(where="scr"),
+)
