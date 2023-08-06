563563150ffc34937ca32564ff46375fac09ad95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:07 +0000

    Add package openpecha/0.7.35/openpecha-0.7.35.tar.gz

---
+++ packages/openpecha/openpecha-0.7.35.tar.gz/openpecha-0.7.35/openpecha/serializers/__init__.py
@@ -0,0 +1,6 @@
+from .editor import EditorSerializer
+from .epub import EpubSerializer
+from .foot_note import SerializeFootNote
+from .hfml import HFMLSerializer
+from .serializemd import SerializeMd
+from .rdf import Rdf
