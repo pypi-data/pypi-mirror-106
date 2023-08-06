93e9b006316b3be37620a097f4857a22a4619d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:19 +0000

    Add package orkestra/0.1.5/orkestra-0.1.5.tar.gz

---
+++ packages/orkestra/orkestra-0.1.5.tar.gz/orkestra-0.1.5/orkestra/utils.py
@@ -0,0 +1,17 @@
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+def rshift(self, right):
+    result = self.next(right)
+    make_composable(right)
+    make_composable(result)
+    return result
+
+
+def make_composable(obj: T) -> T:
+    obj.__class__.__rshift__ = rshift
+    return obj
+
+orkestrate = make_composable
