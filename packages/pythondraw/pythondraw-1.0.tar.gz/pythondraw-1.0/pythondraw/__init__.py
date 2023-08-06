0f2c4c32d98c997c12e4cbbad7e181245b035d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:04 +0000

    Add package pythondraw/1.0/pythondraw-1.0.tar.gz

---
+++ packages/pythondraw/pythondraw-1.0.tar.gz/pythondraw-1.0/pythondraw/__init__.py
@@ -0,0 +1,31 @@
+import turtle
+
+def screensize(a,b):
+  turtle.screensize(a,b)
+
+def startfill():
+  turtle.begin_fill()
+
+def endfill():
+  turtle.end_fill()
+
+def up(a):
+  turtle.forward(a)
+
+def down(a):
+  turtle.left(180)
+  turtle.forward(a)
+
+def left(a):
+  turtle.left(90)
+  turtle.forward(a)
+
+def right(a):
+  turtle.right(90)
+  turtle.forward(a)
+
+def turn_left(a):
+  turtle.left(a)
+
+def turn_right(a):
+  turtle.right(a)
