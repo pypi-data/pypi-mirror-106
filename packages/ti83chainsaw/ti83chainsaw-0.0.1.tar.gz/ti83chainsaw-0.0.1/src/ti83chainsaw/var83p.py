c5ecb3a46b4646be0c4f013ecab36dbe66749aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:44 +0000

    Add package ti83chainsaw/0.0.1/ti83chainsaw-0.0.1.tar.gz

---
+++ packages/ti83chainsaw/ti83chainsaw-0.0.1.tar.gz/ti83chainsaw-0.0.1/src/ti83chainsaw/var83p.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+class var83p:
+  """
+  The chainsaw
+  """
+
+  signature = ''
+  signature2 = ''
+  file_length = 0
+  
+  header_length = 0
+  data_length = 0
+  protected = 0
+  name = '00000000'
+
+  data_length = 0
+  program_length = 0
+
+  data = 0
+
+  checksum = 0
+  
+  def __init__(self):
+      print("Init")
