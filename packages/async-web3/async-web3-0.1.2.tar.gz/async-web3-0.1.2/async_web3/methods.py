1239fb9090f524441c69ee32892f3909cbf30abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:40 +0000

    Add package async-web3/0.1.2/async-web3-0.1.2.tar.gz

---
+++ packages/async-web3/async-web3-0.1.2.tar.gz/async-web3-0.1.2/async_web3/methods.py
@@ -0,0 +1,17 @@
+class RPCMethod:
+
+    web3_clientVersion = "web3_clientVersion"
+
+    eth_accounts = "eth_accounts"
+    eth_blockNumber = "eth_blockNumber"
+    eth_gasPrice = "eth_gasPrice"
+
+    eth_getBalance = "eth_getBalance"
+    eth_getStorageAt = "eth_getStorageAt"
+    eth_getBlockByHash = "eth_getBlockByHash"
+    eth_getBlockByNumber = "eth_getBlockByNumber"
+
+    eth_call = "eth_call"
+
+    eth_subscribe = "eth_subscribe"
+    eth_unsubscribe = "eth_unsubscribe"
