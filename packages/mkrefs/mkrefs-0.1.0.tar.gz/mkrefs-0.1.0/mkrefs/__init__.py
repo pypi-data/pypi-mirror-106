2c80364fdd584232c3d74e1d4548fe224069ddac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:02 +0000

    Add package mkrefs/0.1.0/mkrefs-0.1.0.tar.gz

---
+++ packages/mkrefs/mkrefs-0.1.0.tar.gz/mkrefs-0.1.0/mkrefs/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# see license https://github.com/DerwenAI/mkrefs#license-and-copyright
+
+from .plugin import MkRefsPlugin
+
+from .biblio import load_kg, render_biblio
+
+from .cli import cli
+
+from .version import __version__
