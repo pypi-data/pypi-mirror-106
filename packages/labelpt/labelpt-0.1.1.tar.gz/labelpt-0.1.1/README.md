64f5a44110e21f2afd7b0d30178fe7798825320c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:40 +0000

    Add package labelpt/0.1.1/labelpt-0.1.1.tar.gz

---
+++ packages/labelpt/labelpt-0.1.1.tar.gz/labelpt-0.1.1/README.md
@@ -0,0 +1,18 @@
+## labelpt
+
+Image segmentation annotation tool.
+
+<img src="https://raw.githubusercontent.com/pei223/labelpt/master/labelpt.png">
+<br>
+
+## Functions
+
+- Paint mode
+- Polygon mode
+- Line/Rect/Oval mode
+
+## Install
+
+```
+pip install labelpt
+```
---
+++ packages/labelpt/labelpt-0.1.1.tar.gz/labelpt-0.1.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+from codecs import open
+from os import path
+from setuptools import find_packages
+
+
+def _requires_from_file(filename: str):
+    return open(filename).read().splitlines()
+
+
+def _read_README():
+    return open(path.join(here, 'README.md'), encoding='utf-8').read().replace("\r", "")
+
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='labelpt',
+    install_requires=_requires_from_file("requirements.txt"),
+    packages=find_packages(),
+
+    license='MIT',
+
+    version='0.1.1',
+
+    author='pei223',
+    author_email='peidparc@gmail.com',
+    url='https://github.com/pei223/labelpt',
+
+    description='Annotation tool of image segmentation.',
+    long_description=_read_README(),
+    long_description_content_type='text/markdown',
+
+    keywords='image-segmentation annotation annotation-tool',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],  # reference https://pypi.org/classifiers/
+    entry_points={
+        "console_scripts": [
+            "labelpt=labelpt.run:main",
+        ],
+    },
+    include_package_data=True,
+)
