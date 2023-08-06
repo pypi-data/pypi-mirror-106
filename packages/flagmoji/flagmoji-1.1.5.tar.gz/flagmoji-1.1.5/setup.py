5fd785696244ec7044ba7e9d00a349558d2ca978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:30 +0000

    Add package flagmoji/1.1.5/flagmoji-1.1.5.tar.gz

---
+++ packages/flagmoji/flagmoji-1.1.5.tar.gz/flagmoji-1.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.5'
+DESCRIPTION = 'Converts a country code or country name into a flag emoji'
+LONG_DESCRIPTION = 'A Python package that converts a country code or country name into a flag emoji.'
+
+setup(
+        name="flagmoji",
+        version=VERSION,
+        author="Steven Yu",
+        author_email="stevenyuser@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        url = "https://github.com/stevenyuser/flagmoji",
+        packages=find_packages(),
+        install_requires=['flagmoji'],
+        py_modules=['data', 'flagmoji'],
+
+        keywords=['python', 'flagmoji', "emoji flag converter"],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
