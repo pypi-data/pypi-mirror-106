5932fd12eb0aacc24e2767f52519b84ca4393dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:31 +0000

    Add package QLog/0.2.2/QLog-0.2.2-py3-none-any.whl

---
+++ packages/QLog/QLog-0.2.2-py3-none-any.whl/QLog/system_logger.py
@@ -0,0 +1,32 @@
+"""Logger to log into system logger"""
+
+import logging
+
+from QLog.log_entry import LogEntry
+from QLog.log_level import LogLevel
+from QLog.logger import Logger
+
+
+class SystemLogger(Logger):
+    """Logger to log into system logger"""
+
+    def __init__(self, log_level=LogLevel.INFO):
+        self.log_level = log_level
+        if self.log_level is LogLevel.INFO:
+            logging.getLogger().setLevel(logging.INFO)
+        if self.log_level is LogLevel.WARNING:
+            logging.getLogger().setLevel(logging.WARNING)
+        if self.log_level is LogLevel.ERROR:
+            logging.getLogger().setLevel(logging.ERROR)
+        else:
+            logging.getLogger().setLevel(logging.DEBUG)
+
+    def do_log(self, log_entry: LogEntry):
+        text = log_entry.meta_text + log_entry.text
+        if log_entry.log_level is LogLevel.INFO:
+            return logging.info(text)
+        if log_entry.log_level is LogLevel.WARNING:
+            return logging.warning(text)
+        if log_entry.log_level is LogLevel.ERROR:
+            return logging.error(text)
+        return logging.debug(text)
