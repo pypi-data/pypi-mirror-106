d4fa6239babc69d3769cad7d6f2b56903718e137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:33 +0000

    Add package ascmhl/0.3.1a0/ascmhl-0.3.1a0.tar.gz

---
+++ packages/ascmhl/ascmhl-0.3.1a0.tar.gz/ascmhl-0.3.1a0/mhl/cli/ascmhl.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+__author__ = "Patrick Renner, Alexander Sahm"
+__copyright__ = "Copyright 2020, Pomfort GmbH"
+
+__license__ = "MIT"
+__maintainer__ = "Patrick Renner, Alexander Sahm"
+__email__ = "opensource@pomfort.com"
+"""
+
+import click
+from mhl import commands
+
+
+class NaturalOrderGroup(click.Group):
+    def list_commands(self, ctx):
+        return self.commands.keys()
+
+
+@click.group(cls=NaturalOrderGroup)
+@click.version_option()
+def mhltool_cli():
+    pass
+
+
+# new
+mhltool_cli.add_command(commands.create)
+mhltool_cli.add_command(commands.verify)
+mhltool_cli.add_command(commands.diff)
+mhltool_cli.add_command(commands.info)
+mhltool_cli.add_command(commands.xsd_schema_check)
+
+# old
+mhltool_cli.add_command(commands.directory_hash, "dirhash")
+
+
+if __name__ == '__main__':
+    mhltool_cli()
