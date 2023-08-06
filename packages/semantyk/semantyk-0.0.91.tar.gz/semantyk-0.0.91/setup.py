2df5f59bf6c302d4621cced549a52b57b6ee13dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:17 +0000

    Add package semantyk/0.0.91/semantyk-0.0.91.tar.gz

---
+++ packages/semantyk/semantyk-0.0.91.tar.gz/semantyk-0.0.91/setup.py
@@ -0,0 +1,36 @@
+from pip._internal.req import parse_requirements
+from setuptools import find_packages, setup
+
+from src import semantyk
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    author=semantyk.__author__,
+    author_email=semantyk.__email__,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8'
+    ],
+    description=semantyk.__description__,
+    keywords='semantyk',
+    license='LICENSE',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    maintainer=semantyk.__author__,
+    name='semantyk',
+    package_dir={'': 'src'},
+    packages=find_packages(
+        exclude={
+            'doc',
+            'tests*'
+        }
+    ),
+    url='https://github.com/semantyk/Semantyk',
+    version=semantyk.__version__
+)
