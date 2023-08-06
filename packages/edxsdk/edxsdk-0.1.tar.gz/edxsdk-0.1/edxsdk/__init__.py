7e14b72df70154c357343e5bcaf442b87462bb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:38 +0000

    Add package edxsdk/0.1/edxsdk-0.1.tar.gz

---
+++ packages/edxsdk/edxsdk-0.1.tar.gz/edxsdk-0.1/edxsdk/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： hj
+# datetime： 2021/5/18 0018 上午 9:51 
+# ide： PyCharm2020.1.3
+
---
+++ packages/edxsdk/edxsdk-0.1.tar.gz/edxsdk-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： hj
+# datetime： 2021/5/13 0013 下午 2:46 
+# ide： PyCharm2020.1.3
+import setuptools
+setuptools.setup(
+    name="edxsdk",
+    version="0.1",
+    author="hj",
+    author_email="1599712264@qq.com",
+    description="edx sdk",
+    packages=setuptools.find_packages()
+)
