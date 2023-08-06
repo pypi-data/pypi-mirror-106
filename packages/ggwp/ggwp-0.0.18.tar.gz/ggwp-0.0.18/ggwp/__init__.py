ba605718c74f9b715f8dfe966858eb9abfbe9ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:44 +0000

    Add package ggwp/0.0.18/ggwp-0.0.18.tar.gz

---
+++ packages/ggwp/ggwp-0.0.18.tar.gz/ggwp-0.0.18/ggwp/__init__.py
@@ -0,0 +1,13 @@
+# from folder.file import class
+from .EzUtils import *
+from .DataModel import *
+from .EzCheck import *
+from .EzCohort import *
+from .EzCustomerMovement import *
+from .EzRFM import *
+from .EzBasketEvolving import *
+from .EzLog import *
+from .EzEvaluation import *
+from .EzBenchMark import *
+from .EzPipeline.ConvertVariables import *
+from .EzPipeline.GroupImputer import *
