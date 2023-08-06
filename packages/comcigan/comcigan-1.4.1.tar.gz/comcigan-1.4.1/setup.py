ed45c612a4abcb889af4280020d19e3ec76f5af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:10 +0000

    Add package comcigan/1.4.1/comcigan-1.4.1.tar.gz

---
+++ packages/comcigan/comcigan-1.4.1.tar.gz/comcigan-1.4.1/setup.py
@@ -0,0 +1,25 @@
+from os import environ
+from setuptools import setup, find_packages
+
+version = environ['TRAVIS_TAG'].lstrip('v') if environ['TRAVIS'] == 'true' else environ['VERSION_NUMBER']
+
+setup(
+    name = 'comcigan',
+    version = version,
+    license = 'MIT',
+    author = 'Team IF',
+    author_email = 'Coder-Iro@teamif.io',
+    description = 'Unofficial Comcigan API python wrapper',
+    long_description = open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url = 'https://github.com/Team-IF/comcigan-py',
+    packages = find_packages(),
+    install_requires = ['requests', 'beautifulsoup4', 'aiohttp'],
+    python_requires = '>=3.7',
+    classifiers = [
+        # 패키지에 대한 태그
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+)
