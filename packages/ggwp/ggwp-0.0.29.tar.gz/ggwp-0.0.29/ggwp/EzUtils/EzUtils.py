e713592bb2b5af077185460d6d1d63fa9f90ce43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:25 +0000

    Add package ggwp/0.0.29/ggwp-0.0.29.tar.gz

---
+++ packages/ggwp/ggwp-0.0.29.tar.gz/ggwp-0.0.29/ggwp/EzUtils/EzUtils.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+version = "0.0.29"
+
+class EzUtils:
+    def __init__(self):
+        self.__version__ = version
+        pass
+    
+    def hello_world(self):
+        print("hello world!!")
+
+    def get_q1(self,x):
+        return x.quantile(.25)
+
+    def get_q3(self,x):
+        return x.quantile(.75)
