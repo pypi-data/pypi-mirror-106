2974e369275355fd54bf77f16186319990132ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:12 +0000

    Add package django-doctor/2.1.5/django_doctor-2.1.5-cp36-cp36m-manylinux2010_i686.whl

---
+++ packages/django-doctor/django_doctor-2.1.5-cp36-cp36m-manylinux2010_i686.whl/django_doctor/wizard/precache-manifest.10e782340f109b389365299cb7265168.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d40f6b32ce904723d168",
+    "url": "/static/js/runtime~main.d40f6b32.js"
+  },
+  {
+    "revision": "b0c7497917f181f5ad03",
+    "url": "/static/js/main.b0c74979.chunk.js"
+  },
+  {
+    "revision": "211ee9cb34aef5736aa3",
+    "url": "/static/js/3.211ee9cb.chunk.js"
+  },
+  {
+    "revision": "f471c179962e1b7b3515",
+    "url": "/static/js/2.f471c179.chunk.js"
+  },
+  {
+    "revision": "9b9f037bbbfe2dbd2a09",
+    "url": "/static/js/1.9b9f037b.chunk.js"
+  },
+  {
+    "revision": "b0c7497917f181f5ad03",
+    "url": "/static/css/main.d841e3ae.chunk.css"
+  },
+  {
+    "revision": "f471c179962e1b7b3515",
+    "url": "/static/css/2.3f1fae1f.chunk.css"
+  },
+  {
+    "revision": "facbd4ef0c5b25c845a4bdf4b52f7d40",
+    "url": "/index.html"
+  }
+];
---
+++ packages/django-doctor/django_doctor-2.1.5-cp36-cp36m-manylinux2010_i686.whl/django_doctor/wizard/precache-manifest.3638bfd2a905058cb5e8355cc088c3c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "96d371546ede4133b22b",
+    "url": "/static/js/runtime~main.96d37154.js"
+  },
+  {
+    "revision": "b0c7497917f181f5ad03",
+    "url": "/static/js/main.b0c74979.chunk.js"
+  },
+  {
+    "revision": "211ee9cb34aef5736aa3",
+    "url": "/static/js/3.211ee9cb.chunk.js"
+  },
+  {
+    "revision": "f471c179962e1b7b3515",
+    "url": "/static/js/2.f471c179.chunk.js"
+  },
+  {
+    "revision": "6fd17559727e4b67b383",
+    "url": "/static/js/1.6fd17559.chunk.js"
+  },
+  {
+    "revision": "b0c7497917f181f5ad03",
+    "url": "/static/css/main.d841e3ae.chunk.css"
+  },
+  {
+    "revision": "f471c179962e1b7b3515",
+    "url": "/static/css/2.3f1fae1f.chunk.css"
+  },
+  {
+    "revision": "c5108f070536926f19c633d618697ca4",
+    "url": "/index.html"
+  }
+];
