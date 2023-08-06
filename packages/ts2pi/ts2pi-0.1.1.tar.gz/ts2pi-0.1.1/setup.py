1c6c14a75039a700d3f4748e63943611682faae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:36 +0000

    Add package ts2pi/0.1.1/ts2pi-0.1.1.tar.gz

---
+++ packages/ts2pi/ts2pi-0.1.1.tar.gz/ts2pi-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ts2pi',
+    packages=find_packages(include=['ts2pi']),
+    version='0.1.1',
+    description='Python module to transform a time series into a persistence image representation',
+    author='Clement Piat',
+    url="https://github.com/clementpiat/time-series-persistence-image",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'numpy==1.20.3',
+        'gudhi==3.4.1',
+        'sklearn==0.0'
+    ]
+)
