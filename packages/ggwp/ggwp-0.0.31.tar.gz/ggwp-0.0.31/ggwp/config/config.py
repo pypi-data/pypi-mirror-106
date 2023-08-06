9861b07f739c4ce2d9d4d0c273bfc467d4690846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:33 +0000

    Add package ggwp/0.0.31/ggwp-0.0.31.tar.gz

---
+++ packages/ggwp/ggwp-0.0.31.tar.gz/ggwp-0.0.31/ggwp/config/config.py
@@ -0,0 +1,4 @@
+class EzConfig:
+
+    def __init__(self):
+        self.__version__ = "0.0.30"
---
+++ packages/ggwp/ggwp-0.0.31.tar.gz/ggwp-0.0.31/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+import configparser
+
+config_path = './resource/config.cfg'
+# read config
+parser = configparser.ConfigParser()
+parser.read(config_path)
+version = parser['default']['version']
+
+# save README.md as var
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = [
+    "pandas",
+    "numpy",
+    "sklearn",
+    "xgboost",
+]
+
+
+setuptools.setup(
+    name="ggwp", 
+    version=version,
+    license='MIT',
+    author="Pathompol Nilchaikovit",
+    author_email="data.noob.lol@gmail.com",
+    description="Prepare Fast, Analyze Faster",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/datanooblol/ggwp",
+    install_requires=requirements,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
