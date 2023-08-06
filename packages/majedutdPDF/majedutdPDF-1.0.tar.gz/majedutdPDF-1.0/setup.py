fd5b77e59d04b16ca2957c9039d4c52b55ae0d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:24 +0000

    Add package majedutdPDF/1.0/majedutdPDF-1.0.tar.gz

---
+++ packages/majedutdPDF/majedutdPDF-1.0.tar.gz/majedutdPDF-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from setuptools import version, find_packages
+from pathlib import Path
+# it has a function called setup
+# it has some important keywords arg
+# firstone is name remember to make a unique name
+# next one is version set it to 1.0
+# we need a long_description
+# also we need to tell python about the packagse that sre going to be distributed
+# also we need to exclude data and test subfolders
+setuptools.setup(
+    name="majedutdPDF",
+    version=1.0,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=("test", "data"))
+)
