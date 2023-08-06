5988b658f0fa8b4cf5931e27b329d86f5e13c149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:01 +0000

    Add package aiotractive/0.2.1/aiotractive-0.2.1-py3-none-any.whl

---
+++ packages/aiotractive/aiotractive-0.2.1-py3-none-any.whl/aiotractive/tractive.py
@@ -0,0 +1,38 @@
+"""Entrypoint for the Tractive REST API."""
+
+from .api import API
+from .channel import Channel
+from .trackable_object import TrackableObject
+from .tracker import Tracker
+
+
+class Tractive:
+    def __init__(self, *args, **kwargs):
+        """Initialize the client."""
+        self._api = API(*args, **kwargs)
+
+    async def trackers(self):
+        trackers = await self._api.request(f"user/{await self._api.user_id()}/trackers")
+        return [Tracker(self._api, t) for t in trackers]
+
+    def tracker(self, tracker_id):
+        return Tracker(self._api, {"_id": tracker_id, "_type": "tracker"})
+
+    async def trackable_objects(self):
+        objects = await self._api.request(f"user/{await self._api.user_id()}/trackable_objects")
+        return [TrackableObject(self._api, t) for t in objects]
+
+    def events(self):
+        return Channel(self._api).listen()
+
+    async def close(self):
+        """Close open client session."""
+        await self._api.close()
+
+    async def __aenter__(self):
+        """Async enter."""
+        return self
+
+    async def __aexit__(self, *exc_info):
+        """Async exit."""
+        await self.close()
