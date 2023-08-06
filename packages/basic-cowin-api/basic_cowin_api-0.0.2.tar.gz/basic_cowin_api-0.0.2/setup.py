cb3c8a8eb9f62d1d6cc512d8ae7c0f515f5e5ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:44 +0000

    Add package basic-cowin-api/0.0.2/basic_cowin_api-0.0.2.tar.gz

---
+++ packages/basic-cowin-api/basic_cowin_api-0.0.2.tar.gz/basic_cowin_api-0.0.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name="basic_cowin_api",
+    packages=['cowin_api'],
+    license='GNU 3.0',
+    version="0.0.2",
+    description="Python wrapper for Indian CoWin API"
+                "https://apisetu.gov.in/public/marketplace/api/cowin/cowin-public-v2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Mayank Johri",
+    author_email="mayankjohri@gmail.com",
+
+    url="https://gitlab.com/mayankjohri/cowin_api",
+    keywords=["cowin, covid, vaccine"],
+    include_package_data=True,
+    python_requires=">=3.6",
+
+    install_requires=[
+        "pytest",
+        "requests"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+
+    project_urls={
+        "Bug Reports": "https://gitlab.com/mayankjohri/cowin_api/-/issues",
+        "Source": "https://gitlab.com/mayankjohri/cowin_api",
+    },
+)
