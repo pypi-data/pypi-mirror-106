d1b3e04d1a9952ca71752cc718d35fb5654f7b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:05 +0000

    Add package relmod/0.0.0/relmod-0.0.0.zip

---
+++ packages/relmod/relmod-0.0.0.zip/relmod-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='relmod',
+    version='0.0.0',
+    author='Roger D. Serwy',
+    author_email='roger.serwy@gmail.com',
+    url="http://github.com/serwy/relmod",
+    packages=['relmod'],
+    description='relmod',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
