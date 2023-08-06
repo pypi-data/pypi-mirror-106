4b6f1fbca2e88127361fc1911f8139fd5cd0d5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:10 +0000

    Add package pylucky/1.1/pylucky-1.1-py3-none-any.whl

---
+++ packages/pylucky/pylucky-1.1-py3-none-any.whl/pylucky/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+create by:DY_XiaoDong
+
+Contect us:
+
+Wechat: KS_XiaoDong
+
+QQ: 2661009083
+
+E-mail: xiaodong.kf@foxmail.com
+
+"""
+
+import random
+
+"""
+
+---------------------
+    coding part
+---------------------
+
+"""
+
+def choice(person):
+    lucky = random.choice(person)
+    return lucky
+
+def number(small,big):
+    lucky = random.randint(small,big)
+    return lucky
