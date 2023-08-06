dceb1d7f59763791f9261df342fd52d9edf6afcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:33 +0000

    Add package konoha/4.6.5/konoha-4.6.5.tar.gz

---
+++ packages/konoha/konoha-4.6.5.tar.gz/konoha-4.6.5/konoha/word_tokenizers/character_tokenizer.py
@@ -0,0 +1,12 @@
+from konoha.data.token import Token
+from konoha.word_tokenizers.tokenizer import BaseTokenizer
+
+
+class CharacterTokenizer(BaseTokenizer):
+    """Characterkonoha.tokenizer"""
+
+    def __init__(self):
+        super().__init__(name="character")
+
+    def tokenize(_, text: str):
+        return [Token(surface=char) for char in list(text)]
