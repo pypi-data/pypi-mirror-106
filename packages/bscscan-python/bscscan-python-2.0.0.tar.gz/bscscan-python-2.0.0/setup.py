80c621ba8e77082cb4457b8c901cc56853e9e853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:36 +0000

    Add package bscscan-python/2.0.0/bscscan-python-2.0.0.tar.gz

---
+++ packages/bscscan-python/bscscan-python-2.0.0.tar.gz/bscscan-python-2.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="bscscan-python",
+    version="2.0.0",
+    description="A python API for bscscan.com.",
+    url="https://github.com/pcko1/bscscan-python",
+    author="Panagiotis-Christos Kotsias",
+    author_email="kotsias.pan@gmail.com",
+    license="MIT",
+    packages=[
+        "bscscan",
+        "bscscan.configs",
+        "bscscan.core",
+        "bscscan.enums",
+        "bscscan.modules",
+        "bscscan.utils",
+    ],
+    install_requires=["aiohttp", "requests"],
+    include_package_data=True,
+    zip_safe=False,
+)
