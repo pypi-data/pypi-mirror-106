dd5cfc12232c0b44ef8e084793296826fd02b68a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:00 +0000

    Add package chameleontest/1.0/chameleontest-1.0.tar.gz

---
+++ packages/chameleontest/chameleontest-1.0.tar.gz/chameleontest-1.0/setup.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+import setuptools
+setuptools.setup(
+    name="chameleontest",
+    version="1.0",
+    author="jiaxing",
+    py_modules=["chameleontest"]
+)
