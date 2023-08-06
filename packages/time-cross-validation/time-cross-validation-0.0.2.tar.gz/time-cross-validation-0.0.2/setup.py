c4e6b8085bb5f23bff22ba280adb0db4d4835be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:08 +0000

    Add package time-cross-validation/0.0.2/time-cross-validation-0.0.2.tar.gz

---
+++ packages/time-cross-validation/time-cross-validation-0.0.2.tar.gz/time-cross-validation-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'time-cross-validation',
+    version = '0.0.2',
+    description = 'Time series cross validation tool, supplementing or replacing relevant existing sklearn libraries.',
+    long_description = open('README.md').read(),
+    long_description_content_type = "text/markdown",
+    url = '',
+    author = 'Riccardo Doyle',
+    #py_modules = ["time_cross_validation"],
+    author_email = 'riccardo.doyle.14@ucl.ac.uk',
+    keywords = 'cross validation',
+    packages = find_packages()
+)
