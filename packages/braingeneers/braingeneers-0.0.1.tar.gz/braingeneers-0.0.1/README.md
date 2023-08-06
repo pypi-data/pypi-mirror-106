1d27793d70f1698cf699d40b33c49a15a47a74a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:53 +0000

    Add package braingeneers/0.0.1/braingeneers-0.0.1.tar.gz

---
+++ packages/braingeneers/braingeneers-0.0.1.tar.gz/braingeneers-0.0.1/README.md
@@ -0,0 +1,5 @@
+# Braingeneers Python Utilities
+
+This package is supposed to collect, as well as make installable
+through Pip, all of the Python code and utilities that we develop as
+part of the wetAI project.
---
+++ packages/braingeneers/braingeneers-0.0.1.tar.gz/braingeneers-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='braingeneers',
+    version='0.0.1',
+    description='Braingeneers Python utilities',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/braingeneers/braingeneers',
+    author='Braingeneers',
+    keywords=['machine-learning', 'jupyter', 'neuroscience', 'wetai', 'braingeneers'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    packages=find_packages(exclude=()),
+    install_requires=['numpy', 'scipy', 'requests'],
+    include_package_data=True)
