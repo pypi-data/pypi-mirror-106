6c804d78fd5c22e63e9ad698a24b8cd760b2a096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:06 +0000

    Add package auto-usage/1.0.1/auto_usage-1.0.1-py3-none-any.whl

---
+++ packages/auto-usage/auto_usage-1.0.1-py3-none-any.whl/auto_usage/__init__.py
@@ -0,0 +1,7 @@
+"""Auto Usage - A python tool for auto-generating or auto-updating usage of python cli tools."""
+
+__version__ = "1.0.1"
+__author__ = """zhangxianbing"""
+__email__ = "zhangxianbing09@163.com"
+
+prog_name = "auto-usage"
