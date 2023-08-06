6c795dc458433d7f95122f5ad4cc909eeb3fa215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:15 +0000

    Add package nabu/2021.1.0/nabu-2021.1.0.tar.gz

---
+++ packages/nabu/nabu-2021.1.0.tar.gz/nabu-2021.1.0/nabu/misc/filters.py
@@ -0,0 +1,27 @@
+import numpy as np
+try:
+    import scipy.signal
+    __have_scipy__ = True
+except ImportError:
+    __have_scipy__ = False
+
+def correct_spikes(image, threshold):
+    """
+    Perform a conditional median filtering
+    The filtering is done in-place, meaning that the array content is modified.
+
+    Parameters
+    ----------
+    image: numpy.ndarray
+        Image to filter
+    threshold: float
+        Median filter threshold
+    """
+    m_im = scipy.signal.medfilt2d(image)
+    fixed_part = np.array(image[[0, 0, -1, -1], [0, -1, 0, -1]])
+
+    where = abs(image - m_im) > threshold
+    image[where] = m_im[where]
+    image[[0, 0, -1, -1], [0, -1, 0, -1]] = fixed_part
+
+    return image
