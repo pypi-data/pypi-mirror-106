798fe39dde3f82252134e30fd705a42778428956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:23 +0000

    Add package awyes/2.1.1/awyes-2.1.1.tar.gz

---
+++ packages/awyes/awyes-2.1.1.tar.gz/awyes-2.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['awyes']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=5.4.1,<6.0.0',
+ 'boto3>=1.17.62,<2.0.0',
+ 'docker>=5.0.0,<6.0.0',
+ 'requests>=2.25.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'awyes',
+    'version': '2.1.1',
+    'description': 'Simplify aws deployment',
+    'long_description': None,
+    'author': 'trumanpurnell',
+    'author_email': 'truman.purnell@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
