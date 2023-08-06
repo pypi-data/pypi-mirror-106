24ba6d61b634d57739f6b04ea4e96c5412a41fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:53 +0000

    Add package ToolXmind2Testcase-dev/0.0.2/ToolXmind2Testcase_dev-0.0.2.tar.gz

---
+++ packages/ToolXmind2Testcase-dev/ToolXmind2Testcase_dev-0.0.2.tar.gz/ToolXmind2Testcase_dev-0.0.2/ToolXmind2Testcase/test.py
@@ -0,0 +1,18 @@
+import sys,click
+
+@click.command()
+@click.option('-x', '--xmindpath', 'config', required=True, help='xmind file path')
+@click.option('-e', '--excelpath', 'config', required=True, help='excel file path')
+@click.option('-i', '--ignorelayer', 'config', is_flag=True, default='0')
+# @click.option('--pretty', 'pretty', is_flag=True, default=False, help='pretty print json')
+def main(xmindPath, excelPath, ignorelayer):
+    test(xmindPath, excelPath, ignorelayer)
+
+
+def test(xmindPath, excelPath, ignorelayer):
+    print(xmindPath + 'haha' + excelPath + 'haha' + ignorelayer)
+    # if ignorelayer == '0':
+    #     ConvertXmindToExcel().convert(xmindPath, excelPath)
+    # else:
+    #     ConvertXmindToExcel().convert(xmindPath, excelPath, ignorelayer)
+    sys.exit()
