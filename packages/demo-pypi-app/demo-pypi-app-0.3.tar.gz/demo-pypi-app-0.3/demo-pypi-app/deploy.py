6e819d6f75c2536d62bd354fe7ecc9a3b4492259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:05 +0000

    Add package demo-pypi-app/0.3/demo-pypi-app-0.3.tar.gz

---
+++ packages/demo-pypi-app/demo-pypi-app-0.3.tar.gz/demo-pypi-app-0.3/demo-pypi-app/deploy.py
@@ -0,0 +1,3 @@
+def status():
+  text = input()
+  return "hello!" + text + " , pleasure to meet you, this was a simple python library to check github actions, more to come soon" + ", Thanks for installing"
