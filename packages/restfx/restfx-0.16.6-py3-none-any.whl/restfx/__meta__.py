4592d5e636cd684168027571bf357a2fbf3ef5ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:33 +0000

    Add package restfx/0.16.6/restfx-0.16.6-py3-none-any.whl

---
+++ packages/restfx/restfx-0.16.6-py3-none-any.whl/restfx/__meta__.py
@@ -0,0 +1,3 @@
+name = 'restfx'
+version = '0.16.6'
+website = 'https://gitee.com/hyjiacan/restfx'
---
+++ packages/restfx/restfx-0.16.6-py3-none-any.whl/restfx/internal_assets/scripts/xhr.js
@@ -0,0 +1,88 @@
+(function () {
+  function serializeParams(url, params) {
+    var temp = []
+    for (var key in params) {
+      temp.push(key + '=' + params[key])
+    }
+    if (url.indexOf('?') === -1) {
+      url += '?'
+    }
+    url += '&' + temp.join('&')
+    return url
+  }
+
+  function request(method, url, options) {
+    var xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        getResponse(xhr, options.callback)
+      }
+    }
+    if (options.param) {
+      url = serializeParams(url, options.param)
+    }
+    xhr.responseType = 'arraybuffer'
+    xhr.open(method.toUpperCase(), url, true)
+    xhr.setRequestHeader('accept', 'application/json;text/*;image/*;*/*')
+    xhr.setRequestHeader('x-requested-with', 'XMLHttpRequest')
+    xhr.send(options.data)
+  }
+
+  function decodeResponse(data, asText, callback) {
+    var reader = new FileReader()
+    var blob = new Blob([data])
+    if (asText) {
+      reader.readAsText(blob)
+    } else {
+      reader.readAsDataURL(blob)
+    }
+    reader.onload = function () {
+      callback(reader.result)
+    }
+  }
+
+  /**
+   *
+   * @param {XMLHttpRequest} xhr
+   * @param callback
+   * @returns {{headers: {}, data: string}}
+   */
+  function getResponse(xhr, callback) {
+    var data = xhr.response
+    var headers = Object.create(null)
+    xhr
+      .getAllResponseHeaders()
+      .split('\r\n')
+      .forEach(function (item) {
+        var temp = item.trim().split(':')
+        if (!temp[0]) {
+          return
+        }
+        headers[temp[0].trim()] = (temp[1] || '').trim()
+      })
+    var contentType = headers['content-type']
+    if (!contentType) {
+      headers['content-type'] = contentType = ''
+    }
+
+    var isText = contentType.indexOf('text/') !== -1 || contentType.indexOf('/json') !== -1
+
+    decodeResponse(data, isText, function (data) {
+      if (isText) {
+        try {
+          data = JSON.parse(data)
+        } catch (e) {
+        }
+      }
+      callback({
+        data: data,
+        isText: isText,
+        headers: headers,
+        status: xhr.status,
+        statusText: xhr.statusText
+      })
+    })
+  }
+
+  window.xhr = request
+})()
