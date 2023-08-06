1fc5b1d13e03d3d831fe255c42173b670e04fa5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:34 +0000

    Add package ConfigFileHelper/0.0.1/ConfigFileHelper-0.0.1.tar.gz

---
+++ packages/ConfigFileHelper/ConfigFileHelper-0.0.1.tar.gz/ConfigFileHelper-0.0.1/README.md
@@ -0,0 +1,8 @@
+# Config
+
+Python helper library for dealing with config files in the following formats
+
+- YAML
+- JSON
+- XML
+- INI
---
+++ packages/ConfigFileHelper/ConfigFileHelper-0.0.1.tar.gz/ConfigFileHelper-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from pathlib import Path
+
+thisName    = "ConfigFileHelper"
+
+baseURL     = f"https://github.com/theflyingbadger/{thisName}"
+
+setuptools.setup  (name                          =  thisName
+                  ,version                       =  Path('.\\src\\configFileHelper\\VERSION').read_text()
+                  ,author                        =  "Jonathan Mills"
+                  ,author_email                  =  "jon@badger.shoes"
+                  ,description                   =  "Helper for config files"
+                  ,long_description              =  Path("README.md").read_text()
+                  ,long_description_content_type =  "text/markdown"
+                  ,url                           =  baseURL
+                  ,project_urls                  =  {"Bug Tracker": f"{baseURL}/issues"}
+                  ,classifiers                   =  ["Programming Language :: Python :: 3"
+                                                    ,"License :: OSI Approved :: MIT License"
+                                                    ,"Operating System :: OS Independent"
+                                                    ]
+                  ,package_dir                   =  {"": "src"}
+                  ,packages                      =  setuptools.find_packages(where="src")
+                  ,install_requires              =  Path("requirements.txt").read_text()
+                  ,python_requires               =  '>=3.9'
+                  )
