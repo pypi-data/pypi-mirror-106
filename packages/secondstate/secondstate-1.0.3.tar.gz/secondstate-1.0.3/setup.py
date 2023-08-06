353a0d22ab686bbd7109e42a53caa417b7b19b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:53 +0000

    Add package secondstate/1.0.3/secondstate-1.0.3.tar.gz

---
+++ packages/secondstate/secondstate-1.0.3.tar.gz/secondstate-1.0.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['secondstate']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'secondstate',
+    'version': '1.0.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Lewis',
+    'author_email': 'lewis@fruiti.app',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
