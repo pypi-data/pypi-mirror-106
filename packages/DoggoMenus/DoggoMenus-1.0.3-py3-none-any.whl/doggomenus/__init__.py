2fdf559d66e6eda4576cdfd696c996f702532f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:31 +0000

    Add package DoggoMenus/1.0.3/DoggoMenus-1.0.3-py3-none-any.whl

---
+++ packages/DoggoMenus/DoggoMenus-1.0.3-py3-none-any.whl/doggomenus/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+DoggoMenus
+===============
+
+A basic console menu system for Python!
+
+If you have any bugs, feature requests, or just wanna check out the source code, you can go to the GitHub!
+"""
+
+__title__ = 'doggomenus'
+__author__ = 'Doggo'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 Doggo'
+__version__ = '1.0.0'
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+from .selection import *
+from .command import *
