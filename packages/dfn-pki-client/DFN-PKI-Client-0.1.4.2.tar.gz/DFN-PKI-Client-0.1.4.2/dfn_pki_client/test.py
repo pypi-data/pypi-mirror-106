6b0adb65205958a82a7af979ba1c4f98bca24d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:51 +0000

    Add package dfn-pki-client/0.1.4.2/DFN-PKI-Client-0.1.4.2.tar.gz

---
+++ packages/dfn-pki-client/DFN-PKI-Client-0.1.4.2.tar.gz/DFN-PKI-Client-0.1.4.2/dfn_pki_client/test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import click
+
+from public_service import PublicServicePKI
+from registration_service import RegistrationService
+from domain_service import DomainService
+from utils import get_wsdl
+
+
+@click.command()
+@click.version_option()
+def main():
+
+    proxy = {"http": "http://proxy.charite.de:8080", "https": "http://proxy.charite.de:8080"}
+
+    pki = PublicServicePKI("../config.ini", proxy=proxy)
+    ca_info = pki.get_ca_info(510)
+
+    # print(ca_info)
+
+    with open("PN_Robert_Graetz_-_Charite_Teilnehmerservice_Berlin_2021-01-04.p12", 'rb') as pkcs12_file:
+        pkcs12_data = pkcs12_file.read()
+
+        print(pkcs12_data)
+        print(type(pkcs12_data))
+    pkcs12_password = "5+k,%yyy"
+    rs = RegistrationService("../config.ini", proxy, pkcs12_data, pkcs12_password)
+
+    print(rs.get_ca_info())
+
+    result = rs.search_items_2("certificate", "VALID", None, 510, None, 1)
+    # print(result)
+
+    ds = DomainService("../config.ini", proxy, pkcs12_data, pkcs12_password)
+    result = ds.list_domains(510)
+    # print(result)
+
+    # return obj.DFNCERTTypesDomainListResult(res.Change, results, acl)
+    domain_list_results = ds.list_extended_domains(510)
+    test = domain_list_results.result
+    for domain in test:
+        print(domain)
+        print(type(domain))
+        print(domain.name)
+        print(domain.type)
+        print(domain.secret)
+        print(domain.approved)
+        print(domain.approved_date)
+
+
+
+if __name__ == '__main__':
+    main()
