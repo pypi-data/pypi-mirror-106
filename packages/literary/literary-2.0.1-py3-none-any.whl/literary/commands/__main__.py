5c4db68a4de6d35fad5339f6baef0a23be50b3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:16 +0000

    Add package literary/2.0.1/literary-2.0.1-py3-none-any.whl

---
+++ packages/literary/literary-2.0.1-py3-none-any.whl/literary/commands/__main__.py
@@ -0,0 +1,6 @@
+
+"""# Entrypoint
+"""
+from .launcher import LiteraryLauncher, launch_new_instance
+if ((__name__ == '__main__') and (not LiteraryLauncher.initialized())):
+    launch_new_instance()
