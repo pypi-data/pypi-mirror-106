8433854bc5627de9c44a0a7fd7cde70bfb7b9c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:59 +0000

    Add package majedutdPDF/1.1/majedutdPDF-1.1-py3-none-any.whl

---
+++ packages/majedutdPDF/majedutdPDF-1.1-py3-none-any.whl/majedpdf/pdf2text.py
@@ -0,0 +1,16 @@
+""" This module converts pdf into text """
+
+
+class Converter:
+    """ A simple converter to convert PDFs to Text"""
+
+    def convert(self, path):
+        """ Converts given PDF to Text
+
+        Parameters:
+        path (str): The path to a pdf file
+
+        Returns:
+        str: The content of a PDF file as Text
+        """
+        print("pdf to text...")
