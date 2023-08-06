e546f005e1725b79776c0e502e769052afca9f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:36 +0000

    Add package psat-server-web/0.0.6/psat-server-web-0.0.6.tar.gz

---
+++ packages/psat-server-web/psat-server-web-0.0.6.tar.gz/psat-server-web-0.0.6/psat_server_web/atlas/generate_mod_wsgi_apachectl.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Create a setup script and immediately start the apache instance.  Our URL prefix
+# is specified by the --mount-point setting.  We need to specify a PYTHONPATH before
+# starting the apache instance. Run this script from THIS directory.
+
+if [ -f ~/.config/django/django_env_$CONDA_DEFAULT_ENV ]; then chmod 600 ~/.config/django/django_env_$CONDA_DEFAULT_ENV; source ~/.config/django/django_env_$CONDA_DEFAULT_ENV; fi
+
+export APACHEPATH="/tmp/atlas"
+
+if [ $DJANGO_MYSQL_DBNAME ]
+then
+    export APACHEPATH=/tmp/$DJANGO_MYSQL_DBNAME
+fi
+
+export PORT=8086
+if [ $WSGI_PORT ]
+then
+    export PORT=$WSGI_PORT
+fi
+
+if [ -f $APACHEPATH/apachectl ]; then
+    echo "Stopping Apache if already running"
+    $APACHEPATH/apachectl stop
+    sleep 1
+    # wait a second to make sure the port is released
+else
+    echo "Creating directory $APACHEPATH"
+    mkdir -p APACHEPATH
+fi
+
+mod_wsgi-express setup-server --working-directory atlas --url-alias /static static --url-alias /media media --application-type module atlas.wsgi --server-root $APACHEPATH --port $PORT --mount-point /
+
+export PYTHONPATH=$(pwd)
+$APACHEPATH/apachectl start
