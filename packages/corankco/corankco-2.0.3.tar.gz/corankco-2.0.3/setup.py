4add4fe62e261b10e0cbe89a246f4390516a95a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:47 +0000

    Add package corankco/2.0.3/corankco-2.0.3.tar.gz

---
+++ packages/corankco/corankco-2.0.3.tar.gz/corankco-2.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='corankco',
+      version='2.0.3',
+      description='Kemeny-Young method for rank aggregation of incomplete rankings with ties',
+      long_description=readme(),
+      url='https://github.com/pierreandrieu/corankco',
+      author='Pierre Andrieu',
+      author_email='pierre.andrieu@lilo.org',
+      license='MIT',
+      packages=find_packages(include=['corankco', 'corankco.*']),
+      package_data={'corankco': ['data/*']},
+      python_requires='>=3',
+      zip_safe=False,
+      install_requires=['numpy',
+                        'python-igraph',
+                        'pulp==2.3',
+                        'bioconsertinc>=0.0.9',
+                        ]
+      )
