37d6c388ad169fea5669a580e0ee8c87a1ef524f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:48 +0000

    Add package coord-generator/0.0.4/coord_generator-0.0.4.tar.gz

---
+++ packages/coord-generator/coord_generator-0.0.4.tar.gz/coord_generator-0.0.4/README.txt
@@ -0,0 +1,4 @@
+This programme can generate random coordinations 
+very simple and easy.
+
+view code at : https://github.com/DevER-M/coord_generator
---
+++ packages/coord-generator/coord_generator-0.0.4.tar.gz/coord_generator-0.0.4/coord_generator/__init__.py
@@ -0,0 +1,12 @@
+import random
+def generate_coordinates(min_num,max_num):
+ for coords in range(1):
+    x = random.randint(min_num,max_num)
+    y = random.randint(min_num,max_num)
+    z = random.randint(min_num,max_num)
+    return x,y,z
+def generate_XandY_coordinates(min_num,max_num,):
+ for coords in range(1):
+   x = random.randint(min_num,max_num)
+   y = random.randint(min_num,max_num)
+   return x,y
