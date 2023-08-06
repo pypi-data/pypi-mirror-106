43ec6b44e0b581ca89bc56853bd9424955c63d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:45 +0000

    Add package anoapycore/0.1.4/anoapycore-0.1.4-py3-none-any.whl

---
+++ packages/anoapycore/anoapycore-0.1.4-py3-none-any.whl/anoapycore/ml/__init__.py
@@ -0,0 +1,12 @@
+# ml
+
+from . import data
+# classification
+from . import logistic_regression
+from . import support_vector_classifier
+# regression
+from . import linear_regression
+# clustering
+from . import clustering
+# association
+from . import association
