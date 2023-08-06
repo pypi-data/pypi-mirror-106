eb6f722789d4c33a4d8ea584fd8c7a7b24410188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:13 +0000

    Add package cdk-lambda-subminute/0.1.2/cdk_lambda_subminute-0.1.2.tar.gz

---
+++ packages/cdk-lambda-subminute/cdk_lambda_subminute-0.1.2.tar.gz/cdk_lambda_subminute-0.1.2/README.md
@@ -0,0 +1,10 @@
+# cdk-lambda-subminute
+
+This construct creates a state machine that can invoke a Lambda function per time unit which can be less than one minute. You only need to craft a Lambda function and then assign it as an argument into the construct. An example is included.
+
+https://docs.aws.amazon.com/cdk/api/latest/docs/aws-s3-assets-readme.html#asset-bundling
+https://github.com/aws/aws-cdk/issues/8589
+https://github.com/aws/aws-cdk/issues/14516
+https://github.com/aws/aws-cdk/issues/11914
+
+https://github.com/aws/aws-cdk/blob/master/packages/%40aws-cdk/core/lib/asset-staging.ts
---
+++ packages/cdk-lambda-subminute/cdk_lambda_subminute-0.1.2.tar.gz/cdk_lambda_subminute-0.1.2/src/cdk_lambda_subminute/_jsii/__init__.py
@@ -0,0 +1,31 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_stepfunctions._jsii
+import aws_cdk.aws_stepfunctions_tasks._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "projen-statemachine-example",
+    "0.1.2",
+    __name__[0:-6],
+    "projen-statemachine-example@0.1.2.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
