28781e96ac88a497b036a01cf26fc4612b9d416c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:23 +0000

    Add package diemlib/1.0.3/diemlib-1.0.3-py3-none-any.whl

---
+++ packages/diemlib/diemlib-1.0.3-py3-none-any.whl/diemlib/util.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# diemlib/main.py
+
+"""
+   These are some utilities
+"""
+
+import random
+import string
+
+# some other utilities
+
+
+def get_random_string(length=10):
+    # Random string with the combination of lower and upper case
+    letters = string.ascii_letters
+    return "".join(random.choice(letters) for i in range(length))
+
+
+def guid():
+    # Creates 36 UUID
+    import uuid
+
+    return str(uuid.uuid1())
+
+# split a pandas dataframe in chuncks
+
+
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
