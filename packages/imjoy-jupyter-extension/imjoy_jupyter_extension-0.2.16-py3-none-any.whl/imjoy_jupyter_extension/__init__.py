37f36871fa21abe319e074585ea5221ccc0f4fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:24 +0000

    Add package imjoy-jupyter-extension/0.2.16/imjoy_jupyter_extension-0.2.16-py3-none-any.whl

---
+++ packages/imjoy-jupyter-extension/imjoy_jupyter_extension-0.2.16-py3-none-any.whl/imjoy_jupyter_extension/__init__.py
@@ -0,0 +1,10 @@
+# Jupyter Extension points
+def _jupyter_nbextension_paths():
+    return [
+        dict(
+            section="notebook",
+            src="static",
+            # directory in the `nbextension/` namespace
+            dest="imjoy_jupyter_extension",
+        )
+    ]
