2f7040342c70c6ca057c2eee80ed48cf28ccd7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:18 +0000

    Add package pywhat/0.2.4/pywhat-0.2.4.tar.gz

---
+++ packages/pywhat/pywhat-0.2.4.tar.gz/pywhat-0.2.4/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pywhat']
+
+package_data = \
+{'': ['*'], 'pywhat': ['Data/*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0',
+ 'langdetect>=1.0.8,<2.0.0',
+ 'name_that_hash>=1.7.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['pywhat = pywhat.what:main', 'what = pywhat.what:main']}
+
+setup_kwargs = {
+    'name': 'pywhat',
+    'version': '0.2.4',
+    'description': 'What is that thing?',
+    'long_description': None,
+    'author': 'Bee',
+    'author_email': 'github@skerritt.blog',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
