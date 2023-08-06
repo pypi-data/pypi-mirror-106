a560b7608f7176a6d9b0b9eb681450da10eef4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:15 +0000

    Add package atari-py/0.2.7/atari_py-0.2.7-cp36-cp36m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/atari-py/atari_py-0.2.7-cp36-cp36m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/atari_py/ale_interface/build/CMakeFiles/ale-c-lib.dir/src/emucore/SpeakJet.cxx.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/ale-c-lib.dir/src/emucore/SpeakJet.cxx.o: \
+ /tmp/pip-req-build-uon6xkba/build/lib.linux-aarch64-3.6/atari_py/ale_interface/src/emucore/SpeakJet.cxx \
+ /usr/include/stdc-predef.h
