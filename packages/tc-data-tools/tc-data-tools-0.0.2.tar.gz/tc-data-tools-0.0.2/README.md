f9ffd0112fd30639caa4065b8e5ceebbfbf391b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:39 +0000

    Add package tc-data-tools/0.0.2/tc-data-tools-0.0.2.tar.gz

---
+++ packages/tc-data-tools/tc-data-tools-0.0.2.tar.gz/tc-data-tools-0.0.2/README.md
@@ -0,0 +1,6 @@
+# TC Data Tools
+
+Package to reuse and simplify common tasks for developing data products and machine learning models as:
+- Connect and extract data from databases
+- IO functions to datalake
+- Machine learning tasks
---
+++ packages/tc-data-tools/tc-data-tools-0.0.2.tar.gz/tc-data-tools-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.2"
+
+setup(
+    name="tc-data-tools",
+    version=VERSION,
+    description="Data tools to help our daily job",
+    url="https://github.com/ciro-tc/tc-data-tools",
+    author="TC Data Science",
+    author_email="ciro.oliveira@tc.com.br",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "pandas==1.2.4",
+        "pandas-gbq==0.15.0",
+        "google-cloud-bigquery==2.15.0",
+        "google-cloud-storage==1.38.0",
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
