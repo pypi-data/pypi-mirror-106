cb8172eb63f72b612501c81cdf8d75181ad17045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:01 +0000

    Add package goblet-gcp/0.4.7/goblet-gcp-0.4.7.tar.gz

---
+++ packages/goblet-gcp/goblet-gcp-0.4.7.tar.gz/goblet-gcp-0.4.7/goblet/resources/http.py
@@ -0,0 +1,24 @@
+from goblet.handler import Handler
+
+
+class HTTP(Handler):
+    """Http Trigger"""
+    def __init__(self, http=None):
+        self.http = http
+
+    def register_http(self, func):
+        self.http = func
+
+    def __call__(self, request, context=None):
+        return self.http(request)
+
+    def __add__(self, other):
+        if other.http:
+            self.http = other.http
+        return self
+
+    def deploy(self, sourceUrl=None, entrypoint=None):
+        return
+
+    def destroy(self):
+        return
