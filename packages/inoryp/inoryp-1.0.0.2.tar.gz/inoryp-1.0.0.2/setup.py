95c708cb028b881b5f49a581a9b0ba7942017e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:57 +0000

    Add package inoryp/1.0.0.2/inoryp-1.0.0.2.tar.gz

---
+++ packages/inoryp/inoryp-1.0.0.2.tar.gz/inoryp-1.0.0.2/setup.py
@@ -0,0 +1,18 @@
+# 30 04 2021
+
+from setuptools import setup
+
+setup(
+    name="inoryp",
+    version="1.0.0.2",
+    license="MIT",
+    
+    url="https://github.com/R0htg0r/inoryp",
+    
+    author="R0htg0r",
+    author_email="osvaldo.pedreiro.ecaminhoneiro@gmail.com",
+    
+    description="Python3, biblioteca para retornar seu endereço IP, com facilidade.",
+    packages=["inoryp"],
+    
+)
