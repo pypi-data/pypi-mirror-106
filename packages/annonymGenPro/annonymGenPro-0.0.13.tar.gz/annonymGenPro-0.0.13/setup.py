d6acb789048afdcc8a4cf22c074ca0bbde0fb7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:25 +0000

    Add package annonymGenPro/0.0.13/annonymGenPro-0.0.13.tar.gz

---
+++ packages/annonymGenPro/annonymGenPro-0.0.13.tar.gz/annonymGenPro-0.0.13/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.13'
+DESCRIPTION = 'A cool module for use'
+
+setup(
+    name="annonymGenPro",
+    version=VERSION,
+    author="HushWatcher",
+    author_email="<thisismail@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
