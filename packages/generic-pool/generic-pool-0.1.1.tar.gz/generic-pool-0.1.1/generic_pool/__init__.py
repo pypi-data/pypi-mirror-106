0f7535abdf0b3ca2de5f43b2a7a7cd96c9f3c65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:39 +0000

    Add package generic-pool/0.1.1/generic-pool-0.1.1.tar.gz

---
+++ packages/generic-pool/generic-pool-0.1.1.tar.gz/generic-pool-0.1.1/generic_pool/__init__.py
@@ -0,0 +1,5 @@
+from .errors import Invalid, UnableToCreateValidObject, Unmanaged
+from .factory import Factory
+from .pool import Pool
+
+__all__ = ["Invalid", "UnableToCreateValidObject", "Factory", "Pool"]
