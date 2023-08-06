4b4e5a078f44a2e50783a83e3b573c4597b4e53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:23 +0000

    Add package hans/0.0.3/hans-0.0.3-py3-none-any.whl

---
+++ packages/hans/hans-0.0.3-py3-none-any.whl/hans/stress.py
@@ -0,0 +1,88 @@
+"""
+MIT License
+
+Copyright 2021 Hannes Holey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+from hans.field import VectorField, TensorField
+from hans.material import Material
+
+
+class SymStressField2D(VectorField):
+
+    def __init__(self, disc, geometry, material):
+
+        super().__init__(disc)
+
+        self.geometry = geometry
+        self.material = material
+
+    def set(self, q, h):
+
+        U = self.geometry['U']
+        V = self.geometry['V']
+        eta = Material(self.material).viscosity(U, V, q[0], h[0])
+        zeta = self.material['bulk']
+
+        visc_1 = zeta + 4 / 3 * eta
+        visc_2 = zeta - 2 / 3 * eta
+
+        # origin bottom, U_top = 0, U_bottom = U
+        self.field[0] = -((U * q[0] - 3 * q[1]) * visc_1 * h[1] + (V * q[0] - 3 * q[2]) * visc_2 * h[2]) / (h[0] * q[0])
+        self.field[1] = -((V * q[0] - 3 * q[2]) * visc_1 * h[2] + (U * q[0] - 3 * q[1]) * visc_2 * h[1]) / (h[0] * q[0])
+        self.field[2] = -eta * ((V * q[0] - 3 * q[2]) * h[1] + (U * q[0] - 3 * q[1]) * h[2]) / (h[0] * q[0])
+
+
+class SymStressField3D(TensorField):
+
+    def __init__(self, disc, geometry, material):
+
+        super().__init__(disc)
+
+        self.geometry = geometry
+        self.material = material
+
+    def set(self, q, h, bound):
+
+        U = self.geometry['U']
+        V = self.geometry['V']
+        eta = Material(self.material).viscosity(U, V, q[0], h[0])
+        zeta = self.material['bulk']
+
+        visc_1 = zeta + 4 / 3 * eta
+        visc_2 = zeta - 2 / 3 * eta
+
+        if bound == "top":
+
+            # origin bottom, U_top = 0, U_bottom = U
+            self.field[0] = -2 * ((U * q[0] - 3 * q[1]) * visc_1 * h[1] + (V * q[0] - 3 * q[2]) * visc_2 * h[2]) / (h[0] * q[0])
+            self.field[1] = -2 * ((V * q[0] - 3 * q[2]) * visc_1 * h[2] + (U * q[0] - 3 * q[1]) * visc_2 * h[1]) / (h[0] * q[0])
+            self.field[2] = -2 * visc_2 * ((U * q[0] - 3 * q[1]) * h[1] + (V * q[0] - 3 * q[2]) * h[2]) / (h[0] * q[0])
+            self.field[3] = 2 * eta * (V * q[0] - 3 * q[2]) / (q[0] * h[0])
+            self.field[4] = 2 * eta * (U * q[0] - 3 * q[1]) / (q[0] * h[0])
+            self.field[5] = -2 * eta * ((V * q[0] - 3 * q[2]) * h[1] + h[2] * (U * q[0] - 3 * q[1])) / (q[0] * h[0])
+
+        elif bound == "bottom":
+
+            # origin bottom, U_top = 0, U_bottom = U
+            self.field[3] = -2 * eta * (2 * V * q[0] - 3 * q[2]) / (q[0] * h[0])
+            self.field[4] = -2 * eta * (2 * U * q[0] - 3 * q[1]) / (q[0] * h[0])
