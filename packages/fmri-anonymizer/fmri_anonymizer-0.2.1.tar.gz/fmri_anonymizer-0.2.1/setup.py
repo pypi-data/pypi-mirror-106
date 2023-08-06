b099da3e0c12ec2c27ff847bb85f1d1a0b035921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:07 +0000

    Add package fmri-anonymizer/0.2.1/fmri_anonymizer-0.2.1.tar.gz

---
+++ packages/fmri-anonymizer/fmri_anonymizer-0.2.1.tar.gz/fmri_anonymizer-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(name="fmri_anonymizer",
+      version='0.2.1',
+      __version__='0.2.1',
+      description="Anonymize your DICOM and NIFTI files with this tool easily.",
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      url="https://caoslab.psy.cmu.edu:32443/hugoanda/fmri_anonymizer",
+      author="Hugo Angulo",
+      author_email="hugoanda@andrew.cmu.edu",
+      license="MIT",
+      classifiers=["License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.7", ],
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={'': ['util/deid.dicom']},
+      install_requires=["deid==0.2.12", "dicom2nifti", "pydeface", "numpy", "nibabel", "nipype", "ttictoc"],
+      entry_points={"console_scripts": ["fmri_anonymizer=fmri_anonymizer.__main__:main", ]},
+      python_requires='>=3.7', )
