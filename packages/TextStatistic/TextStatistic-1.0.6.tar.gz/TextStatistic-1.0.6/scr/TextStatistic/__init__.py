a73d226eef732a7c14e777a30fb1e5bd9b2e323b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:31 +0000

    Add package TextStatistic/1.0.6/TextStatistic-1.0.6.tar.gz

---
+++ packages/TextStatistic/TextStatistic-1.0.6.tar.gz/TextStatistic-1.0.6/scr/TextStatistic/__init__.py
@@ -0,0 +1,5 @@
+from scr.TextStatistic.TextStatistic import get_words
+from scr.TextStatistic.TextStatistic import words_into_dict
+from scr.TextStatistic.TextStatistic import count_words
+from scr.TextStatistic.TextStatistic import count_unique_words
+from scr.TextStatistic.TextStatistic import all_words
