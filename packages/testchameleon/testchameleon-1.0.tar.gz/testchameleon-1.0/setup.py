abcce1dbde227c68e5e3978da178af4a615e12d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:48 +0000

    Add package testchameleon/1.0/testchameleon-1.0.tar.gz

---
+++ packages/testchameleon/testchameleon-1.0.tar.gz/testchameleon-1.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name="testchameleon",
+    version="1.0",
+    description="this is a module made by kevin",
+    author="jiaxing",
+    py_modules=["testchameleon._version", "testchameleon.algorithm"]
+)
---
+++ packages/testchameleon/testchameleon-1.0.tar.gz/testchameleon-1.0/testchameleon/__init__.py
@@ -0,0 +1,20 @@
+"""
+A library for creating Learning Python Algorithms files.
+"""
+
+from .algorithm import AlgorithmDemo
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+del get_versions
+
+__doc__ = """
+chameleon - 这是一个学习 Python 算法 强大工具库
+=====================================================================
+介绍：
+------
+1. 可以通过该工具直接学习算法，有相关的算法示例，可以很好的掌握 Python 算法，提高编程逻辑
+2. 开发时间：2021-5-20。
+3. 版本号：0.1.15。
+
+"""
