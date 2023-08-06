37735665ba8fbbf1f2d8c55844e1ef29e78073b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:48 +0000

    Add package baretypes/3.4.0/baretypes-3.4.0.tar.gz

---
+++ packages/baretypes/baretypes-3.4.0.tar.gz/baretypes-3.4.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['baretypes']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'baretypes',
+    'version': '3.4.0',
+    'description': 'Types for bareASGI and bareClient',
+    'long_description': '# bareTypes\n\nTypes for [bareASGI](https://github.com/rob-blackbourn/bareASGI)\nand [bareClient](https://github.com/rob-blackbourn/bareClient)\n(read the [docs](https://rob-blackbourn.github.io/bareTypes/)).\n\n## Installation\n\nThe package can be installed with pip.\n\n```bash\npip install baretypes\n```\n\nThis is a Python 3.7 and later package with no dependencies.\n',
+    'author': 'Rob Blackbourn',
+    'author_email': 'rob.blackbourn@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/rob-blackbourn/bareTypes',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
