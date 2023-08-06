8aefe354524aedfe1b29ca0e820d4875ae6adb04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:36 +0000

    Add package portal-openapi-client/0.1.1/portal_openapi_client-0.1.1-py3-none-any.whl

---
+++ packages/portal-openapi-client/portal_openapi_client-0.1.1-py3-none-any.whl/openapi_client/apis/__init__.py
@@ -0,0 +1,19 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from .api.companies_api import CompaniesApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from openapi_client.api.companies_api import CompaniesApi
+from openapi_client.api.machines_api import MachinesApi
+from openapi_client.api.telemetry_api import TelemetryApi
