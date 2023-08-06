88e905e5ccb71e36a655a003fa50c2b51d988bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:10 +0000

    Add package logregnumpy/0.1.0/logregnumpy-0.1.0.tar.gz

---
+++ packages/logregnumpy/logregnumpy-0.1.0.tar.gz/logregnumpy-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='logregnumpy',
+      version='0.1.0',
+      description='Logistic regression classifier',
+      packages=['logregnumpy'],
+      author_email='kir.klyukvin@gmail.com',
+      zip_safe=False)
