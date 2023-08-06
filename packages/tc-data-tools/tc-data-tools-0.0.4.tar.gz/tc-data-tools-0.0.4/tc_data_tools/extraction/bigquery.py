e16da0a3d3cb676eaf1be516742a7f7300d3e364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:43 +0000

    Add package tc-data-tools/0.0.4/tc-data-tools-0.0.4.tar.gz

---
+++ packages/tc-data-tools/tc-data-tools-0.0.4.tar.gz/tc-data-tools-0.0.4/tc_data_tools/extraction/bigquery.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from tc_data_tools.io import gcs_upload
+
+
+def df_from_bigquery(
+    query, gcp_project, gcs_bucket, gcs_file_path, local_file_path, mode="csv"
+):
+
+    df = pd.read_gbq(query, gcp_project)
+
+    if mode == "csv":
+        df.to_csv(local_file_path)
+    elif mode == "parquet":
+        df.to_parquet(local_file_path)
+
+    gcs_upload(gcp_project, gcs_bucket, gcs_file_path, local_file_path)
+
+    return df
