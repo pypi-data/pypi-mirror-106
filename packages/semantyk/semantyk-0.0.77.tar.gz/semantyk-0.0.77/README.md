1ddaaf39097a2015d616923e750803df5c8dcc49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:56 +0000

    Add package semantyk/0.0.77/semantyk-0.0.77.tar.gz

---
+++ packages/semantyk/semantyk-0.0.77.tar.gz/semantyk-0.0.77/README.md
@@ -0,0 +1,18 @@
+***
+<h1 align='center'>Semantyk</h1>
+<h3 align='center'><i>Ideas Wonder</i></h3>
+<p align='center'>
+  <img alt='Semantyk' src='https://www.semantyk.com/static/Semantyk.png' width='250'/>
+  <br>
+  <br>
+  <b>August 31, 2017</b>
+  <br>
+  <i>Copyright © Semantyk 2021. All rights reserved.</i>
+</p>
+
+***
+
+[![Build Status](https://www.travis-ci.com/semantyk/Semantyk.svg?branch=master)](https://www.travis-ci.com/semantyk/Semantyk)
+[![GitHub stars](https://raster.shields.io/github/stars/semantyk/Semantyk.png)](https://github.com/semantyk/Semantyk/stargazers)
+[![PyPI](https://img.shields.io/pypi/v/semantyk.svg)](https://pypi.python.org/pypi/semantyk)
+[![PyPI](https://img.shields.io/pypi/pyversions/semantyk.svg)](https://pypi.python.org/pypi/semantyk)
---
+++ packages/semantyk/semantyk-0.0.77.tar.gz/semantyk-0.0.77/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from pip._internal.req import parse_requirements
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    author='Semantyk',
+    author_email='contact@semantyk.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8'
+    ],
+    description='Ideas Wonder.',
+    keywords='semantyk',
+    license='LICENSE',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    maintainer='Semantyk',
+    name='semantyk',
+    packages=find_packages(
+        exclude={
+            'doc',
+            'tests*'
+        }
+    ),
+    url='https://github.com/semantyk/Semantyk',
+    version='0.0.77'
+)
