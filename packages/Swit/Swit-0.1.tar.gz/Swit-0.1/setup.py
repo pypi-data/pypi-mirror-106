99921501d85cced1092cdd681bcda3f9731a4dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:22 +0000

    Add package Swit/0.1/Swit-0.1.tar.gz

---
+++ packages/Swit/Swit-0.1.tar.gz/Swit-0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+setup(
+    name='Swit',
+    packages=['Swit'],
+    version='0.1',
+    license='MIT',
+    description='Swit is a basic open-source implementation of Git, meant for experimenting and studying Git\'s concepts and core design.',
+    author='Noga Osin',
+    author_email='nogaos97@gmail.com',
+    url='https://github.com/NogaOs/Swit',
+    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    keywords=['git', 'version control', 'source control'],
+    install_requires=[
+        'wheel',
+        'loguru',
+        'networkx',
+        'matplotlib',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
