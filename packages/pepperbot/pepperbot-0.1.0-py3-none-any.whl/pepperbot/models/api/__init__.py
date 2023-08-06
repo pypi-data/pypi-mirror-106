f1d52bf9d66fcedc95dd5558cb42610d3a93811f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:44 +0000

    Add package pepperbot/0.1.0/pepperbot-0.1.0-py3-none-any.whl

---
+++ packages/pepperbot/pepperbot-0.1.0-py3-none-any.whl/pepperbot/models/api/__init__.py
@@ -0,0 +1,27 @@
+from typing import List, Literal
+
+from pydantic import BaseModel
+from pepperbot.models.user import GroupMember
+
+
+class get_group_member_list_return(BaseModel):
+    data: List[GroupMember]
+    retcode: int
+    status: Literal["ok"]
+
+
+class get_group_member_info_return(BaseModel):
+    data: GroupMember
+    retcode: int
+    status: Literal["ok"]
+
+
+class SelfInfo(BaseModel):
+    user_id: int
+    nickname: str
+
+
+class get_login_info(BaseModel):
+    data: SelfInfo
+    retcode: int
+    status: Literal["ok"]
