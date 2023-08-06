8ab1849edf3544e0c8320326e202dd8c48848ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:46 +0000

    Add package anoapycore/0.1.9/anoapycore-0.1.9-py3-none-any.whl

---
+++ packages/anoapycore/anoapycore-0.1.9-py3-none-any.whl/anoapycore/__init__.py
@@ -0,0 +1,9 @@
+from . import data
+from . import chart
+from . import ml
+from . import statmodel
+
+from pkg_resources import get_distribution
+
+__version__ = get_distribution('anoapycore').version
+
