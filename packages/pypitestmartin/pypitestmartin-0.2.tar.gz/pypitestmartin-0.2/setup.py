dd64a3a988c1ca13dc7c097c03edc6ef6abde863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:11 +0000

    Add package pypitestmartin/0.2/pypitestmartin-0.2.tar.gz

---
+++ packages/pypitestmartin/pypitestmartin-0.2.tar.gz/pypitestmartin-0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="pypitestmartin",
+    version="0.2",
+    python_requires=">=3.7",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    description="Test package",
+    url="",
+    author="Martin",
+    author_email="",
+    license="MIT",
+    install_requires=[
+    ],
+
+)
