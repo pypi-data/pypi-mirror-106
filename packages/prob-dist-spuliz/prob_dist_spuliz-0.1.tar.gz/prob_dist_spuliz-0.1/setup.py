64e36b73908f911d6b0003ae94d6b7f2cd1a915e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:44 +0000

    Add package prob-dist-spuliz/0.1/prob_dist_spuliz-0.1.tar.gz

---
+++ packages/prob-dist-spuliz/prob_dist_spuliz-0.1.tar.gz/prob_dist_spuliz-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prob_dist_spuliz',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_dist_spuliz'],
+      zip_safe=False)
