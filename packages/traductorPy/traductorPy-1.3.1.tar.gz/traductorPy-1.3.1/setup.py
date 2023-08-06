4b42491afa4a370cbd4d6fe05852d3b07623c9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:33 +0000

    Add package traductorPy/1.3.1/traductorPy-1.3.1.tar.gz

---
+++ packages/traductorPy/traductorPy-1.3.1.tar.gz/traductorPy-1.3.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+try:
+    long_description = open('README.md', encoding='utf-8').read()
+except (IOError, ImportError):
+    long_description = ''
+  
+setup(
+  name = 'traductorPy',
+  packages = ['traductorPy'],
+  version = '1.3.1',
+  url = 'https://github.com/jrm2087/PyLang',
+  description = 'Traductor web',
+  long_description = long_description,
+  long_description_content_type='text/markdown',
+  author = ['José D. Rodríguez','Stiven Perdomo','Juan Camilo Castellanos'],
+  author_email = ['jrm2087@gmail.com','stiven97.perdomo@gmail.com ','camilo.castellanos.puentes@gmail.com'],
+  keywords = ['traductor','web'],
+  python_requires=">=3.6",
+  install_requires = ['selenium>=3.141.0'],
+  classifiers = [],
+)                         
