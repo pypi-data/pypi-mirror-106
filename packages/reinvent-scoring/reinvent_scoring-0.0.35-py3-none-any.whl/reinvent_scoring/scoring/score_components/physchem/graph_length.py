3a5c32a645f2fa86cc0742aa9cdc74d3bb70b904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:33 +0000

    Add package reinvent-scoring/0.0.35/reinvent_scoring-0.0.35-py3-none-any.whl

---
+++ packages/reinvent-scoring/reinvent_scoring-0.0.35-py3-none-any.whl/reinvent_scoring/scoring/score_components/physchem/graph_length.py
@@ -0,0 +1,12 @@
+import numpy as np
+from rdkit.Chem import GetDistanceMatrix
+from reinvent_scoring.scoring.component_parameters import ComponentParameters
+from reinvent_scoring.scoring.score_components.physchem.base_physchem_component import BasePhysChemComponent
+
+
+class GraphLength(BasePhysChemComponent):
+    def __init__(self, parameters: ComponentParameters):
+        super().__init__(parameters)
+
+    def _calculate_phys_chem_property(self, mol):
+        return(int(np.max(GetDistanceMatrix(mol))))
