b7ef78d2b91ee35e7d14ac69c36c7546611ea8e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:13 +0000

    Add package monkey-color/0.0.5/monkey_color-0.0.5-py3-none-any.whl

---
+++ packages/monkey-color/monkey_color-0.0.5-py3-none-any.whl/color/color.py
@@ -0,0 +1,57 @@
+
+# Author: 猴子
+# Date: 2021-05-23 15:09:28
+# LastEditTime: 2021-05-23 20:15:41
+# FilePath: /color/color.py
+
+
+end = "\033[0m"
+
+
+class main(object):
+    def __init__(self):
+        for name in dir(self):
+            if not name.startswith('_'):
+                setattr(self, name, f"\033[{getattr(self, name)}m")
+
+
+class txt(main):
+    # 黑色
+    black = 30
+    # 红色
+    red = 31
+    # 绿色
+    green = 32
+    # 黃色
+    yellow = 33
+    # 蓝色
+    blue = 34
+    # 紫红色
+    Fuchsia = 35
+    # 青蓝色
+    cyan = 36
+    # 白色
+    white = 37
+
+
+class background(main):
+    # 黑色
+    black = 40
+    # 红色
+    red = 41
+    # 绿色
+    green = 42
+    # 黃色
+    yellow = 43
+    # 蓝色
+    blue = 44
+    # 紫红色
+    Fuchsia = 45
+    # 青蓝色
+    cyan = 46
+    # 白色
+    white = 47
+
+
+txt = txt()
+background = background()
