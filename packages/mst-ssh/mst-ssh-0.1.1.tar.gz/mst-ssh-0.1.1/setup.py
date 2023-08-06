ac8672965470863ff84835ecc1b7e21c45e52562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:57 +0000

    Add package mst-ssh/0.1.1/mst-ssh-0.1.1.tar.gz

---
+++ packages/mst-ssh/mst-ssh-0.1.1.tar.gz/mst-ssh-0.1.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['mst_ssh']
+entry_points = \
+{'console_scripts': ['mst-ssh = mst_ssh:main']}
+
+setup_kwargs = {
+    'name': 'mst-ssh',
+    'version': '0.1.1',
+    'description': "A command line utility to setup SSH connection to Missouri S&T's Linux virtual machines",
+    'long_description': None,
+    'author': 'Kevin Lai',
+    'author_email': 'zlnh4@umsystem.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
