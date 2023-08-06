a5823cc8121fe0f9231482d1f9d44bdfe5f3a72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:17 +0000

    Add package feffery-antd-components/0.0.1a1/feffery_antd_components-0.0.1a1.tar.gz

---
+++ packages/feffery-antd-components/feffery_antd_components-0.0.1a1.tar.gz/feffery_antd_components-0.0.1a1/feffery_antd_components/AntdInputPassword.py
@@ -0,0 +1,36 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class AntdInputPassword(Component):
+    """An AntdInputPassword component.
+
+
+Keyword arguments:
+- children (a list of or a singular dash component, string or number; optional)
+- id (string; optional)
+- className (string; optional)
+- style (dict; optional)
+- placeholder (string; optional)
+- value (string; optional)
+- allowClear (boolean; default True)"""
+    @_explicitize_args
+    def __init__(self, children=None, id=Component.UNDEFINED, className=Component.UNDEFINED, style=Component.UNDEFINED, placeholder=Component.UNDEFINED, value=Component.UNDEFINED, allowClear=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id', 'className', 'style', 'placeholder', 'value', 'allowClear']
+        self._type = 'AntdInputPassword'
+        self._namespace = 'feffery_antd_components'
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['children', 'id', 'className', 'style', 'placeholder', 'value', 'allowClear']
+        self.available_wildcard_properties =            []
+
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        for k in []:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(AntdInputPassword, self).__init__(children=children, **args)
