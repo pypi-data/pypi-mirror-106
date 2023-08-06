959067064a842056134970a36a546d89a91cbfcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:33 +0000

    Add package chirptext/0.1.2/chirptext-0.1.2.tar.gz

---
+++ packages/chirptext/chirptext-0.1.2.tar.gz/chirptext-0.1.2/chirptext/__version__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# chirptext's package version information
+__author__ = "Le Tuan Anh"
+__email__ = "tuananh.ke@gmail.com"
+__copyright__ = "Copyright (c) 2012, Le Tuan Anh"
+__credits__ = []
+__license__ = "MIT License"
+__description__ = "A minimalist collection of text processing tools for Python 3"
+__url__ = "https://github.com/letuananh/chirptext/"
+__maintainer__ = "Le Tuan Anh"
+__version_major__ = "0.1.2"
+__version__ = "{}".format(__version_major__)
+__version_long__ = "{}".format(__version_major__)
+__status__ = "5 - Production/Stable"
