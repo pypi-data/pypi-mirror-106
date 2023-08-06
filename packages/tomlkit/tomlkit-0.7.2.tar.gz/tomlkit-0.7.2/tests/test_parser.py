7431ed542cbf740529b359344916fd8f41a5860d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:20 +0000

    Add package tomlkit/0.7.2/tomlkit-0.7.2.tar.gz

---
+++ packages/tomlkit/tomlkit-0.7.2.tar.gz/tomlkit-0.7.2/tests/test_parser.py
@@ -0,0 +1,32 @@
+import pytest
+
+from tomlkit.exceptions import EmptyTableNameError
+from tomlkit.exceptions import InternalParserError
+from tomlkit.items import StringType
+from tomlkit.parser import Parser
+
+
+def test_parser_should_raise_an_internal_error_if_parsing_wrong_type_of_string():
+    parser = Parser('"foo"')
+
+    with pytest.raises(InternalParserError) as e:
+        parser._parse_string(StringType.SLL)
+
+    assert e.value.line == 1
+    assert e.value.col == 0
+
+
+def test_parser_should_raise_an_error_for_empty_tables():
+    content = """
+[one]
+
+[]
+"""
+
+    parser = Parser(content)
+
+    with pytest.raises(EmptyTableNameError) as e:
+        parser.parse()
+
+    assert e.value.line == 4
+    assert e.value.col == 1
