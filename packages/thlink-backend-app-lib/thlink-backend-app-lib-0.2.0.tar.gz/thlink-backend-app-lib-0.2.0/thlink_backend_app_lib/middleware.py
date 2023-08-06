9912d4c6de7aa5eb724f895b760739da2dcfda71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:46 +0000

    Add package thlink-backend-app-lib/0.2.0/thlink-backend-app-lib-0.2.0.tar.gz

---
+++ packages/thlink-backend-app-lib/thlink-backend-app-lib-0.2.0.tar.gz/thlink-backend-app-lib-0.2.0/thlink_backend_app_lib/middleware.py
@@ -0,0 +1,51 @@
+from typing import Union, Dict
+import logging
+from aws_lambda_powertools.middleware_factory import lambda_handler_decorator
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.utilities.parser import ValidationError
+from . import BaseModel
+
+
+class BadOperationUserError(Exception):
+    pass
+
+
+class EntityDoesNotExistUserError(Exception):
+    pass
+
+
+class AppErrorModel(BaseModel):
+    type: str
+    message: Union[str, Dict]
+
+
+class AppErrorResponseModel(BaseModel):
+    app_error: AppErrorModel = None
+
+
+logger = Logger(service="Document", level=logging.INFO)
+
+
+def error(type_: str, message: Union[str, Dict]):
+    return AppErrorResponseModel(app_error=AppErrorModel(type=type_, message=message)).dict()
+
+
+@lambda_handler_decorator
+def middleware(handler, event, context):
+    handler = logger.inject_lambda_context(log_event=logger.level == logging.DEBUG)(handler)
+    try:
+        response = handler(event, context)
+        logger.debug("Handler Response", extra=response)
+        return response
+    except ValidationError as e:
+        logger.info("EventValidationUserError")
+        return error("EventValidationUserError", e.json())
+    except BadOperationUserError as e:
+        logger.info("BadOperationUserError")
+        return error("BadOperationUserError", str(e))
+    except EntityDoesNotExistUserError as e:
+        logger.info("EntityDoesNotExistUserError")
+        return error("EntityDoesNotExistUserError", str(e))
+    except:
+        logger.exception("InternalError")
+        return error("InternalError", "Oh no!")
