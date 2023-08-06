91e9bad72b4da2471b0566dc632419701f201eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:53 +0000

    Add package dsFramework/0.1.10/dsFramework-0.1.10-py3-none-any.whl

---
+++ packages/dsFramework/dsFramework-0.1.10-py3-none-any.whl/cli/__init__.py
@@ -0,0 +1,5 @@
+from cli.allow_commands import AllowCommands
+from cli.preprocessor_template import generatedClass
+from cli.labelizer_template import generatedClass
+from cli.postprocessor_template import generatedClass
+
