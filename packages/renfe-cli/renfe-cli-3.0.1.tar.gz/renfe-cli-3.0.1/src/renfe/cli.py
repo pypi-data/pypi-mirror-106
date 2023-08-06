1e2b81e662f29989b0c1879eb3722bc8097db588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:20 +0000

    Add package renfe-cli/3.0.1/renfe-cli-3.0.1.tar.gz

---
+++ packages/renfe-cli/renfe-cli-3.0.1.tar.gz/renfe-cli-3.0.1/src/renfe/cli.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+import optparse
+import logging
+import colorama
+from datetime import date
+
+from renfe.timetable import get_timetable
+from renfe.stations import get_station_by_key, station_exists, get_station_name
+from renfe.utils import RenfeException, ConfigurationMgmt
+
+
+def main():
+    # defaults
+    config = ConfigurationMgmt()
+    stations = config.get_config()
+    today = date.today()
+
+    # opt parser
+    p = optparse.OptionParser()
+    p.add_option('--days', '-d', default=0, help='number of days from today to get the timetable')
+    p.add_option('--origin', '-o', default=stations.get('origin', '79202'),
+                 help='from/origin ID of the train station. Use flag '
+                      '\'-s <possible station name>\' in order to search for IDs')
+    p.add_option('--to', '-t', default=stations.get('to', '71802'),
+                 help='to/destination ID of the train station. Use flag '
+                      '\'-s <possible station name>\' in order to search for IDs')
+    p.add_option('--search', '-s', default='',
+                 help='you need to get the stations IDs, searching by names; '
+                      'in order to apply right inputs for origins and/or destinations')
+    p.add_option('--logging-level', '-l',
+                 help='logging level defaults to warning and possible values are:'
+                      ' debug, info, warning, error and critical')
+    p.add_option('--logging-file', '-f',
+                 help='logging file name is required if you want to submit an issue with more information')
+    p.add_option('--update-config', '-u', default=False, action='store_true', dest='update_config',
+                 help='change your origin and destination stations to defaults when loading this flag')
+    options, arguments = p.parse_args()
+
+    # check and configure input parameters
+    try:
+        config.configs_and_checks(options)
+        if options.update_config:
+            config.set_config({'origin': options.origin, 'to': options.to})
+    except RenfeException as err:
+        logging.error(err)
+        logging.error(
+            "Error configuring.. Check your inputs and enable debug. If the problem persists,"
+            " create an issue at http://www.github.com/gerardcl/renfe-cli/issues")
+        exit(1)
+
+    colorama.init(autoreset=True)
+    print(colorama.Fore.GREEN + "Today is: {}".format(today) + colorama.Fore.RESET)
+
+    # print timetable for given origin and to stations for a given date
+    if options.search == '':
+        if not (station_exists(options.origin) and station_exists(options.to)):
+            logging.error(
+                "Please, provide right values for origin and destination station names")
+            exit(1)
+
+        try:
+            times = get_timetable(get_station_name(options.origin), get_station_name(options.to), int(options.days))
+            print(colorama.Fore.GREEN + "=======================TIMETABLE======================")
+            print(colorama.Fore.GREEN + " {:<10} | {:<10} | {:<10} | {:<10} ".format(
+                'Train', 'Departure', 'Arrival', 'Duration'))
+            for time in times:
+                print(colorama.Fore.GREEN + "------------------------------------------------------")
+                print(colorama.Fore.GREEN + " {:<10} | {:<10} | {:<10} | {:<10} ".format(
+                    time[0], time[1], time[2], time[3]))
+            print(colorama.Fore.GREEN + "======================================================" + colorama.Fore.RESET)
+        except RenfeException as err:
+            logging.error(err)
+            logging.error(
+                "No timetables found... Check your inputs and enable debug. If the problem persists,"
+                " create an issue at http://www.github.com/gerardcl/renfe-cli/issues")
+            exit(1)
+
+    # search into list of Station names and its Renfe identifiers
+    else:
+        try:
+            get_station_by_key(options.search)
+        except RenfeException as err:
+            logging.error(err)
+            logging.error(
+                "Error searching stations IDs... Check your inputs and enable debug. If the problem persists,"
+                " create an issue at http://www.github.com/gerardcl/renfe-cli/issues")
+            exit(1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/renfe-cli/renfe-cli-3.0.1.tar.gz/renfe-cli-3.0.1/src/renfe/utils.py
@@ -0,0 +1,60 @@
+import sys
+import os
+import json
+import logging
+from pathlib import Path
+
+
+class RenfeException(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class ConfigurationMgmt():
+    _config_location = '.renfe_default_stations.json'
+    _logging_levels = {'critical': logging.CRITICAL,
+                       'error': logging.ERROR,
+                       'warning': logging.WARNING,
+                       'info': logging.INFO,
+                       'debug': logging.DEBUG}
+
+    def __init__(self):
+        if os.path.exists(os.path.join(Path.home(), self._config_location)):
+            self.__dict__ = json.load(open(os.path.join(Path.home(), self._config_location)))
+        else:
+            self.__dict__ = {
+                'origin': '79202',  # SILS
+                'to': '71802'  # BCN PASSEIG DE GRACIA
+            }
+
+    # get_defaults
+    def get_config(self):
+        return self.__dict__
+
+    # set_defaults
+    def set_config(self, new_config):
+        logging.debug("setting new defaults to {}".format(new_config))
+        self.__dict__ = new_config
+        json.dump(self.__dict__, open(os.path.join(Path.home(), self._config_location), 'w'))
+
+    def configs_and_checks(self, options):
+        # Logging defaults to warning: critical, error and warning messages.
+        logging.basicConfig(level=self._logging_levels.get(options.logging_level, logging.WARN),
+                            filename=options.logging_file,
+                            format='%(asctime)s %(levelname)s: %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        logging.debug(
+            "params --> days: {}, origin: {}, destination: {}, search: {}, update_config:"
+            " {}, logging_level: {}, logging_file: {} <--"
+            .format(options.days, options.origin, options.to, options.search,
+                    options.update_config, options.logging_level, options.logging_file))
+        # Doing some checks
+        if options.origin == options.to:
+            raise RenfeException("Cannot search timetables if both origin and destiantion are the same")
+        try:
+            int(options.days)
+            if int(options.days) < 0:
+                raise ValueError("Only today or future days can be searched...")
+        except ValueError:
+            logging.debug(sys.exc_info())
+            raise RenfeException("Cannot search timetables if date params are not set with numbers")
