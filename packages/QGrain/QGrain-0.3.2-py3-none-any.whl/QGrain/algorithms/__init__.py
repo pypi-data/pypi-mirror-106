868647add54cf1ccf30979c5afe97f379ac2f5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:42 +0000

    Add package QGrain/0.3.2/QGrain-0.3.2-py3-none-any.whl

---
+++ packages/QGrain/QGrain-0.3.2-py3-none-any.whl/QGrain/algorithms/__init__.py
@@ -0,0 +1,16 @@
+from enum import Enum, unique
+
+@unique
+class DistributionType(Enum):
+    Customized = "Customized"
+    Nonparametric = "NonParametric"
+    Normal = "Normal"
+    Weibull = "Weibull"
+    SkewNormal = "SkewNormal"
+
+@unique
+class FittingState(Enum):
+    NotStarted = 0
+    Fitting = 1
+    Failed = 2
+    Succeeded = 4
