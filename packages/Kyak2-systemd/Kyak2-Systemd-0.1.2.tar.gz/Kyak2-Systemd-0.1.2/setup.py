8f6ac21adb945edc335c50a94115a1d097a75966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:01 +0000

    Add package Kyak2-systemd/0.1.2/Kyak2-Systemd-0.1.2.tar.gz

---
+++ packages/Kyak2-systemd/Kyak2-Systemd-0.1.2.tar.gz/Kyak2-Systemd-0.1.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Kyak2-Systemd",
+    version="0.1.2",
+    author="Uğurcan Yaşayan",
+    author_email="ugurcanyasayan1994@protonmail.com",
+    description="Openbox Logout screen with Tkinter, for users who use systemd",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UnbalancedSkunk/Kyak2",
+    project_urls={
+        "Bug Tracker": "https://github.com/UnbalancedSkunk/Kyak2",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
