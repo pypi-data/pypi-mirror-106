72984082eb3faaf621d53fab4f94a016fdd51be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:25 +0000

    Add package itertree/0.7.3/itertree-0.7.3.tar.gz

---
+++ packages/itertree/itertree-0.7.3.tar.gz/itertree-0.7.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='itertree',
+    version='0.7.3',
+    packages=['itertree', 'itertree.examples'],
+    package_dir={'': 'src'},
+    url='https://github.com/BR1py/itertree',
+    license='MIT',
+    author='B_R',
+    author_email='br_development@posteo.org',
+    description='Python tree structure for data storage and iterations'
+)
---
+++ packages/itertree/itertree-0.7.3.tar.gz/itertree-0.7.3/src/itertree/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+__package__ = 'itertree'
+__version__ = '0.7.3'
+__licence__ = 'MIT'
+__author__ = 'B.R.'
+__url__ = 'https://github.com/BR1py/itertree'
+__description__ = 'Python tree structure for data storage and iterations'
+
+from .itree_helpers import iTLink,iTMatch,TagIdx,TagIdxStr,TagMultiIdx,TagIdxBytes,iTInterval,TEMPORARY,READ_ONLY,LINKED,COPY_DEEP,COPY_OFF,COPY_NORMAL
+from .itree_main import iTree,iTreeLink,iTreeTemporary,iTreeReadOnly
+
+from . import itree_data as Data
+from . import itree_serialize as Serializer
+from . import itree_filter as Filter
