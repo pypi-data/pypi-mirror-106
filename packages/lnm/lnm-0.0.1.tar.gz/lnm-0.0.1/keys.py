c239bc18e52d2d591869752ca1a9982b057d6d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:22 +0000

    Add package lnm/0.0.1/lnm-0.0.1.tar.gz

---
+++ packages/lnm/lnm-0.0.1.tar.gz/lnm-0.0.1/keys.py
@@ -0,0 +1,8 @@
+business_shortcode = "174379" #lipa na mpesa short code
+phone_number = "254718686209"
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "18VioSM2JMDOlgwAGkrR5RK8qnSIf3kR"
+consumer_secret = "HrFh3b2eMYGuIAAW"
+short_code = "601372" #603021
+short_code = "601372"
+test_msisdn = "254708374149" #phone number of the person sending payment
