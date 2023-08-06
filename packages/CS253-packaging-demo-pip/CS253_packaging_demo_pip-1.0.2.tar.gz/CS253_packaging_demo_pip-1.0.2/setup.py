5a40643bc35c315a8d16b88ddc5072eee4a8ab2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:51 +0000

    Add package CS253-packaging-demo-pip/1.0.2/CS253_packaging_demo_pip-1.0.2.tar.gz

---
+++ packages/CS253-packaging-demo-pip/CS253_packaging_demo_pip-1.0.2.tar.gz/CS253_packaging_demo_pip-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from my_pip_pkg import __version__
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='CS253_packaging_demo_pip',
+    version="1.0.2",
+
+    description="A demo project for CS253 presentation",
+    url='https://demo_project.com/dummy.xml',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Ksai',
+    author_email='vksai@iitk.ac.in',
+    install_requires=["numpy"],
+    packages=find_packages(),
+)
