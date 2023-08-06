188362292cd238215c22fc84c56e7521eee283e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:44 +0000

    Add package optmagic/1.0.2/optmagic-1.0.2.tar.gz

---
+++ packages/optmagic/optmagic-1.0.2.tar.gz/optmagic-1.0.2/setup.py
@@ -0,0 +1,25 @@
+# Imports #
+from setuptools import setup, find_namespace_packages
+from os import path
+
+# Load the contents of the README file #
+this_dir = path.abspath(path.dirname(__file__))
+readme_path = path.join(this_dir, 'README.md')
+with open(readme_path, encoding='utf-8') as handle: readme = handle.read()
+
+# Call setup #
+setup(
+    name             = 'optmagic',
+    version          = '1.0.2',
+    description      = 'Automatically make a command line interface from a '
+                       'class or function.',
+    license          = 'MIT',
+    url              = 'http://github.com/xapple/optmagic/',
+    author           = 'Lucas Sinclair',
+    author_email     = 'lucas.sinclair@me.com',
+    packages         = find_namespace_packages(),
+    install_requires = ['docstring_parser'],
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
+    include_package_data = True,
+)
