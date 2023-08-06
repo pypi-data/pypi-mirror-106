06504626f6192b0e38e100659246ae9fb8e47643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:41 +0000

    Add package minisql/0.1.1/minisql-0.1.1.tar.gz

---
+++ packages/minisql/minisql-0.1.1.tar.gz/minisql-0.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='minisql',
+	  author="catrad",
+      version='0.1.1',
+      description='Удобный инструмент для работы с sqlite3! test',
+      author_email='vegasrodrig152@gmail.com',
+      zip_safe=False
+      )
