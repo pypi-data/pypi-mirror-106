c1d524772df70fa8bb4baa873283f9a3452e371f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:00 +0000

    Add package nar.py/0.0.7/nar.py-0.0.7.tar.gz

---
+++ packages/nar.py/nar.py-0.0.7.tar.gz/nar.py-0.0.7/nar/utils.py
@@ -0,0 +1,16 @@
+class Utils:
+
+  class Urls:
+    user_data = '/player/data'
+    president_data = '/president/player'
+    world_nations = '/world/view/list'
+    alliance_nations = '/alliance/list'
+    alliance_info = '/alliance/main-view'
+    alliances = '/world/fetch/alliances'
+    alliance_pending_reqs = '/alliance/list/pending/requests'
+    world_Wars = '/world/wars/list'
+    inbox_reports = '/inbox/reports'
+    inbox_messages = '/inbox/messages'
+    military_units = '/military/players/units'
+    wars_status = '/war/status'
+    mission_status = '/missions/fetch/active'
