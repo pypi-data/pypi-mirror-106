42d64fe93c8fb1c3e52f0fbf92a40f98541ee3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:20 +0000

    Add package pymarkowitz/1.1.5/pymarkowitz-1.1.5.tar.gz

---
+++ packages/pymarkowitz/pymarkowitz-1.1.5.tar.gz/pymarkowitz-1.1.5/pymarkowitz/__init__.py
@@ -0,0 +1,6 @@
+from .Moments import MomentGenerator
+from .Returns import ReturnGenerator
+from .Optimizer import Optimizer
+from .Backtester import Config, WeighMarkowitz
+
+
---
+++ packages/pymarkowitz/pymarkowitz-1.1.5.tar.gz/pymarkowitz-1.1.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='pymarkowitz',
+      version='1.1.5',
+      description='pymarkowitz',
+      url='https://github.com/johnsoong216/pymarkowitz',
+      packages=["pymarkowitz"],
+      author='johnsoong216',
+      author_email='johnsoong216@hotmail.com',
+      license='MIT',
+      keywords=['portfolio-optimization', 'finance', 'mean-variance-optimization'],
+      install_requires=["numpy", "pandas", "pandas-datareader", "sklearn", "seaborn", "plotly", "matplotlib", "bt"],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      zip_safe=False)
+
+
