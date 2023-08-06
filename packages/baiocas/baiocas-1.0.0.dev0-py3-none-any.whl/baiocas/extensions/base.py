ba27581ddea922e2ab4fac9615749a6df9e9f3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:42 +0000

    Add package baiocas/1.0.0.dev0/baiocas-1.0.0.dev0-py3-none-any.whl

---
+++ packages/baiocas/baiocas-1.0.0.dev0-py3-none-any.whl/baiocas/extensions/base.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import logging
+
+
+class Extension(object):
+
+    def __init__(self):
+        self.log = logging.getLogger('%s.%s' % (self.__module__, self.name))
+        self._client = None
+
+    def __repr__(self):
+        return self.name
+
+    @property
+    def client(self):
+        return self._client
+
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+    def receive(self, message):
+        return message
+
+    def register(self, client):
+        self.log.debug('Executing registration callback')
+        self._client = client
+
+    def send(self, message):
+        return message
+
+    def unregister(self):
+        self.log.debug('Executing unregistration callback')
+        self._client = None
---
+++ packages/baiocas/baiocas-1.0.0.dev0-py3-none-any.whl/baiocas/listener.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from collections import namedtuple
+
+
+Listener = namedtuple('Listener', 'id function extra_args extra_kwargs')
