849bdc0d79500b86dcaefbabf7a8044b8b593170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:12 +0000

    Add package robotframework-processcube/1.1.0a3/robotframework-processcube-1.1.0a3.tar.gz

---
+++ packages/robotframework-processcube/robotframework-processcube-1.1.0a3.tar.gz/robotframework-processcube-1.1.0a3/ProcessCubeLibrary/keywords/start_keyword.py
@@ -0,0 +1,32 @@
+
+from atlas_engine_client.core.api import ProcessStartRequest
+from atlas_engine_client.core.api import StartCallbackType
+
+class StartKeyword:
+
+    def __init__(self, client, **kwargs):
+        self._client = client
+
+    def start_processmodel_and_wait(self, process_model, payload={}):
+        request = ProcessStartRequest(
+            process_model_id = process_model,
+            initial_token = payload,
+            return_on = StartCallbackType.CallbackOnProcessInstanceFinished
+        )
+
+        result = self._client.process_model_start(process_model, request)
+
+        return result
+
+    def start_processmodel(self, process_model, payload={}):
+
+        request = ProcessStartRequest(
+            process_model_id = process_model,
+            initial_token = payload,
+            return_on = StartCallbackType.CallbackOnProcessInstanceCreated
+        )
+
+        result = self._client.process_model_start(process_model, request)
+
+        return result
+
