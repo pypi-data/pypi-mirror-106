9b56ef4d6d8ad4bd637a71543ad1b18e242bbaf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:40 +0000

    Add package javaman/1.0.6/javaman-1.0.6.tar.gz

---
+++ packages/javaman/javaman-1.0.6.tar.gz/javaman-1.0.6/src/javaman/accions/clients.py
@@ -0,0 +1,22 @@
+from javaman.connexio import JManCon
+
+
+class Clients:
+    url_crear_client = '/clients'
+    url_get_client = '/clients'
+    url_importar_tercers = '/importar_tercers'
+
+    def __init__(self, con: JManCon):
+        self._con = con
+
+    def crear(self, p_client: dict):
+        req = self._con.post(url=self.url_crear_client, data=p_client)
+        return req.json()
+
+    def get_client(self, p_client: int):
+        req = self._con.get(url=self.url_crear_client+'/'+str(p_client))
+        return req.json()
+
+    def importar_tercers(self, p_dades: dict):
+        req = self._con.post(url=self.url_importar_tercers, data=p_dades)
+        return req.json()
---
+++ packages/javaman/javaman-1.0.6.tar.gz/javaman-1.0.6/src/javaman/accions/portals_web.py
@@ -0,0 +1,13 @@
+from javaman.connexio import JManCon
+
+
+class PortalsWeb:
+    url = '/portals_web/{num_portal}'
+
+    def __init__(self, con: JManCon):
+        self._con = con
+
+    def get_portal_web(self, portal_web_id: int):
+        tmp_url = PortalsWeb.url.format(num_portal=portal_web_id)
+        req = self._con.get(url=tmp_url)
+        return req.json()
