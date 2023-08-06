71bf14cdcf99fe06af884213c8a37c84c27838ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package mpl-plotter/3.8.7/mpl_plotter-3.8.7.tar.gz

---
+++ packages/mpl-plotter/mpl_plotter-3.8.7.tar.gz/mpl_plotter-3.8.7/tests/test_preset_custom.py
@@ -0,0 +1,37 @@
+import unittest
+
+from mpl_plotter.presets.custom import generate_preset_2d, two_d
+from mpl_plotter.presets.custom import generate_preset_3d, three_d
+
+
+class PresetTests(unittest.TestCase):
+
+    def test_2d(self):
+        """
+        Preferred use:
+
+            generate_preset_2d(preset_dest="presets", preset_name="MYPRESET2D", disable_warning=True, overwrite=False)
+
+            my_plot = three_d(preset_dir="presets", preset_name="MYPRESET2D").line
+
+        Use to enable testing:
+        """
+        from tests.presets.MYPRESET2D import preset
+        my_plot = two_d(direct_preset=preset).line
+
+        my_plot(show=True, demo_pad_plot=True, color="blue", title="TITLE")
+
+    def test_3d(self):
+        """
+        Preferred use:
+
+            generate_preset_3d(preset_dest="presets", preset_name="MYPRESET3D", disable_warning=True, overwrite=False)
+
+            my_plot = three_d(preset_dir="presets", preset_name="MYPRESET3D").line
+
+        Use to enable testing:
+        """
+        from tests.presets.MYPRESET3D import preset
+        my_plot = three_d(direct_preset=preset).line
+
+        my_plot(show=True, demo_pad_plot=True, color="blue", title="TITLE")
