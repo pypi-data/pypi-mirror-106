ec87a31049ea047bcaad1110e30e32ee424552d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:40 +0000

    Add package pyconverters-inscriptis/0.2.5/pyconverters-inscriptis-0.2.5.tar.gz

---
+++ packages/pyconverters-inscriptis/pyconverters-inscriptis-0.2.5.tar.gz/pyconverters-inscriptis-0.2.5/tests/test_inscriptis.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from pyconverters_inscriptis.inscriptis import InscriptisConverter, InscriptisParameters
+
+
+def test_inscriptis():
+    testdir = Path(__file__).parent
+    source = Path(testdir, 'data/test.html')
+    result = Path(testdir, 'data/test.txt')
+    with source.open("r") as fin:
+        converter = InscriptisConverter()
+        options = InscriptisParameters(encoding="utf-8")
+        docs = converter.convert(fin, options)
+        assert len(docs) == 1
+        with result.open("r") as fin2:
+            text = fin2.read()
+            assert docs[0].text == text
+
+
+def test_table_in_table():
+    testdir = Path(__file__).parent
+    source = Path(testdir, 'data/table-in-table.html')
+    result = Path(testdir, 'data/table-in-table.txt')
+    with source.open("r") as fin:
+        converter = InscriptisConverter()
+        options = InscriptisParameters(encoding="utf-8")
+        docs = converter.convert(fin, options)
+        assert len(docs) == 1
+        with result.open("r") as fin2:
+            text = fin2.read()
+            assert docs[0].text == text
+
+
+def test_table_pre():
+    testdir = Path(__file__).parent
+    source = Path(testdir, 'data/table-pre.html')
+    result = Path(testdir, 'data/table-pre.txt')
+    with source.open("r") as fin:
+        converter = InscriptisConverter()
+        options = InscriptisParameters(encoding="utf-8")
+        docs = converter.convert(fin, options)
+        assert len(docs) == 1
+        with result.open("r") as fin2:
+            text = fin2.read()
+            assert docs[0].text == text
