dabb5701e3a9b4ea8448cf256d0350e221a02fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:00 +0000

    Add package logger4py/0.3.0/logger4py-0.3.0.tar.gz

---
+++ packages/logger4py/logger4py-0.3.0.tar.gz/logger4py-0.3.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+setup(
+    name='logger4py',
+    version='0.3.0',    
+    description='Logging Python Package',
+    url='https://gitlab.com/Mahmoudaouinti/logger4py.git',
+    author='IoT Team',
+    author_email='aouintimahmod@gmail.com',
+    packages=['logger4py'],
+    install_requires=[ 'watchdog'],
+    include_package_data = True,
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',      
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+    keywords='sample, setuptools, development',
+
+)
