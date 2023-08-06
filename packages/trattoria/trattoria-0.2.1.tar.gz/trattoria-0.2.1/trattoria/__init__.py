420b10050c1bb3ae8e82513ebfb8fb6889c9e772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:13 +0000

    Add package trattoria/0.2.1/trattoria-0.2.1.tar.gz

---
+++ packages/trattoria/trattoria-0.2.1.tar.gz/trattoria-0.2.1/trattoria/__init__.py
@@ -0,0 +1,10 @@
+from .core import (
+    ZeroFinderParameters,
+    G2Result,
+    TimeTraceResult,
+    ZeroFinderResult,
+    LifetimeResult,
+    PTUFile,
+)
+
+from trattoria_core import G2Parameters, TimeTraceParameters, LifetimeParameters
