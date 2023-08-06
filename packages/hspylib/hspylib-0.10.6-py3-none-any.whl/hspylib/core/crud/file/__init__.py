79781d2fae7c77e2199efde79bb6b5a63cc62732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:05 +0000

    Add package hspylib/0.10.6/hspylib-0.10.6-py3-none-any.whl

---
+++ packages/hspylib/hspylib-0.10.6-py3-none-any.whl/hspylib/core/crud/file/__init__.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+#
+# HSPyLib v0.10.6
+#
+# Package: main.hspylib.core.crud.file
+"""Package initialization."""
+
+__all__ = [
+  'file_repository', 
+  'file_storage'
+]
---
+++ packages/hspylib/hspylib-0.10.6-py3-none-any.whl/hspylib/modules/cli/menu/factory/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# HSPyLib v0.10.6
+#
+# Package: main.hspylib.modules.cli.menu.factory
+"""Package initialization."""
+
+__all__ = [
+  'menu_entry', 
+  'menu_factory', 
+  'menu_option'
+]
