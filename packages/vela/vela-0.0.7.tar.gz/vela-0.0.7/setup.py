1f43c84891361440fab466521d5a28cc50cd3ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:35 +0000

    Add package vela/0.0.7/vela-0.0.7.tar.gz

---
+++ packages/vela/vela-0.0.7.tar.gz/vela-0.0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vela',
+    version='0.0.7',
+    py_modules=['vela'],
+    install_requires=[],
+    # entry_points='''
+    #     [console_scripts]
+    #     example=example:example
+    # ''',
+)
+
