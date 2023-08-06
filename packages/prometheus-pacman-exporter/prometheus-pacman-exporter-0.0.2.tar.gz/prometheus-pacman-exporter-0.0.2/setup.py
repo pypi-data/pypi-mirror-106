8659b17c853732cc4c96fe1aeb4b2f1f23617def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:53 +0000

    Add package prometheus-pacman-exporter/0.0.2/prometheus-pacman-exporter-0.0.2.tar.gz

---
+++ packages/prometheus-pacman-exporter/prometheus-pacman-exporter-0.0.2.tar.gz/prometheus-pacman-exporter-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="prometheus-pacman-exporter",
+    description="Export pacman statistics to prometheus",
+    version="0.0.2",
+    license="GPLv3+",
+    author="Dennis Fink",
+    author_email="dennis.fink@c3l.lu",
+    py_modules="prometheus_pacman_exporter",
+    platforms="any",
+    install_requires=["prometheus-client"],
+    entry_points={
+        "console_scripts": [
+            "prometheus-pacman-exporter=prometheus_pacman_exporter:main"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Topic :: System :: Monitoring",
+    ],
+)
