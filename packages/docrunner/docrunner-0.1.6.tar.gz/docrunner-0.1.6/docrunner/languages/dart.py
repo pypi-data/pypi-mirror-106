7aad3a17ee4761a1fb177be1af7a0501daf03254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:09 +0000

    Add package docrunner/0.1.6/docrunner-0.1.6.tar.gz

---
+++ packages/docrunner/docrunner-0.1.6.tar.gz/docrunner-0.1.6/docrunner/languages/dart.py
@@ -0,0 +1,29 @@
+import os
+
+from ..exceptions.base_exception import DocrunnerBaseException
+from ..models.options import Options
+from ..utils.general import log_exception
+from ..utils.language import create_language_files
+
+
+def run_dart(
+    options: Options
+):
+    """Runs all dart code within a markdown '.md' file
+
+    Parameters
+    ----------
+    options : Options
+        Docrunner options
+    """
+
+    try:
+        code_filepaths = create_language_files(
+            options=options,
+        )
+
+        for filepath in list(code_filepaths.keys()):
+            os.system(f'dart run {filepath}')
+
+    except DocrunnerBaseException as error:
+        log_exception(error)
