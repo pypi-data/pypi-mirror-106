706b620daae4f477dfa74d556f69baa957ca1cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:07 +0000

    Add package double-stream-handler/1.0.0rc2/double_stream_handler-1.0.0rc2-py3-none-any.whl

---
+++ packages/double-stream-handler/double_stream_handler-1.0.0rc2-py3-none-any.whl/double_stream_handler/__init__.py
@@ -0,0 +1,57 @@
+import logging
+import sys
+
+from typing import TextIO, Tuple
+
+
+__version__ = "1.0.0-rc.2"
+
+
+class DoubleStreamHandler(logging.Handler):
+    terminator = "\n"
+
+    def __init__(
+        self,
+        err_level: int = logging.WARNING,
+        streams: Tuple[TextIO, TextIO] = (sys.stdout, sys.stderr),
+    ) -> None:
+        logging.Handler.__init__(self)
+
+        self.out, self.err = streams
+        self.err_level = err_level
+
+    def flush(self) -> None:
+        self.acquire()
+        try:
+            for stream in [self.err, self.out]:
+                if stream and hasattr(stream, "flush"):
+                    stream.flush()
+        finally:
+            self.release()
+
+    def emit(self, record: logging.LogRecord):
+        if record.levelno >= self.err_level:
+            stream = self.err
+        else:
+            stream = self.out
+
+        try:
+            msg = self.format(record)
+            stream.write(msg + self.terminator)
+            self.flush()
+        except RecursionError:
+            raise
+        except Exception:
+            self.handleError(record)
+
+    def __repr__(self):
+        err_level_name = logging.getLevelName(self.err_level)
+        out_name = str(getattr(self.out, "name", ""))
+        err_name = str(getattr(self.err, "name", ""))
+
+        return "<%s err_level='%s', streams=(%s, %s)>" % (
+            self.__class__.__name__,
+            err_level_name,
+            out_name,
+            err_name,
+        )
