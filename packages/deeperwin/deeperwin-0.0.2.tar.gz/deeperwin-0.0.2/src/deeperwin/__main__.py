6a96bcd16af46b91da9aebcd67628fc446ee6107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:47 +0000

    Add package deeperwin/0.0.2/deeperwin-0.0.2.tar.gz

---
+++ packages/deeperwin/deeperwin-0.0.2.tar.gz/deeperwin-0.0.2/src/deeperwin/__main__.py
@@ -0,0 +1,6 @@
+import argparse
+import json
+from deeperwin import DefaultConfig
+from deeperwin.main import calculate_with_weight_sharing, calculate_without_weight_sharing
+from deeperwin.utilities.utils import getCodeVersion
+
