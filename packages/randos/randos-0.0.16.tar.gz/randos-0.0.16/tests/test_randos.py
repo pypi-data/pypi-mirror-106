7d3b584faf1766bdbfa608df09a845dafe4bf0fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:21 +0000

    Add package randos/0.0.16/randos-0.0.16.tar.gz

---
+++ packages/randos/randos-0.0.16.tar.gz/randos-0.0.16/tests/test_randos.py
@@ -0,0 +1,92 @@
+from randos import *
+from pytest import mark
+from random import choice, randint
+
+
+@mark.xfail(raises=ValueError)
+@mark.parametrize('length,minimum', [(randint(1, 9999), randint(0, 9999)) for _ in range(1000)])
+def test_random_ints(length: int, minimum: int):
+
+    output = random_ints(length, minimum, minimum - randint(0, 9999))
+    # correct length?
+    assert len(output) == length
+    # correct min?
+    assert min(output) >= minimum
+
+
+@mark.parametrize('length,minimum', [(randint(1, 9999), randint(0, 9999)) for _ in range(1000)])
+def test_random_ints(length: int, minimum: int):
+
+    output = random_ints(length, minimum, minimum + randint(0, 9999))
+    # correct length?
+    assert len(output) == length
+    # correct min?
+    assert min(output) >= minimum
+
+
+@mark.parametrize('include_country', [randint(0, 100) % 2 == 0 for _ in range(1000)])
+def test_random_city(include_country: bool):
+    output = random_city(include_country)
+    # not null?
+    assert output
+    # correct length?
+    assert len(output) == 2 if include_country else len(output)
+    # correct type?
+    assert isinstance(
+        output, tuple) if include_country else isinstance(output, str)
+
+
+@mark.parametrize('include_abbr', [randint(0, 100) % 2 == 0 for _ in range(1000)])
+def test_random_country(include_abbr: bool):
+    output = random_country(include_abbr)
+    # not null?
+    assert output
+    # has length?
+    assert len(output)
+
+
+@mark.parametrize('include_desc', [randint(0, 100) % 2 == 0 for _ in range(1000)])
+def test_random_emoji(include_desc: bool):
+    output = random_emoji(include_desc)
+    # not null?
+    assert output
+    # has length?
+    assert len(output)
+
+
+@mark.parametrize(
+    'file_path',
+    [f'randos/data/{choice(["CITIES", "COUNTRIES", "EMOJIS"])}.txt' for _ in range(1000)])
+def test_random_emoji(file_path: str):
+    output = random_line(file_path, ',' if random_bool() else None)
+    # not null?
+    assert output
+    # has length?
+    assert len(output)
+
+
+@mark.parametrize('_', list(range(1000)))
+def test_random_bool(_):
+    output = random_bool()
+    assert output is True or output is False
+
+
+@mark.parametrize('include_country', [randint(0, 100) % 2 == 0 for _ in range(1000)])
+def test_random_travel_city(include_country: bool):
+    output = random_travel_city(include_country)
+    # not null?
+    assert output
+    # correct length?
+    assert len(output) == 2 if include_country else len(output)
+    # correct type?
+    assert isinstance(
+        output, tuple) if include_country else isinstance(output, str)
+
+
+@mark.parametrize('_', list(range(10)))
+def test_random_travel_destination(_):
+    output = random_travel_destination()
+    # not null?
+    assert output
+    # has length?
+    assert len(output)
