417e687f09c8dd47cd5c583f3f11d082345b3ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:38 +0000

    Add package oc4idskit/0.0.1/oc4idskit-0.0.1-py3-none-any.whl

---
+++ packages/oc4idskit/oc4idskit-0.0.1-py3-none-any.whl/oc4idskit/cli/__main__.py
@@ -0,0 +1,22 @@
+import logging
+
+from ocdskit.cli.__main__ import main as _main
+
+
+def main():
+    modules = (
+        "oc4idskit.cli.commands.convert_from_ocds",
+        "oc4idskit.cli.commands.split_project_packages",
+    )
+
+    logger = logging.getLogger("oc4idskit")
+
+    _main(
+        description="Open Contracting for Infrastructure Data Standards CLI",
+        modules=modules,
+        logger=logger,
+    )
+
+
+if __name__ == "__main__":
+    main()
