eb98a391e08dce88f3482184eeb051a1847dc995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:08 +0000

    Add package nar.py/0.0.5/nar.py-0.0.5.tar.gz

---
+++ packages/nar.py/nar.py-0.0.5.tar.gz/nar.py-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+DESCRIPTION = 'A Wrapper for browser game Nations at Risk\'s API'
+
+# Setting up
+setup(
+    name="nar.py",
+    version=VERSION,
+    author="Blitz#6652",
+    author_email="<blitz2k4.dev@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'nar', 'nations at risk'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
